aee0d28b0a57c700e658be6983adab3ffde9e3be
Ingo Budde <bingo@mail.upb.de>
2011-01-30 22:19:15 +0000

    Initial commit of the GMF patterneditor for umlrt.

---
+++ patterneditor/src/patterneditor/PatterneditorFactory.java
@@ -0,0 +1,46 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package patterneditor;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see patterneditor.PatterneditorPackage
+ * @generated
+ */
+public interface PatterneditorFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	PatterneditorFactory eINSTANCE = patterneditor.impl.PatterneditorFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>Pattern Diagram</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Pattern Diagram</em>'.
+	 * @generated
+	 */
+	PatternDiagram createPatternDiagram();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	PatterneditorPackage getPatterneditorPackage();
+
+} //PatterneditorFactory
