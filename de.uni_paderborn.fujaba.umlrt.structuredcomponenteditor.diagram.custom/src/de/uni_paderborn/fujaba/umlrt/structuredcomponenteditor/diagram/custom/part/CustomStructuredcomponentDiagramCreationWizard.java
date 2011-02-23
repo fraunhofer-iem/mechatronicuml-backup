4f9d2291de4c3f517366e0fa8753f30d897dad50
Ingo Budde <bingo@mail.upb.de>
2011-02-23 14:48:37 +0000

    New File Wizard for structuredcomponent editor

---
+++ de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.diagram.custom/src/de/uni_paderborn/fujaba/umlrt/structuredcomponenteditor/diagram/custom/part/CustomStructuredcomponentDiagramCreationWizard.java
@@ -0,0 +1,74 @@
+package de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.diagram.custom.part;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint;
+import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
+import org.eclipse.gmf.runtime.diagram.core.services.view.CreateDiagramViewOperation;
+import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+
+import de.uni_paderborn.fujaba.newwizard.diagrams.FujabaDiagramNewWizard;
+import de.uni_paderborn.fujaba.umlrt.model.component.ComponentFactory;
+import de.uni_paderborn.fujaba.umlrt.model.component.StructuredComponent;
+import de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.StructuredComponentDiagram;
+import de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.StructuredcomponenteditorFactory;
+
+public class CustomStructuredcomponentDiagramCreationWizard extends
+		FujabaDiagramNewWizard {
+
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		super.init(workbench, selection);
+
+		setWindowTitle(getWindowTitle());
+		setDefaultPageImageDescriptor(de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.diagram.part.StructuredcomponentDiagramEditorPlugin
+				.getBundledImageDescriptor("icons/wizban/NewPatterneditorWizard.gif")); //$NON-NLS-1$
+
+	}
+
+	@Override
+	protected String getDiagramFileExtension() {
+		return "structuredcomponent_diagram"; //$NON-NLS-1$
+	}
+
+	@Override
+	protected String getModelId() {
+		return de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.diagram.edit.parts.StructuredComponentDiagramEditPart.MODEL_ID;
+	}
+
+	@Override
+	protected PreferencesHint getDiagramPreferencesHint() {
+		return de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.diagram.part.StructuredcomponentDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT;
+	}
+
+	@Override
+	protected String getEditorId() {
+		return de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.diagram.part.StructuredcomponentDiagramEditor.ID;
+
+	}
+
+	/**
+	 * Create a new instance of domain element associated with canvas.
+	 */
+	@Override
+	protected EObject createInitialModel() {
+		StructuredComponentDiagram diagram = StructuredcomponenteditorFactory.eINSTANCE
+				.createStructuredComponentDiagram();
+		StructuredComponent structuredComponent = ComponentFactory.eINSTANCE
+				.createStructuredComponent();
+		diagram.setStructuredComponent(structuredComponent);
+		return diagram;
+	}
+
+	@Override
+	public boolean isValidDiagramElement(EObject object) {
+		return ViewService
+				.getInstance()
+				.provides(
+						new CreateDiagramViewOperation(
+								new EObjectAdapter(object),
+								de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.diagram.edit.parts.StructuredComponentDiagramEditPart.MODEL_ID,
+								de.uni_paderborn.fujaba.umlrt.structuredcomponenteditor.diagram.part.StructuredcomponentDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT));
+	}
+
+}
