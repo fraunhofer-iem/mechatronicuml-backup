203538ce7d815b21cb5b2da4200b3eea04ad6dad
Denis Braun <braund@mail.upb.de>
2011-02-23 12:12:26 +0000

    custom settings for the component instance configuration diagram

---
+++ de.uni_paderborn.fujaba.umlrt.componentInstanceConfigurationEditor.diagram.custom/src/de/uni_paderborn/fujaba/umlrt/componentInstanceConfigurationEditor/diagram/custom/part/CustomComponentInstanceConfigurationCreationWizard.java
@@ -0,0 +1,68 @@
+package de.uni_paderborn.fujaba.umlrt.componentInstanceConfigurationEditor.diagram.custom.part;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint;
+import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
+import org.eclipse.gmf.runtime.diagram.core.services.view.CreateDiagramViewOperation;
+import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+
+import de.uni_paderborn.fujaba.newwizard.diagrams.FujabaDiagramNewWizard;
+import de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.ComponentInstanceConfiguration;
+import de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.componentInstanceConfigurationFactory;
+
+public class CustomComponentInstanceConfigurationCreationWizard extends FujabaDiagramNewWizard {
+
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		super.init(workbench, selection);
+
+		setWindowTitle(getWindowTitle());
+		setDefaultPageImageDescriptor(de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditorPlugin
+				.getBundledImageDescriptor("icons/wizban/NewComponentInstanceConfigurationEditorWizard.gif")); //$NON-NLS-1$
+
+	}
+
+	@Override
+	protected String getDiagramFileExtension() {
+		return "componentInstanceConfiguration_diagram"; //$NON-NLS-1$
+	}
+
+	@Override
+	protected String getModelId() {
+		return de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.edit.parts.ComponentInstanceConfigurationEditPart.MODEL_ID;
+	}
+
+	@Override
+	protected PreferencesHint getDiagramPreferencesHint() {
+		return de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT;
+	}
+
+	@Override
+	protected String getEditorId() {
+		return de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditor.ID;
+	}
+
+	/**
+	 * Create a new instance of domain element associated with canvas.
+	 */
+	@Override
+	protected EObject createInitialModel() {
+		ComponentInstanceConfiguration CICDiagram = componentInstanceConfigurationFactory.eINSTANCE
+				.createComponentInstanceConfiguration();
+
+		return CICDiagram;
+	}
+
+	@Override
+	public boolean isValidDiagramElement(EObject object) {
+		return ViewService
+				.getInstance()
+				.provides(
+						new CreateDiagramViewOperation(
+								new EObjectAdapter(object),
+								getModelId(),
+								getDiagramPreferencesHint()));
+	}
+
+}
---
+++ de.uni_paderborn.fujaba.umlrt.componentInstanceConfigurationEditor.diagram.custom/src/de/uni_paderborn/fujaba/umlrt/componentInstanceConfigurationEditor/diagram/custom/part/CustomComponentInstanceConfigurationEditorUtil.java
@@ -0,0 +1,116 @@
+package de.uni_paderborn.fujaba.umlrt.componentInstanceConfigurationEditor.diagram.custom.part;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.operations.OperationHistoryFactory;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
+import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
+import org.eclipse.gmf.runtime.emf.core.GMFEditingDomainFactory;
+import org.eclipse.gmf.runtime.notation.Diagram;
+
+import de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.ComponentInstanceConfiguration;
+import de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.componentInstanceConfigurationFactory;
+
+public class CustomComponentInstanceConfigurationEditorUtil {
+
+	/**
+	 * This method should be called within a workspace modify operation since it
+	 * creates resources.
+	 */
+	public static Resource createDiagram(URI diagramURI, URI modelURI,
+			IProgressMonitor progressMonitor) {
+		TransactionalEditingDomain editingDomain = GMFEditingDomainFactory.INSTANCE
+				.createEditingDomain();
+		progressMonitor
+				.beginTask(
+						de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.Messages.ComponentInstanceConfigurationDiagramEditorUtil_CreateDiagramProgressTask,
+						3);
+		final Resource diagramResource = editingDomain.getResourceSet()
+				.createResource(diagramURI);
+		final Resource modelResource = editingDomain.getResourceSet()
+				.createResource(modelURI);
+		final String diagramName = diagramURI.lastSegment();
+		AbstractTransactionalCommand command = new AbstractTransactionalCommand(
+				editingDomain,
+				de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.Messages.ComponentInstanceConfigurationDiagramEditorUtil_CreateDiagramCommandLabel,
+				Collections.EMPTY_LIST) {
+			protected CommandResult doExecuteWithResult(
+					IProgressMonitor monitor, IAdaptable info)
+					throws ExecutionException {
+				de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.ComponentInstanceConfiguration model = createInitialModel();
+				attachModelToResource(model, modelResource);
+
+				Diagram diagram = ViewService
+						.createDiagram(
+								model,
+								de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.edit.parts.ComponentInstanceConfigurationEditPart.MODEL_ID,
+								de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);
+				if (diagram != null) {
+					diagramResource.getContents().add(diagram);
+					diagram.setName(diagramName);
+					diagram.setElement(model);
+				}
+
+				try {
+					modelResource
+							.save(de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditorUtil
+									.getSaveOptions());
+					diagramResource
+							.save(de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditorUtil
+									.getSaveOptions());
+				} catch (IOException e) {
+
+					de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditorPlugin
+							.getInstance()
+							.logError(
+									"Unable to store model and diagram resources", e); //$NON-NLS-1$
+				}
+				return CommandResult.newOKCommandResult();
+			}
+
+		};
+		try {
+			OperationHistoryFactory.getOperationHistory().execute(command,
+					new SubProgressMonitor(progressMonitor, 1), null);
+		} catch (ExecutionException e) {
+			de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditorPlugin
+					.getInstance().logError(
+							"Unable to create model and diagram", e); //$NON-NLS-1$
+		}
+		de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditorUtil
+				.setCharset(WorkspaceSynchronizer.getFile(modelResource));
+		de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.diagram.part.ComponentInstanceConfigurationDiagramEditorUtil
+				.setCharset(WorkspaceSynchronizer.getFile(diagramResource));
+		return diagramResource;
+	}
+
+	/**
+	 * Create a new instance of domain element associated with canvas.
+	 */
+	private static ComponentInstanceConfiguration createInitialModel() {
+		ComponentInstanceConfiguration CICDiagram = componentInstanceConfigurationFactory.eINSTANCE
+				.createComponentInstanceConfiguration();
+
+		return CICDiagram;
+	}
+
+	/**
+	 * Store model element in the resource.
+	 */
+	private static void attachModelToResource(
+			de.uni_paderborn.fujaba.umlrt.componentInstanceConfiguration.ComponentInstanceConfiguration model,
+			Resource resource) {
+		resource.getContents().add(model);
+	}
+
+}
