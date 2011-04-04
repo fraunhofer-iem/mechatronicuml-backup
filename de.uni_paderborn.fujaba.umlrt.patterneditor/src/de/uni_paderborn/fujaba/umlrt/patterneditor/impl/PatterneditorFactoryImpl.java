b692862831fb91ed2cfebedc47d9e055c3055a59
Denis Braun <braund@mail.upb.de>
2011-04-04 07:51:35 +0000

    added generated code

---
+++ de.uni_paderborn.fujaba.umlrt.patterneditor/src/de/uni_paderborn/fujaba/umlrt/patterneditor/impl/PatterneditorFactoryImpl.java
@@ -0,0 +1,99 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uni_paderborn.fujaba.umlrt.patterneditor.impl;
+
+import de.uni_paderborn.fujaba.umlrt.patterneditor.*;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class PatterneditorFactoryImpl extends EFactoryImpl implements PatterneditorFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static PatterneditorFactory init() {
+		try {
+			PatterneditorFactory thePatterneditorFactory = (PatterneditorFactory)EPackage.Registry.INSTANCE.getEFactory("http:///de/uni_paderborn/fujaba/umlrt/patterneditor"); 
+			if (thePatterneditorFactory != null) {
+				return thePatterneditorFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new PatterneditorFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PatterneditorFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case PatterneditorPackage.PATTERN_DIAGRAM: return createPatternDiagram();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PatternDiagram createPatternDiagram() {
+		PatternDiagramImpl patternDiagram = new PatternDiagramImpl();
+		return patternDiagram;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PatterneditorPackage getPatterneditorPackage() {
+		return (PatterneditorPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static PatterneditorPackage getPackage() {
+		return PatterneditorPackage.eINSTANCE;
+	}
+
+} //PatterneditorFactoryImpl
