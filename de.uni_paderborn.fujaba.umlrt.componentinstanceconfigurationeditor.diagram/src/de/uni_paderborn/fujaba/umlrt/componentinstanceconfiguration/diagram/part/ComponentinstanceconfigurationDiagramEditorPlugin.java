5b2284feef229bd5862e384a7d3bf7fc92b40e0e
Denis Braun <braund@mail.upb.de>
2011-04-14 12:16:02 +0000

    removed unneeded adding of a factory

--- de.uni_paderborn.fujaba.umlrt.componentinstanceconfigurationeditor.diagram/src/de/uni_paderborn/fujaba/umlrt/componentinstanceconfiguration/diagram/part/ComponentinstanceconfigurationDiagramEditorPlugin.java
+++ de.uni_paderborn.fujaba.umlrt.componentinstanceconfigurationeditor.diagram/src/de/uni_paderborn/fujaba/umlrt/componentinstanceconfiguration/diagram/part/ComponentinstanceconfigurationDiagramEditorPlugin.java
@@ -148,8 +148,6 @@ public class ComponentinstanceconfigurationDiagramEditorPlugin extends
 		factories
 				.add(new de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.provider.RealtimestatechartItemProviderAdapterFactory());
 		factories
-				.add(new de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.reconf.provider.ReconfItemProviderAdapterFactory());
-		factories
 				.add(new de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.helper.provider.HelperItemProviderAdapterFactory());
 		factories
 				.add(new de.uni_paderborn.fujaba.umlrt.model.msgiface.provider.MsgifaceItemProviderAdapterFactory());
