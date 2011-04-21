f41b8c7a7bdff7c8633c8a17973211e8d33a38d4
Denis Braun <braund@mail.upb.de>
2011-04-21 15:01:32 +0000

    added missing methods

--- de.uni_paderborn.fujaba.umlrt.model/src/de/uni_paderborn/fujaba/umlrt/model/realtimestatechart/impl/SynchronizationChannelImpl.java
+++ de.uni_paderborn.fujaba.umlrt.model/src/de/uni_paderborn/fujaba/umlrt/model/realtimestatechart/impl/SynchronizationChannelImpl.java
@@ -6,26 +6,28 @@
  */
 package de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.impl;
 
-import de.uni_paderborn.fujaba.umlrt.model.core.AbstractStatechart;
-import java.lang.reflect.InvocationTargetException;
+import de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.RealtimestatechartPackage;
+import de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.State;
+import de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.SynchronizationChannel;
 
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.common.util.EList;
+
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EParameter;
 import org.eclipse.emf.ecore.InternalEObject;
+
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
 import org.eclipse.emf.ecore.util.EcoreUtil;
+
 import org.storydriven.modeling.CommentableElement;
 import org.storydriven.modeling.SDMPackage;
-import org.storydriven.modeling.calls.Callable;
-import org.storydriven.modeling.impl.NamedElementImpl;
 
-import de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.RealtimestatechartPackage;
-import de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.State;
-import de.uni_paderborn.fujaba.umlrt.model.realtimestatechart.SynchronizationChannel;
+import org.storydriven.modeling.calls.Callable;
+
+import org.storydriven.modeling.impl.NamedElementImpl;
 
 /**
  * <!-- begin-user-doc -->
