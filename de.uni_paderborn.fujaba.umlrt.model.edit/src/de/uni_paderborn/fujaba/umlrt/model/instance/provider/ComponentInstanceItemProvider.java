0382806836b24a6feadaa371c120af8d8c7b0b03
Ingo Budde <bingo@mail.upb.de>
2011-05-05 15:01:58 +0000

    Modified edit code to hide the Property "ComponentInstance.componentPart" from the Properties View.

--- de.uni_paderborn.fujaba.umlrt.model.edit/src/de/uni_paderborn/fujaba/umlrt/model/instance/provider/ComponentInstanceItemProvider.java
+++ de.uni_paderborn.fujaba.umlrt.model.edit/src/de/uni_paderborn/fujaba/umlrt/model/instance/provider/ComponentInstanceItemProvider.java
@@ -73,7 +73,6 @@ public class ComponentInstanceItemProvider
 			super.getPropertyDescriptors(object);
 
 			addComponentTypePropertyDescriptor(object);
-			addComponentPartPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}
@@ -92,28 +91,6 @@ public class ComponentInstanceItemProvider
 				 getString("_UI_ComponentInstance_componentType_feature"),
 				 getString("_UI_PropertyDescriptor_description", "_UI_ComponentInstance_componentType_feature", "_UI_ComponentInstance_type"),
 				 InstancePackage.Literals.COMPONENT_INSTANCE__COMPONENT_TYPE,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
-	}
-
-	/**
-	 * This adds a property descriptor for the Component Part feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	protected void addComponentPartPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_ComponentInstance_componentPart_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_ComponentInstance_componentPart_feature", "_UI_ComponentInstance_type"),
-				 InstancePackage.Literals.COMPONENT_INSTANCE__COMPONENT_PART,
 				 true,
 				 false,
 				 true,
