c61d102bce480581881705f1a3226130a4569a57
Denis Braun <braund@mail.upb.de>
2011-05-16 17:21:10 +0000

    added further adapter

--- de.uni_paderborn.fujaba.umlrt.model/src/de/uni_paderborn/fujaba/umlrt/model/realtimestatechart/impl/ClockConstraintImpl.java
+++ de.uni_paderborn.fujaba.umlrt.model/src/de/uni_paderborn/fujaba/umlrt/model/realtimestatechart/impl/ClockConstraintImpl.java
@@ -101,19 +101,15 @@ public class ClockConstraintImpl extends EObjectImpl implements ClockConstraint 
 	 */
 	protected ClockConstraintImpl() {
 		super();
-		
-		// Install a notification adapter that informs the
-		// clockConstraintExpr-reference, whenever one of the dependent features
-		// was modified
+
 		DerivedAttributeAdapter clockConstraintExprAdapter1 = new DerivedAttributeAdapter(this, RealtimestatechartPackage.Literals.CLOCK_CONSTRAINT__CLOCK_CONSTRAINT_EXPR, false);
 		clockConstraintExprAdapter1.addLocalDependency(RealtimestatechartPackage.Literals.CLOCK_CONSTRAINT__BOUND);
-		
-		// Install a notification adapter that informs the
-		// clockConstraintExpr-reference, whenever one of the dependent features
-		// was modified
+
 		DerivedAttributeAdapter clockConstraintExprAdapter2 = new DerivedAttributeAdapter(this, RealtimestatechartPackage.Literals.CLOCK_CONSTRAINT__CLOCK_CONSTRAINT_EXPR, false);
 		clockConstraintExprAdapter2.addLocalDependency(RealtimestatechartPackage.Literals.CLOCK_CONSTRAINT__OPERATOR);
 
+		DerivedAttributeAdapter clockConstraintExprAdapter3 = new DerivedAttributeAdapter(this, RealtimestatechartPackage.Literals.CLOCK_CONSTRAINT__CLOCK_CONSTRAINT_EXPR, false);
+		clockConstraintExprAdapter3.addLocalDependency(RealtimestatechartPackage.Literals.CLOCK_CONSTRAINT__CLOCK);
 	}
 
 	/**
