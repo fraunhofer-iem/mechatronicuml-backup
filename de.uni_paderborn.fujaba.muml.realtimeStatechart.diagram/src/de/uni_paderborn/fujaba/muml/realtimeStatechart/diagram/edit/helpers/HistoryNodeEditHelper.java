c85fc2c3a3b829b548e47eb1df0ebb5bcfc1f635
Ingo Budde <bingo@mail.upb.de>
2011-06-24 11:10:55 +0000

    These files were not committed in #546, probably because these are deleted files.

--- de.uni_paderborn.fujaba.muml.realtimeStatechart.diagram/src/de/uni_paderborn/fujaba/muml/realtimeStatechart/diagram/edit/helpers/HistoryNodeEditHelper.java
+++
@@ -1,9 +0,0 @@
-package de.uni_paderborn.fujaba.muml.realtimeStatechart.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class HistoryNodeEditHelper
-		extends
-		de.uni_paderborn.fujaba.muml.realtimeStatechart.diagram.edit.helpers.MumlBaseEditHelper {
-}
