1c9f4f15ae0a6d0379147b3f45a6edcc993f25c7
Stefan Dziwok <xell@upb.de>
2011-07-25 22:27:55 +0000

    forgot some files
    and forgot to mention new ocl constraint:
    - class MessageInterface: NoMessageTypeOrNotAtLeastTwoGeneralizations
    self.messageTypes->size()>=1 or self.superType->size()>=2

--- de.uni_paderborn.fujaba.muml.model.edit/src/de/uni_paderborn/fujaba/muml/model/realtimestatechart/provider/TransitionItemProvider.java
+++ de.uni_paderborn.fujaba.muml.model.edit/src/de/uni_paderborn/fujaba/muml/model/realtimestatechart/provider/TransitionItemProvider.java
@@ -762,6 +762,11 @@ public class TransitionItemProvider
 
 		newChildDescriptors.add
 			(createChildParameter
+				(SDMPackage.Literals.EXTENDABLE_ELEMENT__ANNOTATION,
+				 EcoreFactory.eINSTANCE.createEAnnotation()));
+
+		newChildDescriptors.add
+			(createChildParameter
 				(SDMPackage.Literals.EXTENDABLE_ELEMENT__EXTENSION,
 				 ActivitiesFactory.eINSTANCE.createOperationExtension()));
 
