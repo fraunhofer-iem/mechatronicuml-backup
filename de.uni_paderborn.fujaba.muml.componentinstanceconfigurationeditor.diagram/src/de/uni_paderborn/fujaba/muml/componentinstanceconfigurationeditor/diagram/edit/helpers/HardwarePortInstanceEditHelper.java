d1727cd3047870d614138dcfb09e5de3b41faaa9
Ingo Budde <bingo@mail.upb.de>
2011-12-02 18:31:11 +0000

    Fix for #194: ComponentInstances always show "portinstance" as name

--- de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram/src/de/uni_paderborn/fujaba/muml/componentinstanceconfigurationeditor/diagram/edit/helpers/HardwarePortInstanceEditHelper.java
+++
@@ -1,9 +0,0 @@
-package de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class HardwarePortInstanceEditHelper
-		extends
-		de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.edit.helpers.MumlBaseEditHelper {
-}
--- de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram/src/de/uni_paderborn/fujaba/muml/componentinstanceconfigurationeditor/diagram/edit/helpers/PortInstanceEditHelper.java
+++
@@ -1,9 +0,0 @@
-package de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class PortInstanceEditHelper
-		extends
-		de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.edit.helpers.MumlBaseEditHelper {
-}
