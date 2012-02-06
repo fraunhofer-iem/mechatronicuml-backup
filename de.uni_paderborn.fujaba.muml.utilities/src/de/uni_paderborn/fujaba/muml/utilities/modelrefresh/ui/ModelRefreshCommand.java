e561e638f5400b2750d82b5f97152de25183a343
Ingo Budde <bingo@mail.upb.de>
2012-02-06 14:50:38 +0000

    Sharing de.uni_paderborn.fujaba.muml.utilities, which contains useful utilities that make using Fujaba easier (for example migration tools).

---
+++ de.uni_paderborn.fujaba.muml.utilities/src/de/uni_paderborn/fujaba/muml/utilities/modelrefresh/ui/ModelRefreshCommand.java
@@ -0,0 +1,27 @@
+package de.uni_paderborn.fujaba.muml.utilities.modelrefresh.ui;
+
+import java.util.Collections;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import de.uni_paderborn.fujaba.muml.utilities.modelrefresh.Refresher;
+
+public class ModelRefreshCommand extends AbstractHandler {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		if (selection instanceof IStructuredSelection && !selection.isEmpty()) {
+			IStructuredSelection ssel = (IStructuredSelection) selection;
+			new Refresher(Collections.emptyMap(), Collections.emptyMap()).run(ssel.toList());
+		}
+		return null;
+	}
+
+}
