0ece595251b846b3cb5dffcbe8396189162c824f
Uwe Pohlmann <upohl>
2012-03-07 12:27:41 +0000

    added new tag for real-time tool suite version which running on eclipse 3.6 helios
    it depends on the sdm plugins
    -de.fujaba.modelinstance 272
    -de.fujaba.modelinstance.edit 271
    -de.fujaba.modelinstance.edit.eef 284
    -de.fujaba.modelinstance.editor 1033
    -de.fujaba.newwizard 306
    -org.storydriven.modeling 299
    -org.storydriven.modeling.edit 299
    -org.storydriven.modeling.edit.eef 305

---
+++ de.uni_paderborn.fujaba.common/src/de/uni_paderborn/fujaba/common/emf/edit/ui/ExtensibleCreationDialog.java
@@ -0,0 +1,284 @@
+package de.uni_paderborn.fujaba.common.emf.edit.ui;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.ui.EMFEditUIPlugin;
+import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.viewers.IContentProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+import de.uni_paderborn.fujaba.common.emf.edit.ui.extensions.IDialogExtension;
+
+/**
+ * 
+ * @author bingo
+ * 
+ */
+public class ExtensibleCreationDialog extends Dialog {
+
+	public final static String EXTENSION_GROUP_ALWAYS_VISIBLE = "always visible";
+	public final static String EXTENSION_GROUP_DEFAULT = "default";
+	public final static String EXTENSION_GROUP_XTEXT_PARSER = "XTextParser";
+
+	private Composite container;
+
+	private IRefreshProhibitedPropertySection mainPropertySection;
+
+	@Override
+	protected Button createButton(Composite parent, int id, String label,
+			boolean defaultButton) {
+		Button button = super.createButton(parent, id, label, defaultButton);
+		if (id == IDialogConstants.CANCEL_ID) {
+			button.setEnabled(false);
+		}
+		return button;
+	}
+
+	private Map<Object, List<IDialogExtension>> groupToExtensions = new LinkedHashMap<Object, List<IDialogExtension>>();
+
+	/**
+	 * The StructuralFeature that we set the values for.
+	 */
+	private EStructuralFeature structuralFeature;
+
+	/**
+	 * The initial size for this dialog.
+	 */
+	private static final Point INITIAL_DIALOG_SIZE = new Point(550, 500);
+
+	/**
+	 * The LabelProvider used to get Labels for Parameters and typeClassifiers.
+	 */
+	private ILabelProvider labelProvider;
+
+	/**
+	 * The ContentProvider to access items of an ItemProvider.
+	 */
+	private IContentProvider contentProvider;
+
+	/**
+	 * The Object containing the StructuralFeature, we are setting the values
+	 * for.
+	 */
+	private EObject containerObject;
+
+	private AdapterFactory adapterFactory;
+
+	/**
+	 * Constructs this ExtensibleCreationDialog.
+	 * 
+	 * @param parentShell
+	 *            The parent shell to use for this Dialog.
+	 * @param labelProvider
+	 *            The LabelProvider to use in order to create element names.
+	 * @param containerObject
+	 *            The object containing the <code>structuralFeature</code>.
+	 * @param structuralFeature
+	 *            The StructuralFeature to set values for.
+	 * @param adapterFactory
+	 *            The adapter factory for content providers, item providers and
+	 *            ItemPropertyDescriptors.
+	 * 
+	 */
+	public ExtensibleCreationDialog(Shell parentShell,
+			ILabelProvider labelProvider, EObject containerObject,
+			EStructuralFeature structuralFeature,
+			AdapterFactory adapterFactory,
+			IRefreshProhibitedPropertySection mainPropertySection) {
+		super(parentShell);
+		setShellStyle(getShellStyle() | SWT.RESIZE | SWT.MAX);
+		this.labelProvider = labelProvider;
+		this.structuralFeature = structuralFeature;
+		this.containerObject = containerObject;
+		this.mainPropertySection = mainPropertySection;
+		this.adapterFactory = adapterFactory;
+
+		contentProvider = new AdapterFactoryContentProvider(adapterFactory);
+	}
+
+	public void addExtension(IDialogExtension extension, Object group) {
+		getExtensionsInGroup(group).add(extension);
+	}
+
+	public List<IDialogExtension> getExtensionsInGroup(Object group) {
+		List<IDialogExtension> extensions = groupToExtensions.get(group);
+		if (extensions == null) {
+			extensions = new ArrayList<IDialogExtension>();
+			groupToExtensions.put(group, extensions);
+		}
+		return extensions;
+	}
+
+	public Collection<Object> getRegisteredGroups() {
+		return groupToExtensions.keySet();
+	}
+
+	
+	public Map<Object, List<IDialogExtension>> getGroupMap() {
+		return groupToExtensions;
+	}
+
+	@Override
+	protected void configureShell(Shell shell) {
+		super.configureShell(shell);
+		shell.setText(EMFEditUIPlugin.INSTANCE.getString(
+				"_UI_FeatureEditorDialog_title",
+				new Object[] { structuralFeature.getName(),
+						labelProvider.getText(containerObject) }));
+		shell.setImage(labelProvider.getImage(containerObject));
+	}
+
+	public IItemPropertyDescriptor getItemPropertyDescriptor() {
+		IItemPropertySource ips = (IItemPropertySource) adapterFactory.adapt(
+				containerObject, IItemPropertySource.class);
+		if (ips != null) {
+			return ips
+					.getPropertyDescriptor(containerObject, structuralFeature);
+		}
+		return null;
+	}
+
+	/**
+	 * Create contents of the dialog window.
+	 * 
+	 * @param parent
+	 *            the parent composite to contain the dialog area.
+	 * @return The dialog area.
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		container = (Composite) super.createDialogArea(parent);
+		container.setLayout(new GridLayout(2, false));
+
+		for (List<IDialogExtension> extensions : groupToExtensions.values()) {
+			for (IDialogExtension extension : extensions) {
+				Composite mainArea = new Composite(container, SWT.NONE);
+				GridLayout gridLayoutMainArea = new GridLayout(1, false);
+				mainArea.setLayout(gridLayoutMainArea);
+
+				Composite buttonArea = new Composite(container, SWT.NONE);
+				GridLayout gridLayoutButtonArea = new GridLayout(1, false);
+				GridData gridDataButtonArea = new GridData(SWT.FILL, SWT.FILL,
+						false, false, 1, 1);
+				gridLayoutButtonArea.marginTop = 7;
+				buttonArea.setLayout(gridLayoutButtonArea);
+				buttonArea.setLayoutData(gridDataButtonArea);
+
+				extension.createMainArea(mainArea);
+				extension.createButtonArea(buttonArea);
+			}
+		}
+
+		for (List<IDialogExtension> extensions : groupToExtensions.values()) {
+			for (IDialogExtension extension : extensions) {
+				extension.initialize();
+			}
+		}
+
+		return container;
+	}
+
+	@Override
+	protected Point getInitialSize() {
+		return INITIAL_DIALOG_SIZE;
+	}
+
+	// Begin removed because of #204
+	// @Override
+	// protected void okPressed() {
+	// for (IDialogExtension extension : extensions) {
+	// extension.okPressed();
+	// }
+	// super.okPressed();
+	// }
+	// End removed
+
+	@Override
+	public boolean close() {
+		// Execute everything within a try-catch block to prevent problems to
+		// close the window.
+		try {
+			// Begin added because of #204
+			for (List<IDialogExtension> extensions : groupToExtensions.values()) {
+				for (IDialogExtension extension : extensions) {
+					extension.okPressed();
+				}
+			}
+
+			// Hack to force notification (the real result will be set
+			// afterwards)
+			getItemPropertyDescriptor().setPropertyValue(containerObject,
+					Collections.EMPTY_LIST);
+			// End added
+
+			for (List<IDialogExtension> extensions : groupToExtensions.values()) {
+				for (IDialogExtension extension : extensions) {
+					extension.dispose();
+				}
+			}
+			contentProvider.dispose();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return super.close();
+	}
+
+	// We must forbid calling refresh on the main Property Section, while the
+	// dialog is open (part of the fix of the Widget-Disposed bug).
+	@Override
+	public int open() {
+		mainPropertySection.pushRefreshProhibition();
+		int result = super.open();
+		mainPropertySection.popRefreshProhibition();
+		return result;
+	}
+
+	public ILabelProvider getLabelProvider() {
+		return labelProvider;
+	}
+
+	public IContentProvider getContentProvider() {
+		return contentProvider;
+	}
+
+	public EStructuralFeature getStructuralFeature() {
+		return structuralFeature;
+	}
+
+	public EObject getContainerObject() {
+		return containerObject;
+	}
+
+	public void setPropertyValue(Object value) {
+		IItemPropertyDescriptor itemPropertyDescriptor = getItemPropertyDescriptor();
+		if (itemPropertyDescriptor != null) {
+			itemPropertyDescriptor.setPropertyValue(containerObject, value);
+		}
+	}
+	
+	public void layout() {
+		container.layout(false);
+	}
+
+}
---
+++ de.uni_paderborn.fujaba.common/src/de/uni_paderborn/fujaba/common/emf/edit/ui/extensions/ObjectCreationDialogExtension.java
@@ -0,0 +1,159 @@
+package de.uni_paderborn.fujaba.common.emf.edit.ui.extensions;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.ItemProvider;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+import de.uni_paderborn.fujaba.common.emf.edit.ui.ExtensibleCreationDialog;
+
+public class ObjectCreationDialogExtension extends AbstractDialogExtension {
+
+	private ObjectsListDialogExtension objectsListDialogExtension;
+
+	private ItemProvider values;
+
+	private ComboViewer comboViewer;
+
+	private AdapterFactory adapterFactory;
+
+	private List<EClass> instanceClasses = new ArrayList<EClass>();
+
+	/**
+	 * The Create-Button.
+	 */
+	private Button btnCreate;
+
+	public ObjectCreationDialogExtension(
+			ExtensibleCreationDialog creationDialog,
+			AdapterFactory adapterFactory) {
+		super(creationDialog);
+		this.adapterFactory = adapterFactory;
+
+		EStructuralFeature structuralFeature = creationDialog
+				.getStructuralFeature();
+		Assert.isLegal(structuralFeature.getEType() instanceof EClass);
+
+		EClass type = (EClass) structuralFeature.getEType();
+
+		IItemPropertySource ips = (IItemPropertySource) adapterFactory.adapt(
+				structuralFeature, IItemPropertySource.class);
+
+		if (ips != null) {
+			IItemPropertyDescriptor itemPropertyDescriptor = ips
+					.getPropertyDescriptor(
+							structuralFeature,
+							org.eclipse.emf.ecore.EcorePackage.Literals.ETYPED_ELEMENT__ETYPE);
+			Collection<?> choices = itemPropertyDescriptor
+					.getChoiceOfValues(structuralFeature);
+			for (Object choice : choices) {
+				org.eclipse.emf.ecore.EClassifier classifier = (org.eclipse.emf.ecore.EClassifier) choice;
+				if (classifier instanceof EClass
+						&& type.isSuperTypeOf((EClass) classifier)
+						&& !((EClass) classifier).isAbstract()) {
+					instanceClasses.add((EClass) classifier);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public void initialize() {
+
+	}
+
+	@Override
+	public void createMainArea(Composite parent) {
+		super.createMainArea(parent);
+		values = new ItemProvider(adapterFactory, instanceClasses);
+		comboViewer = new ComboViewer(parent, SWT.NONE);
+
+		comboViewer
+				.setContentProvider(getCreationDialog().getContentProvider());
+		comboViewer.setLabelProvider(new LabelProvider() {
+
+			@Override
+			public String getText(Object element) {
+				if (element instanceof org.eclipse.emf.ecore.ENamedElement) {
+					return ((org.eclipse.emf.ecore.ENamedElement) element)
+							.getName();
+				}
+				return super.getText(element);
+			}
+
+		});
+		comboViewer.setInput(values);
+
+		// Select first instanceClass
+		if (values.hasChildren()) {
+			comboViewer.setSelection(new StructuredSelection(
+					new Object[] { values.getChildren().get(0) }));
+		}
+	}
+
+	@Override
+	public void createButtonArea(Composite composite) {
+		super.createButtonArea(composite);
+		btnCreate = new Button(composite, SWT.NONE);
+		btnCreate.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
+				1, 1));
+		btnCreate.setText("&Create");
+
+		btnCreate.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent event) {
+
+				final EObject newObject = EcoreUtil.create(getInstanceClass());
+				
+				objectsListDialogExtension.addListItem(newObject);
+				objectsListDialogExtension.getTableViewer().refresh();
+				objectsListDialogExtension.getTableViewer().setSelection(
+						new StructuredSelection(new Object[] { newObject }));
+			}
+		});
+	}
+
+	protected EClass getInstanceClass() {
+		ISelection selection = comboViewer.getSelection();
+		if (selection instanceof IStructuredSelection) {
+			return (EClass) ((IStructuredSelection) selection)
+					.getFirstElement();
+		}
+		return null;
+	}
+
+	@Override
+	public void okPressed() {
+	}
+
+	public void setObjectsListDialogExtension(
+			ObjectsListDialogExtension objectsListDialogExtension) {
+		this.objectsListDialogExtension = objectsListDialogExtension;
+	}
+
+}
