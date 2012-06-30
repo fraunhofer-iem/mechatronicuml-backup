63058948449e481d40f83fbbe61b834d8e7b9d14
Ingo Budde <bingo@mail.upb.de>
2012-06-30 07:12:01 +0000

    Realtimestatechart-Editor: Transition priority label is updated after priority was changed in the model (fixes #243).

--- de.uni_paderborn.fujaba.muml.realtimeStatechart.diagram.custom/src/de/uni_paderborn/fujaba/muml/realtimeStatechart/diagram/custom/edit/parts/CustomTransitionEditPart.java
+++ de.uni_paderborn.fujaba.muml.realtimeStatechart.diagram.custom/src/de/uni_paderborn/fujaba/muml/realtimeStatechart/diagram/custom/edit/parts/CustomTransitionEditPart.java
@@ -3,11 +3,9 @@ package de.uni_paderborn.fujaba.muml.realtimeStatechart.diagram.custom.edit.part
 import org.eclipse.draw2d.Connection;
 import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.gmf.runtime.notation.View;
 
 import de.uni_paderborn.fujaba.muml.common.figures.TransitionPriorityDecoration;
-import de.uni_paderborn.fujaba.muml.model.realtimestatechart.Prioritizable;
 import de.uni_paderborn.fujaba.muml.model.realtimestatechart.RealtimestatechartPackage;
 import de.uni_paderborn.fujaba.muml.model.realtimestatechart.Transition;
 import de.uni_paderborn.fujaba.muml.realtimeStatechart.diagram.edit.parts.TransitionEditPart;
@@ -22,14 +20,10 @@ public class CustomTransitionEditPart extends TransitionEditPart {
 	protected void handleNotificationEvent(Notification notification) {
 		Transition transition = (Transition) getNotationView().getElement();
 		Object feature = notification.getFeature();
-		if (feature instanceof EStructuralFeature) {
-			EStructuralFeature structuralFeature = (EStructuralFeature) feature;
-			if (structuralFeature.getContainerClass() == Prioritizable.class) {
-				int featureID = notification.getFeatureID(Prioritizable.class);
-				if (featureID == RealtimestatechartPackage.TRANSITION__PRIORITY) {
-					((CustomTransitionFigure) getFigure()).setPriority(transition.getPriority());
-				}
-			}
+		if (RealtimestatechartPackage.Literals.PRIORITIZABLE__PRIORITY
+				.equals(feature)) {
+			((CustomTransitionFigure) getFigure()).setPriority(transition
+					.getPriority());
 		}
 		super.handleNotificationEvent(notification);
 	}
@@ -40,22 +34,21 @@ public class CustomTransitionEditPart extends TransitionEditPart {
 		transitionFigure.setPriority(transition.getPriority());
 		return transitionFigure;
 	}
-	
+
 	public class CustomTransitionFigure extends TransitionFigure {
 
 		public CustomTransitionFigure() {
 			super();
 			getFigureTransitionDeadlineLabelFigure().setTextWrap(true);
-			getFigureTransitionDeadlineLabelFigure().setTextJustification(PositionConstants.RIGHT);
+			getFigureTransitionDeadlineLabelFigure().setTextJustification(
+					PositionConstants.RIGHT);
 		}
 
 		public void setPriority(int priority) {
 			TransitionPriorityDecoration priorityDecoration = (TransitionPriorityDecoration) getSourceDecoration();
 			priorityDecoration.setPriority(priority);
 		}
-		
+
 	}
-	
-	
 
 }
