5c53486f0e3b99641f0e5d85153730728dbd50a0
Ingo Budde <bingo@mail.upb.de>
2012-07-02 12:57:08 +0000

    Component Instances can be directly edited again.

---
+++ de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom/src/de/uni_paderborn/fujaba/muml/componentinstanceconfigurationeditor/diagram/custom/parsers/CustomAtomicComponentInstanceLabelExpressionLabelParser5027.java
@@ -0,0 +1,19 @@
+package de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom.parsers;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.ecore.EObject;
+
+import de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.parsers.AtomicComponentInstanceLabelExpressionLabelParser5027;
+import de.uni_paderborn.fujaba.muml.model.instance.ComponentInstance;
+
+public class CustomAtomicComponentInstanceLabelExpressionLabelParser5027 extends
+		AtomicComponentInstanceLabelExpressionLabelParser5027 {
+	@Override
+	protected IStatus updateValues(EObject target, String newString)
+			throws ExecutionException {
+		((ComponentInstance) target).setName(newString);
+		return Status.OK_STATUS;
+	}
+}
--- de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom/src/de/uni_paderborn/fujaba/muml/componentinstanceconfigurationeditor/diagram/custom/providers/CustomComponentinstanceconfigurationParserProvider.java
+++ de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom/src/de/uni_paderborn/fujaba/muml/componentinstanceconfigurationeditor/diagram/custom/providers/CustomComponentinstanceconfigurationParserProvider.java
@@ -2,18 +2,43 @@ package de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagra
 
 import org.eclipse.gmf.runtime.common.ui.services.parser.IParser;
 
+import de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom.parsers.CustomStructuredComponentInstanceLabelExpressionLabelParser5029;
 import de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.providers.MumlParserProvider;
 
 public class CustomComponentinstanceconfigurationParserProvider extends
 		MumlParserProvider {
-//
-//	private de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom.parsers.CustomComponentInstanceLabelExpressionLabelParser5018 componentInstanceLabel_5018Parser;
-//
-//	protected IParser getComponentInstanceLabel_5002Parser() {
-//		if (componentInstanceLabel_5018Parser == null) {
-//			componentInstanceLabel_5018Parser = new de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom.parsers.CustomComponentInstanceLabelExpressionLabelParser5018();
-//		}
-//		return componentInstanceLabel_5018Parser;
-//	}
+
+	@Override
+	protected IParser getAtomicComponentInstanceLabel_5027Parser() {
+		if (atomicComponentInstanceLabel_5027Parser == null) {
+			atomicComponentInstanceLabel_5027Parser = new de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom.parsers.CustomAtomicComponentInstanceLabelExpressionLabelParser5027();
+		}
+		return atomicComponentInstanceLabel_5027Parser;
+	}
+
+	@Override
+	protected IParser getStructuredComponentInstanceLabel_5026Parser() {
+		if (structuredComponentInstanceLabel_5026Parser == null) {
+			structuredComponentInstanceLabel_5026Parser = new de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom.parsers.CustomStructuredComponentInstanceLabelExpressionLabelParser5026();
+		}
+		return structuredComponentInstanceLabel_5026Parser;
+	}
+
+	@Override
+	protected IParser getAtomicComponentInstanceLabel_5028Parser() {
+		if (atomicComponentInstanceLabel_5028Parser == null) {
+			atomicComponentInstanceLabel_5028Parser = new de.uni_paderborn.fujaba.muml.componentinstanceconfigurationeditor.diagram.custom.parsers.CustomAtomicComponentInstanceLabelExpressionLabelParser5028();
+		}
+		return atomicComponentInstanceLabel_5028Parser;
+	}
+
+	@Override
+	protected IParser getStructuredComponentInstanceLabel_5029Parser() {
+		if (structuredComponentInstanceLabel_5029Parser == null) {
+			structuredComponentInstanceLabel_5029Parser = new CustomStructuredComponentInstanceLabelExpressionLabelParser5029();
+		}
+		return structuredComponentInstanceLabel_5029Parser;
+	}
+
 
 }
