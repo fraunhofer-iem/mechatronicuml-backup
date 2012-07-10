862f4b10f4b2f2695e9915552b77324e70b45117
Uwe Pohlmann <upohl>
2012-07-10 09:21:43 +0000

    removed failure with missing rule INT in action language. added ';' after do while in action language removed valueType of literalexpression removed EXP in arithmeticOperator added UNSET in assignmentOperator added UNSET in INCREMENTDecrementOperator removed OCL Constraint which enforce that a assignment must have INCREMENTDecrementOperator xor assignmentOperator because default enum is always set

--- de.uni_paderborn.fujaba.muml.model.actionLanguage.edit/src/de/uni_paderborn/fujaba/muml/model/actionLanguage/provider/LiteralExpressionItemProvider.java
+++ de.uni_paderborn.fujaba.muml.model.actionLanguage.edit/src/de/uni_paderborn/fujaba/muml/model/actionLanguage/provider/LiteralExpressionItemProvider.java
@@ -66,7 +66,6 @@ public class LiteralExpressionItemProvider
 			super.getPropertyDescriptors(object);
 
 			addValuePropertyDescriptor(object);
-			addValueTypePropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}
@@ -89,28 +88,6 @@ public class LiteralExpressionItemProvider
 				 false,
 				 false,
 				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
-	}
-
-	/**
-	 * This adds a property descriptor for the Value Type feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	protected void addValueTypePropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_LiteralExpression_valueType_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_LiteralExpression_valueType_feature", "_UI_LiteralExpression_type"),
-				 ActionLanguagePackage.Literals.LITERAL_EXPRESSION__VALUE_TYPE,
-				 true,
-				 false,
-				 true,
-				 null,
 				 null,
 				 null));
 	}
