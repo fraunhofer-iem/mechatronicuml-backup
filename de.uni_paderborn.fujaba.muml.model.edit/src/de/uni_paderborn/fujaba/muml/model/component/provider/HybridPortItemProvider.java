d7ca1772fd3475c15e964c7956fe2ffef8979175
Christian Heinzemann <chris227@upb.de>
2012-07-17 15:23:04 +0000

    added attribute samplingInterval to class hybrid port, added and extended documentation in several classes

--- de.uni_paderborn.fujaba.muml.model.edit/src/de/uni_paderborn/fujaba/muml/model/component/provider/HybridPortItemProvider.java
+++ de.uni_paderborn.fujaba.muml.model.edit/src/de/uni_paderborn/fujaba/muml/model/component/provider/HybridPortItemProvider.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -24,6 +25,7 @@ import org.eclipse.emf.edit.provider.ViewerNotification;
 
 import de.uni_paderborn.fujaba.muml.model.component.ComponentPackage;
 import de.uni_paderborn.fujaba.muml.model.component.HybridPort;
+import de.uni_paderborn.fujaba.muml.model.core.CoreFactory;
 
 /**
  * This is the item provider adapter for a {@link de.uni_paderborn.fujaba.muml.model.component.HybridPort} object.
@@ -202,6 +204,36 @@ public class HybridPortItemProvider
 				 null));
 	}
 
+	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(ComponentPackage.Literals.HYBRID_PORT__SAMPLING_INTERVAL);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
+	}
+
 	protected void addSenderMessageInterfacePropertyDescriptor(Object object) {
 		// do nothing (bug #222)
 	}
@@ -254,6 +286,9 @@ public class HybridPortItemProvider
 			case ComponentPackage.HYBRID_PORT__IS_HYBRID_OUT_PORT:
 				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 				return;
+			case ComponentPackage.HYBRID_PORT__SAMPLING_INTERVAL:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				return;
 		}
 		super.notifyChanged(notification);
 	}
@@ -268,6 +303,11 @@ public class HybridPortItemProvider
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(ComponentPackage.Literals.HYBRID_PORT__SAMPLING_INTERVAL,
+				 CoreFactory.eINSTANCE.createNaturalNumber()));
 	}
 
 }
