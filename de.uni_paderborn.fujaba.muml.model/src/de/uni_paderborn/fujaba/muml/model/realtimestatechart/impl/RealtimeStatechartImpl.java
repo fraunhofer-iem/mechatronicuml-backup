4faf3cacbfcd1013a9f34dc29d0c22a4d5320fb2
Christian Heinzemann <chris227@upb.de>
2012-12-18 08:20:56 +0000

    fixed compile error in muml.model after Stefan's changes

--- de.uni_paderborn.fujaba.muml.model/src/de/uni_paderborn/fujaba/muml/model/realtimestatechart/impl/RealtimeStatechartImpl.java
+++ de.uni_paderborn.fujaba.muml.model/src/de/uni_paderborn/fujaba/muml/model/realtimestatechart/impl/RealtimeStatechartImpl.java
@@ -33,6 +33,7 @@ import de.uni_paderborn.fujaba.muml.model.core.BehavioralElement;
 import de.uni_paderborn.fujaba.muml.model.core.CorePackage;
 import de.uni_paderborn.fujaba.muml.model.core.Operation;
 import de.uni_paderborn.fujaba.muml.model.core.Variable;
+import de.uni_paderborn.fujaba.muml.model.protocol.Role;
 import de.uni_paderborn.fujaba.muml.model.realtimestatechart.Clock;
 import de.uni_paderborn.fujaba.muml.model.realtimestatechart.RealtimeStatechart;
 import de.uni_paderborn.fujaba.muml.model.realtimestatechart.RealtimestatechartPackage;
