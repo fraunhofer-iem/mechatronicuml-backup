d640027b547055ba9374b2b673e29e56b901798f
Christian Heinzemann <chris227@upb.de>
2013-01-18 14:04:39 +0000

    tagged release version 0.3.2 from my local working copy

---
+++ de.uni_paderborn.fujaba.muml.atomiccomponenteditor.diagram/src/de/uni_paderborn/fujaba/muml/atomiccomponenteditor/diagram/part/MumlUriEditorInputTester.java
@@ -0,0 +1,23 @@
+package de.uni_paderborn.fujaba.muml.atomiccomponenteditor.diagram.part;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.emf.common.ui.URIEditorInput;
+
+/**
+ * @generated
+ */
+public class MumlUriEditorInputTester extends PropertyTester {
+
+	/**
+	 * @generated
+	 */
+	public boolean test(Object receiver, String method, Object[] args,
+			Object expectedValue) {
+		if (false == receiver instanceof URIEditorInput) {
+			return false;
+		}
+		URIEditorInput editorInput = (URIEditorInput) receiver;
+		return "atomiccomponent_diagram".equals(editorInput.getURI().fileExtension()); //$NON-NLS-1$
+	}
+
+}
