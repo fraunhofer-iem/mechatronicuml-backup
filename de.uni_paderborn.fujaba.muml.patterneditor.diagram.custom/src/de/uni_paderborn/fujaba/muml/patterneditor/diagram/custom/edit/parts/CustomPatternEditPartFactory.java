bdbfcb2b3cfa53903efc117721821c7d38e21c8b
Christian Heinzemann <chris227@upb.de>
2013-01-18 14:05:43 +0000

    tagged release version 0.3.2 from my local working copy

---
+++ de.uni_paderborn.fujaba.muml.patterneditor.diagram.custom/src/de/uni_paderborn/fujaba/muml/patterneditor/diagram/custom/edit/parts/CustomPatternEditPartFactory.java
@@ -0,0 +1,40 @@
+package de.uni_paderborn.fujaba.muml.patterneditor.diagram.custom.edit.parts;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.notation.View;
+
+import de.uni_paderborn.fujaba.muml.patterneditor.diagram.edit.parts.CoordinationPattern2EditPart;
+import de.uni_paderborn.fujaba.muml.patterneditor.diagram.edit.parts.CoordinationPatternCoordinationPatternContainerCompartmentEditPart;
+import de.uni_paderborn.fujaba.muml.patterneditor.diagram.edit.parts.ModelElementCategoryEditPart;
+import de.uni_paderborn.fujaba.muml.patterneditor.diagram.edit.parts.MumlEditPartFactory;
+import de.uni_paderborn.fujaba.muml.patterneditor.diagram.edit.parts.RoleEditPart;
+import de.uni_paderborn.fujaba.muml.patterneditor.diagram.part.MumlVisualIDRegistry;
+
+/**
+ * Our customized EditPartFactory, which makes sure our overridden
+ * EditPart-Classes are instantiated.
+ * 
+ * @author bingo
+ * 
+ */
+public class CustomPatternEditPartFactory extends MumlEditPartFactory {
+	@Override
+	public EditPart createEditPart(EditPart context, Object model) {
+		if (model instanceof View) {
+			View view = (View) model;
+
+			switch (MumlVisualIDRegistry.getVisualID(view)) {
+			case CoordinationPatternCoordinationPatternContainerCompartmentEditPart.VISUAL_ID:
+				return new CustomCoordinationPatternCoordinationPatternContainerCompartmentEditPart(view);
+			case RoleEditPart.VISUAL_ID:
+				return new CustomRoleEditPart(view);
+			case CoordinationPattern2EditPart.VISUAL_ID:
+				return new CustomCoordination2PatternEditPart(view);
+//			case ConstrainableElementConstraintEditPart.VISUAL_ID:
+//				return new CustomTextualConstraintEditPart(view);
+
+			}
+		}
+		return super.createEditPart(context, model);
+	}
+}
---
+++ de.uni_paderborn.fujaba.muml.patterneditor.diagram.custom/src/de/uni_paderborn/fujaba/muml/patterneditor/diagram/custom/edit/parts/CustomTextualConstraintEditPart.java
@@ -0,0 +1,41 @@
+package de.uni_paderborn.fujaba.muml.patterneditor.diagram.custom.edit.parts;
+
+
+//
+///**
+//* A custom EditPart for TextualConstraints. We customize the NodeFigure here.
+//* 
+//* @author bingo
+//* 
+//*/
+//public class CustomTextualConstraintEditPart extends ConstrainableElementConstraintEditPart {
+//
+//	public CustomTextualConstraintEditPart(View view) {
+//		super(view);
+//	}
+//
+//	/**
+//	 * Make sure, the node figure returns the used PolygonPoints, so that
+//	 * connections can connect to the correct location.
+//	 */
+//	protected NodeFigure createNodePlate() {
+//		DefaultSizeNodeFigure result = new DefaultSizeNodeFigure(40, 40) {
+//			public PointList getPolygonPoints() {
+//				PointList points = new PointList(6);
+//				Rectangle anchRect = getHandleBounds();
+//				points.addPoint(anchRect.x, anchRect.y);
+//				points.addPoint(anchRect.x + anchRect.width * 30 / 40,
+//						anchRect.y);
+//				points.addPoint(anchRect.x + anchRect.width, anchRect.y
+//						+ anchRect.height * 10 / 40);
+//				points.addPoint(anchRect.x + anchRect.width, anchRect.y
+//						+ anchRect.height);
+//				points.addPoint(anchRect.x, anchRect.y + anchRect.height);
+//				points.addPoint(anchRect.x, anchRect.y);
+//				return points;
+//			}
+//		};
+//		return result;
+//	}
+//
+//}
