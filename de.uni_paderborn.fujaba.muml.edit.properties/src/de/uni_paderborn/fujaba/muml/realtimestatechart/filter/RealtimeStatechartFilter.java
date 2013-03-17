4fe4aa8dc9ee232096c6195572a07e5af766b4c6
Ingo Budde <bingo@mail.upb.de>
2013-03-17 20:54:32 +0000

    #459: Shared initial version of edit.properties, which will contain the MUML Custom Property Tabs, as they are in SDM.

---
+++ de.uni_paderborn.fujaba.muml.edit.properties/src/de/uni_paderborn/fujaba/muml/realtimestatechart/filter/RealtimeStatechartFilter.java
@@ -0,0 +1,13 @@
+package de.uni_paderborn.fujaba.muml.realtimestatechart.filter;
+
+import org.eclipse.jface.viewers.IFilter;
+
+public class RealtimeStatechartFilter implements IFilter {
+
+	@Override
+	public boolean select(Object toTest) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
