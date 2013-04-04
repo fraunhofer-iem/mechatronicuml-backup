363e20c6bc3e3be33fe352dbe1641dd1d590d1b9
Christian Heinzemann <chris227@upb.de>
2013-04-04 14:49:58 +0000

    transformation is called correctly and creates a reconfigurable structured component, references are not yet copied, additional contents are not yet created

--- de.uni_paderborn.fujaba.muml.reconfiguration.ui/src/de/uni_paderborn/fujaba/muml/reconfiguration/ui/commands/MakeStructuredComponentReconfigurableCommand.java
+++ de.uni_paderborn.fujaba.muml.reconfiguration.ui/src/de/uni_paderborn/fujaba/muml/reconfiguration/ui/commands/MakeStructuredComponentReconfigurableCommand.java
@@ -7,8 +7,14 @@ import java.util.List;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -18,7 +24,6 @@ import org.eclipse.m2m.qvt.oml.ExecutionDiagnostic;
 import org.eclipse.m2m.qvt.oml.ModelExtent;
 import org.eclipse.m2m.qvt.oml.TransformationExecutor;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.storydriven.storydiagrams.activities.Activity;
 
 import de.uni_paderborn.fujaba.modelinstance.ModelElementCategory;
 import de.uni_paderborn.fujaba.muml.component.StaticStructuredComponent;
@@ -32,15 +37,19 @@ public class MakeStructuredComponentReconfigurableCommand extends AbstractHandle
 		//get and process current selection
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
 		
+		//collect all StaticStructureComponents from the selection
 		Iterator iter = ((IStructuredSelection) selection).iterator();
 		while(iter.hasNext()){
 			GraphicalEditPart editPart = (GraphicalEditPart) iter.next();
 			
-			//if a StaticStructuredComponent has been found, call transformation function
+			//add StaticStructuredComponent to list
 			if(editPart instanceof StaticStructuredComponentEditPart){
 				StaticStructuredComponent sc = (StaticStructuredComponent) ((View) editPart.getModel()).getElement();
-				
-				makeStructuredComponentReconfigurable(sc);
+
+				ICommandProxy cmd = new ICommandProxy(
+						new StaticStructuredComponentTransformationCommand(editPart
+								.getEditingDomain(), sc));
+				cmd.execute();
 			}
 			
 		}
@@ -48,6 +57,11 @@ public class MakeStructuredComponentReconfigurableCommand extends AbstractHandle
 		return null;
 	}
 	
+	/**
+	 * Helper functions that transforms one StaticStructuredComponent into a ReconfigurableStructuredComponent.
+	 * This function calls a QVT-O transformation.
+	 * @param sc
+	 */
 	private void makeStructuredComponentReconfigurable(StaticStructuredComponent sc){
 		ModelElementCategory category = (ModelElementCategory) sc.eContainer();
 		
@@ -79,6 +93,34 @@ public class MakeStructuredComponentReconfigurableCommand extends AbstractHandle
 		}
 		
 		
+	}
+	
+	/**
+	 * Helper class to edit the resource/model.
+	 * 
+	 * Direct manipulation would lead to a "java.lang.IllegalStateException:
+	 * Cannot modify resource set without a write transaction"
+	 * 
+	 */
+	class StaticStructuredComponentTransformationCommand extends AbstractTransactionalCommand {
+		
+		private StaticStructuredComponent sc;
+
+		public StaticStructuredComponentTransformationCommand(
+				TransactionalEditingDomain editingDomain, StaticStructuredComponent comp) {
+			super(editingDomain, "Make Structured Component Reconfigurable", null);
+			this.sc = comp;
+		}
+
+		@Override
+		protected CommandResult doExecuteWithResult(IProgressMonitor monitor,
+				IAdaptable info) throws ExecutionException {
+
+			
+			makeStructuredComponentReconfigurable(sc);
+
+			return CommandResult.newOKCommandResult();
+		}
 	}
 
 }
