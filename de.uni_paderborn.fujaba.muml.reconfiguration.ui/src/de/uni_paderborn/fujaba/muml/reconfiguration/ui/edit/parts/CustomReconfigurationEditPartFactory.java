769c4b4e6cbdbe62ccb0e08769c904db755ad986
Christian Heinzemann <chris227@upb.de>
2013-04-26 15:26:01 +0000

    - registered custom edit part factory for reconfigurable components (no contents yet) extending the edit part factory of component.diagram.custom - tried to register a palette extension which is currently not working

---
+++ de.uni_paderborn.fujaba.muml.reconfiguration.ui/src/de/uni_paderborn/fujaba/muml/reconfiguration/ui/edit/parts/CustomReconfigurationEditPartFactory.java
@@ -0,0 +1,20 @@
+package de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.parts;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.notation.View;
+
+import de.uni_paderborn.fujaba.muml.component.diagram.custom.edit.parts.CustomStructuredcomponentEditPartFactory;
+
+public class CustomReconfigurationEditPartFactory extends
+		CustomStructuredcomponentEditPartFactory {
+
+	@Override
+	public EditPart createEditPart(EditPart context, Object model) {
+		if (model instanceof View) {
+			View view = (View) model;
+		}
+		
+		return super.createEditPart(context, model);
+	}
+	
+}
