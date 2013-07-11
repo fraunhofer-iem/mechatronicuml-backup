8f02fdf083b8c936447244835287655a38e5b0f3
Ingo Budde <bingo@mail.upb.de>
2013-07-11 12:41:08 +0000

    Fixed #72: Changing the order of Regions in State is now possible using old and new properties, just by changing the order in the list; priorities are adapted.

--- de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.custom/src/de/uni_paderborn/fujaba/muml/realtimestatechart/diagram/custom/edit/parts/CustomStateEditPart.java
+++ de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.custom/src/de/uni_paderborn/fujaba/muml/realtimestatechart/diagram/custom/edit/parts/CustomStateEditPart.java
@@ -1,13 +1,24 @@
 package de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.custom.edit.parts;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.PointList;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.gmf.runtime.gef.ui.figures.DefaultSizeNodeFigure;
 import org.eclipse.gmf.runtime.gef.ui.figures.NodeFigure;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
 import org.eclipse.gmf.runtime.notation.View;
 
 import de.uni_paderborn.fujaba.muml.common.edit.policies.statechart.StateEditPolicy;
+import de.uni_paderborn.fujaba.muml.realtimestatechart.RealtimestatechartPackage;
+import de.uni_paderborn.fujaba.muml.realtimestatechart.Region;
+import de.uni_paderborn.fujaba.muml.realtimestatechart.State;
 import de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.edit.parts.StateEditPart;
+import de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.edit.parts.StateRegionCompartmentEditPart;
+import de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.part.MumlVisualIDRegistry;
 
 public class CustomStateEditPart extends StateEditPart {
 
@@ -29,6 +40,53 @@ public class CustomStateEditPart extends StateEditPart {
 		};
 		plate.setMinimumSize(new Dimension(0, 0));
 		return plate;
+	}
+
+	@Override
+	protected void handleNotificationEvent(Notification notification) {
+		if (notification.getFeature() == RealtimestatechartPackage.Literals.STATE__EMBEDDED_REGIONS && notification.getEventType() == Notification.MOVE) {
+			// #72: Reorder notation views when region order changes, to force re-ordering in the editor; also see comparator in CustomStateRegionCompartmentEditPart
+			State state = (State) resolveSemanticElement();
+			View regionCompartment = getChildView(getNotationView(), StateRegionCompartmentEditPart.VISUAL_ID);
+			if (regionCompartment != null) {
+				List<View> orderedViews = new ArrayList<View>();
+				for (Region region : state.getEmbeddedRegions()) {
+					for (Object child : regionCompartment.getPersistedChildren()) {
+						View childView = (View) child;
+						if (childView.getElement() == region) {
+							orderedViews.add(childView);
+							break; // break inner loop
+						}
+					}
+				}
+				getEditingDomain().getCommandStack().execute(SetCommand.create(getEditingDomain(), regionCompartment, NotationPackage.Literals.VIEW__PERSISTED_CHILDREN, orderedViews));
+			}
+			
+		}
+		
+		super.handleNotificationEvent(notification);
+	}
+
+	private static View getChildView(View view, int visualId) {
+		List<View> views = getChildViews(view, visualId);
+		if (!views.isEmpty()) {
+			return views.get(0);
+		}
+		return null;
+	}
+
+	private static List<View> getChildViews(View view, int visualId) {
+		List<View> childViews = new ArrayList<View>();
+		String type = MumlVisualIDRegistry.getType(visualId);
+
+		for (Object child : view.getChildren()) {
+			View childView = (View) child;
+			if (type.equals(childView.getType())) {
+				childViews.add(childView);
+			}
+		}
+	
+		return childViews;
 	}
 
 }
--- de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.custom/src/de/uni_paderborn/fujaba/muml/realtimestatechart/diagram/custom/edit/parts/CustomStateRegionCompartmentEditPart.java
+++ de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.custom/src/de/uni_paderborn/fujaba/muml/realtimestatechart/diagram/custom/edit/parts/CustomStateRegionCompartmentEditPart.java
@@ -1,16 +1,23 @@
 package de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.custom.edit.parts;
 
+import java.util.Comparator;
+
 import org.eclipse.draw2d.Border;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.LineBorder;
 import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.notation.Shape;
+import org.eclipse.gmf.runtime.notation.SortingDirection;
 import org.eclipse.gmf.runtime.notation.View;
 
+import de.uni_paderborn.fujaba.muml.realtimestatechart.Region;
+import de.uni_paderborn.fujaba.muml.realtimestatechart.State;
 import de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.edit.parts.RegionEditPart;
 import de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.edit.parts.StateRegionCompartmentEditPart;
 
 public class CustomStateRegionCompartmentEditPart extends
 		StateRegionCompartmentEditPart {
+	public static final String DEFAULT_SORTING_KEY = "default_sorting_key";
 
 	public CustomStateRegionCompartmentEditPart(View view) {
 		super(view);
@@ -51,5 +58,49 @@ public class CustomStateRegionCompartmentEditPart extends
 			lastLineBorder.setColor(ColorConstants.white);
 		}
 	}
+	
+
+	// BEGIN #72: Visual order of regions depends on the order of regions in the model.
+	@Override
+	protected Comparator<Shape> getComparator(String name,
+			SortingDirection direction) {
+		if (DEFAULT_SORTING_KEY.equals(name)) {
+			return new StateCompartmentComparator(direction);
+		}
+		return null;
+	}
+
+	public class StateCompartmentComparator implements Comparator<Shape> {
+
+		private final SortingDirection direction;
+		
+		public StateCompartmentComparator(SortingDirection direction) {
+			this.direction = direction;
+		}
+
+		@Override
+		public int compare(Shape lhs, Shape rhs) {
+			State state = (State) CustomStateRegionCompartmentEditPart.this.resolveSemanticElement();
+			
+			int result = 0;
+			for (Region region : state.getEmbeddedRegions()) {
+				if (lhs.getElement() == region) {
+					result = 1;
+					break;
+				} else if (rhs.getElement() == region) {
+					result = -1;
+					break;
+				}
+			}
+			
+			if (direction.getValue() == SortingDirection.DESCENDING) {
+				result = -result;
+			}
+			return result;
+
+		}
+	}
+	// END #72
+
 
 }
