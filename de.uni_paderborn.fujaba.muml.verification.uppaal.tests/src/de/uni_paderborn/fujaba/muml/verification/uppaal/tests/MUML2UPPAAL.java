d70c77683ff96cd226a8864d063b88b5087a2885
Christopher Gerking <cgerking@campus.upb.de>
2013-07-17 17:41:27 +0000

    Share UPPAAL test project including a small M2M testing framework based on EMF Compare

---
+++ de.uni_paderborn.fujaba.muml.verification.uppaal.tests/src/de/uni_paderborn/fujaba/muml/verification/uppaal/tests/MUML2UPPAAL.java
@@ -0,0 +1,72 @@
+package de.uni_paderborn.fujaba.muml.verification.uppaal.tests;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import de.uni_paderborn.fujaba.muml.MumlPackage;
+import de.uni_paderborn.fujaba.muml.protocol.CoordinationProtocol;
+import de.uni_paderborn.fujaba.muml.verification.uppaal.UppaalPackage;
+import de.uni_paderborn.fujaba.muml.verification.uppaal.job.MUML2UPPAALJob;
+
+@RunWith(value = Parameterized.class)
+public class MUML2UPPAAL extends Model2Model {
+	
+	public MUML2UPPAAL(String source, String reference) {
+		super(source,reference);
+	}
+	
+	protected EObject getTarget() {
+		
+		 EObject sourceObject = getSource();
+		    
+		 MUML2UPPAALJob m2m = new MUML2UPPAALJob((CoordinationProtocol) sourceObject);
+		 m2m.schedule();
+		    
+		 return m2m.getNTA();
+		    	
+	}
+	
+	@Parameters
+	public static Collection<Object[]> data() {
+		
+		Object[][] data = new Object[][] {
+				
+			{ "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking0/blindOvertaking.fujaba#//@categories.2/@modelElements.0", "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking0/NTA.uppaal" },
+			{ "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking1/blindOvertaking.fujaba#//@categories.2/@modelElements.0", "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking1/NTA.uppaal" },
+			{ "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking2/blindOvertaking.fujaba#//@categories.2/@modelElements.0", "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking2/NTA.uppaal" },
+			{ "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking3/blindOvertaking.fujaba#//@categories.2/@modelElements.0", "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking3/NTA.uppaal" },
+			{ "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking4/blindOvertaking.fujaba#//@categories.2/@modelElements.0", "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking4/NTA.uppaal" },
+			{ "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking5/blindOvertaking.fujaba#//@categories.2/@modelElements.0", "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking5/NTA.uppaal" },
+			{ "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking6/blindOvertaking.fujaba#//@categories.2/@modelElements.0", "platform:/plugin/de.uni_paderborn.fujaba.muml.verification.uppaal.tests/resources/overtaking6/NTA.uppaal" },
+		
+		};
+		
+		return Arrays.asList(data);
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		// Initialize the model
+	    MumlPackage.eINSTANCE.eClass();
+	    UppaalPackage.eINSTANCE.eClass();
+	    
+	    // Register the XMI resource factory for the .fujaba and .uppaal extensions
+
+	    Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+	    Map<String, Object> m = reg.getExtensionToFactoryMap();
+	    m.put("fujaba", new XMIResourceFactoryImpl());
+	    m.put("uppaal", new XMIResourceFactoryImpl());
+		
+	}
+
+}
---
+++ de.uni_paderborn.fujaba.muml.verification.uppaal.tests/src/de/uni_paderborn/fujaba/muml/verification/uppaal/tests/Model2Model.java
@@ -0,0 +1,105 @@
+package de.uni_paderborn.fujaba.muml.verification.uppaal.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.Diagnostician;
+import org.junit.Test;
+
+public abstract class Model2Model {
+	
+	public Model2Model(String source, String reference) {
+		
+		this.source = URI.createURI(source);
+		this.reference = URI.createURI(reference);
+		
+	}
+	
+	private URI source;
+	private URI reference;
+	
+	protected EObject getSource() {
+		
+		return loadEObjectFromURI(source);
+		
+	}
+	
+	protected EObject getReference() {
+		
+		return loadEObjectFromURI(reference);
+		
+	}
+	
+	protected abstract EObject getTarget();
+	
+	@Test
+	public void test() {
+		
+		EObject expected = getReference();
+	    EObject actual = getTarget();
+	    
+	    assertNoDifferences(expected, actual);
+	    
+	    assertValid(actual);
+	    
+	}
+
+	protected static EObject loadEObjectFromURI(URI uri) {
+		
+	    ResourceSet resSet = new ResourceSetImpl();
+	    	    
+	    Resource resource = resSet.getResource(uri.trimFragment(), true);
+	    
+	    EObject result = null;
+	    
+	    if (uri.hasFragment()) {
+	    	result = resource.getEObject(uri.fragment());
+	    };
+	    
+	    return result != null ? result : resource.getContents().get(0);
+		
+	}
+
+	protected static void assertNoDifferences(EObject actual, EObject expected) {
+			 
+		IComparisonScope scope = new DefaultComparisonScope(expected, actual, null);
+		Comparison comparison = EMFCompare.builder().build().compare(scope);
+	 		
+		OutputStream stream = new OutputStream() {
+						
+			private StringBuilder string = new StringBuilder();
+	        @Override
+	        public void write(int b) throws IOException {
+	            this.string.append((char) b );
+	        }
+	
+	        public String toString(){
+	            return this.string.toString();
+	        }
+	    
+		};
+		
+		EMFComparePrettyPrinter.printDifferences(comparison, new PrintStream(stream));
+		
+		assertTrue(stream.toString(), comparison.getDifferences().isEmpty());
+		
+	}
+	
+	protected static void assertValid(EObject eObject) {
+		
+		Diagnostic diagnostic = Diagnostician.INSTANCE.validate(eObject);
+	    assertTrue(BasicDiagnostic.toIStatus(diagnostic).isOK());
+		
+	}
+
+}
