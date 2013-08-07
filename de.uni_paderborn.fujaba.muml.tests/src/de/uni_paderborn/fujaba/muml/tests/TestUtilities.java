660e4d1513b48240b3faeb370aabcaa38411212c
Christopher Gerking <cgerking@campus.upb.de>
2013-08-07 15:50:20 +0000

    add special case for org.storydriven.core to be resolved when testing on the CI server

--- de.uni_paderborn.fujaba.muml.tests/src/de/uni_paderborn/fujaba/muml/tests/TestUtilities.java
+++ de.uni_paderborn.fujaba.muml.tests/src/de/uni_paderborn/fujaba/muml/tests/TestUtilities.java
@@ -2,6 +2,8 @@ package de.uni_paderborn.fujaba.muml.tests;
 
 import java.io.File;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -40,27 +42,25 @@ public class TestUtilities {
 	}
 
 	public static void registerWorkspaceProject(String projectName) {
-				
-		// Find out absolute path of project
-		StringBuffer absoluteProjectPath = new StringBuffer();
 		
 		// XXX: new File("") yields the working directory of the running JVM
 		// the lines below assume that this working directory is always a project folder (de.uni_paderborn.fujaba.muml.tests) under the workspace root
 		// however, the working directory can be changed in the JUnit launch configuration and might differ from this project folder
-		absoluteProjectPath.append(new File("").getAbsoluteFile()
-				.getParentFile());
-		absoluteProjectPath.append('/');
-		absoluteProjectPath.append(projectName);
-		// no idea why, but this is necessary two times :)
-		absoluteProjectPath.append('/');
-		absoluteProjectPath.append(projectName);
-
+		String workspaceRoot = new File("").getAbsoluteFile().getParentFile().getAbsolutePath();
+		
+		IPath workspacePath = new Path(workspaceRoot);
+		
+		IPath projectPath = workspacePath.append(projectName);
+		
+		// insert "sdm" path to match workspace structure on continuous integration server
+		if (!new File(projectPath.toOSString()).exists()) {
+			projectPath = workspacePath.append("sdm").append(projectName);
+		}
+		
+		// add a trailing separator to avoid cutting off the project name in URI$Hierarchical.mergePath
+		URI uri = URI.createFileURI(projectPath.addTrailingSeparator().toPortableString());
+				
 		// Register platform mapping for project
-		EcorePlugin.getPlatformResourceMap().put(projectName,
-				URI.createFileURI(
-				//project.getLocation().toOSString()
-				absoluteProjectPath.toString()
-		)
-		);
+		EcorePlugin.getPlatformResourceMap().put(projectName,uri);
 	}
 }
