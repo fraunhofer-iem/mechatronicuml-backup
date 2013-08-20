bef063235425aee5db4c4a00b2f3134fd2731636
Davic Schubert <tox>
2013-08-20 14:43:17 +0000

    Added EditPart for ReconfigurableStructuredComponent

--- de.uni_paderborn.fujaba.muml.reconfiguration.ui/src/de/uni_paderborn/fujaba/muml/reconfiguration/ui/edit/parts/CustomReconfigurationEditPartFactory.java
+++ de.uni_paderborn.fujaba.muml.reconfiguration.ui/src/de/uni_paderborn/fujaba/muml/reconfiguration/ui/edit/parts/CustomReconfigurationEditPartFactory.java
@@ -12,6 +12,19 @@ public class CustomReconfigurationEditPartFactory extends
 	public EditPart createEditPart(EditPart context, Object model) {
 		if (model instanceof View) {
 			View view = (View) model;
+			
+			int visualid = de.uni_paderborn.fujaba.muml.component.diagram.part.MumlVisualIDRegistry
+					.getVisualID(view);
+			
+			int reconfid = de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.parts.ReconfigurableStructuredComponentEditPart.VISUAL_ID;
+			
+			switch (de.uni_paderborn.fujaba.muml.component.diagram.part.MumlVisualIDRegistry
+					.getVisualID(view)) {
+
+			case de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.parts.ReconfigurableStructuredComponentEditPart.VISUAL_ID:
+				return new de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.parts.ReconfigurableStructuredComponentEditPart(
+						view);
+			}
 		}
 		
 		return super.createEditPart(context, model);
