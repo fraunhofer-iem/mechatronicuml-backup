f583b06453c02a25c2bafdfd8074f4718ff5c19e
Andreas Dann <adann@mail.upb.de>
2013-12-25 12:49:01 +0000

    created editPolicy to resize Compartments/Container based on the embedded Figures the solution is based on an editPolicy licensed under the EPL (eclipse public license)

--- de.uni_paderborn.fujaba.muml.hardware.common/src/de/uni_paderborn/fujaba/muml/hardware/common/edit/policies/EditPolicyRoles.java
+++ de.uni_paderborn.fujaba.muml.hardware.common/src/de/uni_paderborn/fujaba/muml/hardware/common/edit/policies/EditPolicyRoles.java
@@ -10,5 +10,6 @@ public interface EditPolicyRoles {
 	public static final String RESOURCE_VISUALIZATION_ROLE = "RessourceVisualizationRole"; //$NON-NLS-1$
 	public static final String HWPORT_VISUALIZATION_ROLE = "HWPortVisualizationRole"; //$NON-NLS-1$
 
-
+	   // Key for edit policy installation
+    public static final String RESIZE_CONTAINER = "ResizeContainer";
 }
