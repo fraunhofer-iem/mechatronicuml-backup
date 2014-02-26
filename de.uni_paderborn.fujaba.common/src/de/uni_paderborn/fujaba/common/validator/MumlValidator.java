6ca0c54a3b92fe667f915581c9065e995acbc656
Ingo Budde <bingo@mail.upb.de>
2014-02-26 13:25:38 +0000

    Added functionality to define warnings (as opposed to errors) in muml.ecore; syntax in first line: -- @meta warning

--- de.uni_paderborn.fujaba.common/src/de/uni_paderborn/fujaba/common/validator/MumlValidator.java
+++ de.uni_paderborn.fujaba.common/src/de/uni_paderborn/fujaba/common/validator/MumlValidator.java
@@ -1,8 +1,11 @@
 package de.uni_paderborn.fujaba.common.validator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.DiagnosticChain;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EClass;
@@ -11,14 +14,21 @@ import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.util.EObjectValidator;
 
 public class MumlValidator extends EObjectValidator {
+	
+	public static final String META_TAG = "@meta";
+	
 	protected void reportConstraintDelegateViolation(EClass eClass,
 			EObject eObject, DiagnosticChain diagnostics,
 			Map<Object, Object> context, String constraint, int severity,
 			String source, int code) {
 
-		
 		// Try to create message by using the comment in the OCL.
 		String ocl = getOCL(eClass, constraint);
+
+		// To test the warning behavior:
+		// ocl = "-- @meta warning\n" + ocl;
+
+		String[] metainfos = getMetainfos(ocl);
 		String message = getErrorMessage(ocl);
 		
 		// If we have no message, create one by using the constraint name.
@@ -32,6 +42,12 @@ public class MumlValidator extends EObjectValidator {
 				builder.append(constraint.charAt(i));
 			}
 			message = builder.toString();
+		}
+		
+		for (String metainfo : metainfos) {
+			if (metainfo.toLowerCase().equals("warning")) {
+				severity = Diagnostic.WARNING;
+			}
 		}
 
 		diagnostics.add(new BasicDiagnostic(severity, source, code,
@@ -53,13 +69,14 @@ public class MumlValidator extends EObjectValidator {
 			ocl = ocl.trim();
 
 			StringBuilder builder = new StringBuilder();
-			
-			
 			for (String line : ocl.split("\n")) {
 				line = line.trim();
 				if (line.startsWith("--")) {
 					line = line.substring(2).trim();
 					if (line.length() > 0) {
+						if (getMetainfo(line) != null) {
+							continue;
+						}
 						builder.append(line);
 						builder.append('\n');
 					}
@@ -69,6 +86,33 @@ public class MumlValidator extends EObjectValidator {
 			message = builder.toString();
 		}
 		return message;
+	}
+	
+	protected String[] getMetainfos(String ocl) {
+		List<String> metainfos = new ArrayList<String>();
+
+		if (ocl != null) {
+			ocl = ocl.trim();
+
+			for (String line : ocl.split("\n")) {
+				line = line.trim();
+				if (line.startsWith("--")) {
+					line = line.substring(2).trim();
+					String metainfo = getMetainfo(line);
+					if (line.length() > 0 && metainfo != null) {
+						metainfos.add(metainfo);
+					}
+				}
+			}
+		}
+		return metainfos.toArray(new String[] {});
+	}
+
+	protected static String getMetainfo(String line) {
+		if (line.startsWith(META_TAG)) {
+			return line.substring(META_TAG.length()).trim();
+		}
+		return null;
 	}
 
 	private static String getOCL(EClass eClass, String name) {
