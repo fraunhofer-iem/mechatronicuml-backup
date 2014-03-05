d19d8f37052ab597a57f3d201f1e025c5c703685
Ingo Budde <bingo@mail.upb.de>
2014-03-05 15:17:07 +0000

    Added @generated NOT annotation to modified edit code to prevent it from being overwritten.

--- de.uni_paderborn.fujaba.muml.edit/src/de/uni_paderborn/fujaba/muml/valuetype/provider/RangeItemProvider.java
+++ de.uni_paderborn.fujaba.muml.edit/src/de/uni_paderborn/fujaba/muml/valuetype/provider/RangeItemProvider.java
@@ -172,7 +172,7 @@ public class RangeItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
