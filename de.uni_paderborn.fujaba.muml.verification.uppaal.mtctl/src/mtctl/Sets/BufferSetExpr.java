5ddf594e03b617a4e06c82de6d9d75b34d944190
Jan Bobolz <jbobolz>
2014-03-25 17:03:34 +0000

    Forgot some essential mtctl files from previous commit.

---
+++ de.uni_paderborn.fujaba.muml.verification.uppaal.mtctl/src/mtctl/Sets/BufferSetExpr.java
@@ -0,0 +1,17 @@
+/**
+ */
+package mtctl.Sets;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Buffer Set Expr</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see mtctl.Sets.SetsPackage#getBufferSetExpr()
+ * @model
+ * @generated
+ */
+public interface BufferSetExpr extends SetExpr {
+} // BufferSetExpr
