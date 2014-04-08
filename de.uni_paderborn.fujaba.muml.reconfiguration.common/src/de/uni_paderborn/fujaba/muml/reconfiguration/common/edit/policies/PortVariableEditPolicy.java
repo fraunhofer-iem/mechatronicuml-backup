899a6971fe9ac7b59f1c1900c2ef8d19b700d09f
Christian Heinzemann <chris227@upb.de>
2014-04-08 12:55:36 +0000

    branched reconfiguration meta model + ui and runtime meta model

---
+++ de.uni_paderborn.fujaba.muml.reconfiguration.common/src/de/uni_paderborn/fujaba/muml/reconfiguration/common/edit/policies/PortVariableEditPolicy.java
@@ -0,0 +1,30 @@
+package de.uni_paderborn.fujaba.muml.reconfiguration.common.edit.policies;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+
+import de.uni_paderborn.fujaba.muml.common.edit.policies.ports.PortTypeEditPolicy;
+import de.uni_paderborn.fujaba.muml.component.Port;
+import de.uni_paderborn.fujaba.muml.componentstorypattern.ComponentstorypatternPackage;
+
+public class PortVariableEditPolicy extends PortTypeEditPolicy {
+	@Override
+	public void handleNotificationEvent(Notification notification) {
+		if (ComponentstorypatternPackage.Literals.PORT_VARIABLE__TYPE
+				.equals(notification.getFeature())) {
+			refreshArrow();
+			refreshPortType();
+		}
+		super.handleNotificationEvent(notification);
+	}
+
+	@Override
+	protected Port getPort() {
+		EObject element = getSemanticElement();
+		if (element != null) {
+			return (Port) element
+					.eGet(ComponentstorypatternPackage.Literals.PORT_VARIABLE__TYPE);
+		}
+		return null;
+	}
+}
