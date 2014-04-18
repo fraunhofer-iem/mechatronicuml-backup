7f8c044cb5713f69655ca23df88892391ebb0e3b
Andreas Dann <adann@mail.upb.de>
2014-04-18 13:01:17 +0000

    deleted old PDM Resource Type Editor

--- de.uni_paderborn.fujaba.muml.hardware.resourcetype.diagram/src/de/uni_paderborn/fujaba/muml/hardware/resourcetype/diagram/edit/helpers/ProcessorEditHelper.java
+++
@@ -1,9 +0,0 @@
-package de.uni_paderborn.fujaba.muml.hardware.resourcetype.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class ProcessorEditHelper
-		extends
-		de.uni_paderborn.fujaba.muml.hardware.resourcetype.diagram.edit.helpers.HardwareBaseEditHelper {
-}
