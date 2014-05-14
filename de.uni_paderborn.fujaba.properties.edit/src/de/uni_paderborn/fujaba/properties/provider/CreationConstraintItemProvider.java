a4c40bbcd29c2f6a07f05f5e93df6101d7a6aa6c
Ingo Budde <bingo@mail.upb.de>
2014-05-14 09:45:54 +0000

    APE: Make CreationConstraint.filters a containment.

--- de.uni_paderborn.fujaba.properties.edit/src/de/uni_paderborn/fujaba/properties/provider/CreationConstraintItemProvider.java
+++ de.uni_paderborn.fujaba.properties.edit/src/de/uni_paderborn/fujaba/properties/provider/CreationConstraintItemProvider.java
@@ -3,6 +3,8 @@
 package de.uni_paderborn.fujaba.properties.provider;
 
 
+import de.uni_paderborn.fujaba.properties.CreationConstraint;
+import de.uni_paderborn.fujaba.properties.PropertiesFactory;
 import de.uni_paderborn.fujaba.properties.PropertiesPackage;
 
 import java.util.Collection;
@@ -13,6 +15,7 @@ import org.eclipse.emf.common.notify.Notification;
 
 import org.eclipse.emf.common.util.ResourceLocator;
 
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -21,6 +24,7 @@ import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.eclipse.emf.edit.provider.ViewerNotification;
 
 /**
  * This is the item provider adapter for a {@link de.uni_paderborn.fujaba.properties.CreationConstraint} object.
@@ -85,6 +89,36 @@ public class CreationConstraintItemProvider
 	}
 
 	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(PropertiesPackage.Literals.CREATION_CONSTRAINT__FILTERS);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
+	}
+
+	/**
 	 * This returns CreationConstraint.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
@@ -116,6 +150,12 @@ public class CreationConstraintItemProvider
 	@Override
 	public void notifyChanged(Notification notification) {
 		updateChildren(notification);
+
+		switch (notification.getFeatureID(CreationConstraint.class)) {
+			case PropertiesPackage.CREATION_CONSTRAINT__FILTERS:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				return;
+		}
 		super.notifyChanged(notification);
 	}
 
@@ -129,6 +169,11 @@ public class CreationConstraintItemProvider
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(PropertiesPackage.Literals.CREATION_CONSTRAINT__FILTERS,
+				 PropertiesFactory.eINSTANCE.createOCLFilter()));
 	}
 
 	/**
