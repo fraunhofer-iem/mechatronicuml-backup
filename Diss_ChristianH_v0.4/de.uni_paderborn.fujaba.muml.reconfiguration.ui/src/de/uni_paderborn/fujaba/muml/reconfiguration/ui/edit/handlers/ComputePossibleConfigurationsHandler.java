1840bd3b715474a8f2b084e7f9eda8d495ed7813
Davic Schubert <tox>
2014-05-22 12:34:41 +0000

    extended command by call of SDMReachabilityComputation

--- Diss_ChristianH_v0.4/de.uni_paderborn.fujaba.muml.reconfiguration.ui/src/de/uni_paderborn/fujaba/muml/reconfiguration/ui/edit/handlers/ComputePossibleConfigurationsHandler.java
+++ Diss_ChristianH_v0.4/de.uni_paderborn.fujaba.muml.reconfiguration.ui/src/de/uni_paderborn/fujaba/muml/reconfiguration/ui/edit/handlers/ComputePossibleConfigurationsHandler.java
@@ -1,19 +1,20 @@
 package de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.handlers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.m2m.qvt.oml.BasicModelExtent;
@@ -23,15 +24,14 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.storydriven.storydiagrams.activities.Activity;
 
-import de.uni_paderborn.fujaba.common.FujabaCommonPlugin;
 import de.uni_paderborn.fujaba.common.edit.commands.ExecuteQvtoTransformationCommand;
-import de.uni_paderborn.fujaba.modelinstance.RootNode;
-import de.uni_paderborn.fujaba.muml.component.DiscretePort;
 import de.uni_paderborn.fujaba.muml.component.diagram.custom.part.Activator;
-import de.uni_paderborn.fujaba.muml.component.diagram.edit.parts.DiscretePortEditPart;
 import de.uni_paderborn.fujaba.muml.componentstorydiagram.ComponentStoryRule;
-import de.uni_paderborn.fujaba.muml.protocol.Role;
-import de.uni_paderborn.fujaba.muml.reconfiguration.ManagerSpecificationEntry;
+import de.uni_paderborn.fujaba.muml.instance.ComponentInstanceConfiguration;
+import de.uni_paderborn.fujaba.muml.reachanalysis.sdm.SDMReachabilityComputation;
+import de.uni_paderborn.fujaba.muml.reachanalysis.sdm.export.SDMReachabilityGraphExporter;
+import de.uni_paderborn.fujaba.muml.reconfiguration.ExecutorSpecificationEntry;
+import de.uni_paderborn.fujaba.muml.reconfiguration.ReconfigurableComponent;
 import de.uni_paderborn.fujaba.muml.reconfiguration.ReconfigurableStructuredComponent;
 import de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.parts.ReconfigurableStructuredComponentEditPart;
 
@@ -58,18 +58,37 @@ public class ComputePossibleConfigurationsHandler extends AbstractHandler {
 				ReconfigurableStructuredComponent reconfComponent = (ReconfigurableStructuredComponent) ((View) editPart.getModel())
 						.getElement();
 
-				ModelExtent sds = csd2sd(editPart.getEditingDomain(), reconfComponent);
+				List<Activity> activities = csd2sd(editPart.getEditingDomain(), reconfComponent);
+
+				HashSet<EObject> initialGraphObjects = new HashSet<EObject>();
+				//check whether explicit initial configuration has been set
+				if(reconfComponent.getInitialConfiguration() != null){
+					initialGraphObjects.add(reconfComponent.getInitialConfiguration());
+					initialGraphObjects.add(reconfComponent);
+				} else { // if not, built default initial configuration
+					initialGraphObjects.add(reconfComponent);
+					initialGraphObjects.add(buildDefaultInitialCIC(reconfComponent));
+				}
+
+				SDMReachabilityComputation reachComp = new SDMReachabilityComputation(initialGraphObjects, activities);
+				reachComp.setMaxPathLength(5);
+				reachComp.computeReachabilityGraph();
+				
+				SDMReachabilityGraphExporter exporter = new SDMReachabilityGraphExporter();
+				exporter.export(reachComp.getReachabilityGraph());
 			}
+
+
 
 		}
 
 		return null;
 	}
 	
-	public static ModelExtent csd2sd(EditingDomain editingDomain, ReconfigurableStructuredComponent reconfComp) {
+	public static List<Activity> csd2sd(EditingDomain editingDomain, ReconfigurableStructuredComponent reconfComp) {
 		
-		List<Activity> inputActivities = new BasicEList<Activity>();
-		for(ManagerSpecificationEntry mSE : reconfComp.getManager().getSpecificationEntries()){
+		List<EObject> inputActivities = new BasicEList<EObject>();
+		for(ExecutorSpecificationEntry mSE : reconfComp.getExecutor().getSpecificationEntries()){
 			if(mSE.getReconfigurationRule() instanceof ComponentStoryRule){
 				Activity activity = ((ComponentStoryRule) mSE.getReconfigurationRule()).getActivity();
 				inputActivities.add(activity);
@@ -80,7 +99,7 @@ public class ComputePossibleConfigurationsHandler extends AbstractHandler {
 		ModelExtent outputExtent = new BasicModelExtent();
 		
 		List<ModelExtent> modelExtents = Arrays.asList(new ModelExtent[] { inputExtent, outputExtent });
-		
+		//List<ModelExtent> modelExtents = Arrays.asList(new ModelExtent[] { inputExtent });
 
 		// Load QVTO script
 		TransformationExecutor transformationExecutor = Activator.getInstance()
@@ -97,11 +116,24 @@ public class ComputePossibleConfigurationsHandler extends AbstractHandler {
 		if (!command.hasChanged() && editingDomain.getCommandStack().canUndo()) {
 			editingDomain.getCommandStack().undo();
 		}
-		return outputExtent;
+		
+		
+		List<Activity> activities = new ArrayList<Activity>();
+		for(EObject obj : outputExtent.getContents()){
+			if (obj instanceof Activity){
+				activities.add((Activity)obj);
+			}
+		}
+		
+		return activities;
+	}
+	
+	private ComponentInstanceConfiguration buildDefaultInitialCIC(ReconfigurableComponent comp){
+		
+		//TODO: implement
+		
+		return null;
 	}
 
-//	private static RootNode getRootNode(DiscretePort thePort) {
-//		return (RootNode) thePort.getComponent().eContainer().eContainer();
-//	}
 
 }
