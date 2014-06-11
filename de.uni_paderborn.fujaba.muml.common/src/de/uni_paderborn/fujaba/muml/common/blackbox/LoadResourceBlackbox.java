b84899a3f63ef5fee0066402943b334c53a8733d
Ingo Budde <bingo@mail.upb.de>
2014-06-11 14:38:29 +0000

    Added qvto/java blackbox "LoadResourceBlackbox" for usage in pre-reconcile.qvto

---
+++ de.uni_paderborn.fujaba.muml.common/src/de/uni_paderborn/fujaba/muml/common/blackbox/LoadResourceBlackbox.java
@@ -0,0 +1,12 @@
+package de.uni_paderborn.fujaba.muml.common.blackbox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadResourceBlackbox {
+
+	public List<Object> getResourceContents(String resourceURI) {
+		System.out.println("TEST!");
+		return new ArrayList<Object>();
+	}
+}
