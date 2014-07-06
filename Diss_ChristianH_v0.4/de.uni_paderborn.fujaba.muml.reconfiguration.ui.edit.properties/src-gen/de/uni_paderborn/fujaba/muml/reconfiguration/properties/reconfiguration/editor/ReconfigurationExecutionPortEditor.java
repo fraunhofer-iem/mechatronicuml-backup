d10e59a545f3b3d9c3b46ed0bd7f9160c737f856
Davic Schubert <tox>
2014-07-06 12:22:33 +0000

    generated properties for signature of Reconfiguration Rule

--- Diss_ChristianH_v0.4/de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/reconfiguration/properties/reconfiguration/editor/ReconfigurationExecutionPortEditor.java
+++ Diss_ChristianH_v0.4/de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/reconfiguration/properties/reconfiguration/editor/ReconfigurationExecutionPortEditor.java
@@ -23,6 +23,37 @@ public class ReconfigurationExecutionPortEditor
 	protected void createProperties() {
 		super.createProperties();
 
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addReconfigurationExecutionPortInterfaceEntries_GeneralTab_Editor(
+					null, true);
+		}
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addReconfigurationExecutionPortInterfaceEntries_GeneralTab_Editor(
+			String category, boolean front) {
+		addEditorToCategory(
+				category,
+				createReconfigurationExecutionPortInterfaceEntries_GeneralTab_Editor(),
+				front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createReconfigurationExecutionPortInterfaceEntries_GeneralTab_Editor() {
+		de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.ListPropertyEditor(
+				adapterFactory,
+				de.uni_paderborn.fujaba.muml.reconfiguration.ReconfigurationPackage.eINSTANCE
+						.getReconfigurationExecutionPort_InterfaceEntries());
+
+		editor.setTooltipMessage("The interface entries of this reconfiguration execution port. Each interface entry\nspecifies one reconfiguration message that is offered to the parent.");
+
+		return editor;
+
 	}
 
 	//
@@ -57,7 +88,7 @@ public class ReconfigurationExecutionPortEditor
 		public boolean hasTab(java.lang.String tab) {
 			return java.util.Arrays.asList(
 					new java.lang.String[]{"property.tab.general",
-							"property.tab.general",
+							"property.tab.general", "property.tab.general",
 							"property.tab.documentation",
 							"property.tab.extensions", "property.tab.general",
 							"property.tab.general", "property.tab.general",
--- Diss_ChristianH_v0.4/de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/reconfiguration/properties/reconfiguration/editor/ReconfigurationRuleEditor.java
+++ Diss_ChristianH_v0.4/de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/reconfiguration/properties/reconfiguration/editor/ReconfigurationRuleEditor.java
@@ -28,6 +28,10 @@ public abstract class ReconfigurationRuleEditor
 					true);
 		}
 
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addReconfigurationRuleSignature_GeneralTab_Editor(null, false);
+		}
+
 	}
 
 	/**
@@ -51,6 +55,30 @@ public abstract class ReconfigurationRuleEditor
 						.getReconfigurationRule_ReconfiguredComponent());
 
 		editor.setTooltipMessage("The component on which the reconfiguration rule is to be applied.");
+
+		return editor;
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addReconfigurationRuleSignature_GeneralTab_Editor(
+			String category, boolean front) {
+		addEditorToCategory(category,
+				createReconfigurationRuleSignature_GeneralTab_Editor(), front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createReconfigurationRuleSignature_GeneralTab_Editor() {
+		de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.NavigationFeaturePropertyEditor(
+				adapterFactory,
+				de.uni_paderborn.fujaba.muml.reconfiguration.ReconfigurationPackage.eINSTANCE
+						.getReconfigurationRule_Signature());
+
+		editor.setTooltipMessage("The signature of the reconfiguration rule that defines the name as well as the\ninput and output parameters of this reconfiguration rule.");
 
 		return editor;
 
