492aef492027900b7a506d3111126d6378556738
Davic Schubert <tox>
2014-07-06 15:41:38 +0000

    Generated Tabbed Properties for ComponentstoryPattern

---
+++ de.uni_paderborn.fujaba.muml.componentstorypattern.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/componentstorypattern/properties/componentstorypattern/editor/ComponentPartVariableEditor.java
@@ -0,0 +1,96 @@
+package de.uni_paderborn.fujaba.muml.componentstorypattern.properties.componentstorypattern.editor;
+
+/**
+ * @generated
+ */
+public class ComponentPartVariableEditor
+		extends
+			de.uni_paderborn.fujaba.muml.componentstorypattern.properties.componentstorypattern.editor.PartVariableEditor {
+
+	/**
+	 * @generated
+	 */
+	public ComponentPartVariableEditor(String tab,
+			org.eclipse.emf.common.notify.AdapterFactory adapterFactory,
+			org.eclipse.emf.ecore.EClass eClass) {
+		super(tab, adapterFactory, eClass);
+	}
+
+	/**
+	 * @generated
+	 */
+	@Override
+	protected void createProperties() {
+		super.createProperties();
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addComponentPartVariableTriggerEmbeddedComponentExpressions_GeneralTab_Editor(
+					null, false);
+		}
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addComponentPartVariableTriggerEmbeddedComponentExpressions_GeneralTab_Editor(
+			String category, boolean front) {
+		addEditorToCategory(
+				category,
+				createComponentPartVariableTriggerEmbeddedComponentExpressions_GeneralTab_Editor(),
+				front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createComponentPartVariableTriggerEmbeddedComponentExpressions_GeneralTab_Editor() {
+		de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.NavigationFeaturePropertyEditor(
+				adapterFactory,
+				de.uni_paderborn.fujaba.muml.componentstorypattern.ComponentstorypatternPackage.eINSTANCE
+						.getComponentPartVariable_TriggerEmbeddedComponentExpressions());
+
+		return editor;
+
+	}
+
+	//
+	// instantiation
+	//
+
+	/**
+	 * @generated
+	 */
+	public ComponentPartVariableEditor(String tab) {
+		this(
+				tab,
+				de.uni_paderborn.fujaba.muml.componentstorypattern.properties.util.PropertiesUtil.INSTANCE
+						.getAdapterFactory(),
+				de.uni_paderborn.fujaba.muml.componentstorypattern.ComponentstorypatternPackage.eINSTANCE
+						.getComponentPartVariable());
+	}
+
+	/**
+	 * @generated
+	 */
+	public static class Factory
+			implements
+				de.uni_paderborn.fujaba.properties.runtime.factory.IPropertyEditorFactory {
+		@Override
+		public de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createPropertyEditor(
+				String tab) {
+			return new ComponentPartVariableEditor(tab);
+		}
+
+		@Override
+		public boolean hasTab(java.lang.String tab) {
+			return java.util.Arrays.asList(
+					new java.lang.String[]{"property.tab.general",
+							"property.tab.general", "property.tab.general",
+							"property.tab.general", "property.tab.general",
+							"property.tab.general", "property.tab.extensions",
+							"property.tab.documentation"}).contains(tab);
+		}
+	}
+
+}
