eb73ef464c05c19dd0fe575a9227aec620af306b
Davic Schubert <tox>
2014-08-07 09:52:18 +0000

    added APE support for TimingSpecification features

--- de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/reconfiguration/properties/reconfiguration/editor/ExecutionTimingSpecificationThreePhaseEditor.java
+++ de.uni_paderborn.fujaba.muml.reconfiguration.ui.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/reconfiguration/properties/reconfiguration/editor/ExecutionTimingSpecificationThreePhaseEditor.java
@@ -23,6 +23,99 @@ public class ExecutionTimingSpecificationThreePhaseEditor
 	protected void createProperties() {
 		super.createProperties();
 
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addExecutionTimingSpecificationThreePhaseTimeForFading_GeneralTab_Editor(
+					null, false);
+		}
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addExecutionTimingSpecificationThreePhaseTimeForSetup_GeneralTab_Editor(
+					null, false);
+		}
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addExecutionTimingSpecificationThreePhaseTimeForTeardown_GeneralTab_Editor(
+					null, false);
+		}
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addExecutionTimingSpecificationThreePhaseTimeForFading_GeneralTab_Editor(
+			String category, boolean front) {
+		addEditorToCategory(
+				category,
+				createExecutionTimingSpecificationThreePhaseTimeForFading_GeneralTab_Editor(),
+				front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createExecutionTimingSpecificationThreePhaseTimeForFading_GeneralTab_Editor() {
+		de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.NavigationFeaturePropertyEditor(
+				adapterFactory,
+				de.uni_paderborn.fujaba.muml.reconfiguration.ReconfigurationPackage.eINSTANCE
+						.getExecutionTimingSpecificationThreePhase_TimeForFading());
+
+		editor.setTooltipMessage("The time needed for executing the fading function.");
+
+		return editor;
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addExecutionTimingSpecificationThreePhaseTimeForSetup_GeneralTab_Editor(
+			String category, boolean front) {
+		addEditorToCategory(
+				category,
+				createExecutionTimingSpecificationThreePhaseTimeForSetup_GeneralTab_Editor(),
+				front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createExecutionTimingSpecificationThreePhaseTimeForSetup_GeneralTab_Editor() {
+		de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.NavigationFeaturePropertyEditor(
+				adapterFactory,
+				de.uni_paderborn.fujaba.muml.reconfiguration.ReconfigurationPackage.eINSTANCE
+						.getExecutionTimingSpecificationThreePhase_TimeForSetup());
+
+		editor.setTooltipMessage("The time needed for executing the setup phase.");
+
+		return editor;
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addExecutionTimingSpecificationThreePhaseTimeForTeardown_GeneralTab_Editor(
+			String category, boolean front) {
+		addEditorToCategory(
+				category,
+				createExecutionTimingSpecificationThreePhaseTimeForTeardown_GeneralTab_Editor(),
+				front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createExecutionTimingSpecificationThreePhaseTimeForTeardown_GeneralTab_Editor() {
+		de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.NavigationFeaturePropertyEditor(
+				adapterFactory,
+				de.uni_paderborn.fujaba.muml.reconfiguration.ReconfigurationPackage.eINSTANCE
+						.getExecutionTimingSpecificationThreePhase_TimeForTeardown());
+
+		editor.setTooltipMessage("The time needed for executing the teardown phase.");
+
+		return editor;
+
 	}
 
 	//
@@ -56,8 +149,9 @@ public class ExecutionTimingSpecificationThreePhaseEditor
 		@Override
 		public boolean hasTab(java.lang.String tab) {
 			return java.util.Arrays.asList(
-					new java.lang.String[]{"property.tab.extensions"})
-					.contains(tab);
+					new java.lang.String[]{"property.tab.general",
+							"property.tab.general", "property.tab.general",
+							"property.tab.extensions"}).contains(tab);
 		}
 	}
 
