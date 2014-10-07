9e0f26f1a4285ad72c39efb9a06f47ab6e4028ee
Ingo Budde <bingo@mail.upb.de>
2014-10-07 14:27:38 +0000

    Removed unnessesary code

--- PlatformIndependentModeling/de.uni_paderborn.fujaba.muml.common/src/de/uni_paderborn/fujaba/muml/common/MumlCommonPlugin.java
+++ PlatformIndependentModeling/de.uni_paderborn.fujaba.muml.common/src/de/uni_paderborn/fujaba/muml/common/MumlCommonPlugin.java
@@ -18,7 +18,6 @@ import de.uni_paderborn.fujaba.muml.protocol.provider.ProtocolItemProviderAdapte
 
 public class MumlCommonPlugin implements BundleActivator {
 
-	private ComposedAdapterFactory adapterFactory;
 
 	private static MumlCommonPlugin instance;
 
@@ -29,7 +28,7 @@ public class MumlCommonPlugin implements BundleActivator {
 	static BundleContext getContext() {
 		return context;
 	}
-
+	
 	public static MumlCommonPlugin getInstance() {
 		return MumlCommonPlugin.instance;
 	}
@@ -43,7 +42,6 @@ public class MumlCommonPlugin implements BundleActivator {
 	 */
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
-		adapterFactory = createAdapterFactory();
 		MumlCommonPlugin.context = bundleContext;
 		MumlCommonPlugin.instance = this;
 	}
@@ -56,70 +54,8 @@ public class MumlCommonPlugin implements BundleActivator {
 	 */
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
-		adapterFactory.dispose();
-		adapterFactory = null;
 		MumlCommonPlugin.context = null;
 		MumlCommonPlugin.instance = null;
-	}
-
-	protected ComposedAdapterFactory createAdapterFactory() {
-		ArrayList<AdapterFactory> factories = new ArrayList<AdapterFactory>();
-		fillDefaultItemProviderFactories(factories);
-		return new ComposedAdapterFactory(factories);
-	}
-
-	public static void fillDefaultItemProviderFactories(List<AdapterFactory> factories) {
-		List<AdapterFactory> positivePriorityFactories = de.uni_paderborn.fujaba.common.FujabaCommonPlugin
-				.getInstance().getCustomItemProviderAdapterFactories(ID, true);
-		List<AdapterFactory> negativePriorityFactories = de.uni_paderborn.fujaba.common.FujabaCommonPlugin
-				.getInstance().getCustomItemProviderAdapterFactories(ID, false);
-
-		// Custom Factories with positive priority
-		factories.addAll(positivePriorityFactories);
-
-		// Default Factories
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.component.provider.ComponentItemProviderAdapterFactory());
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.constraint.provider.ConstraintItemProviderAdapterFactory());
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.types.provider.TypesItemProviderAdapterFactory());
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.behavior.provider.BehaviorItemProviderAdapterFactory());
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.connector.provider.ConnectorItemProviderAdapterFactory());
-
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.valuetype.provider.ValuetypeItemProviderAdapterFactory());
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.instance.provider.InstanceItemProviderAdapterFactory());
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.protocol.provider.ProtocolItemProviderAdapterFactory());
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.realtimestatechart.provider.RealtimestatechartItemProviderAdapterFactory());
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.msgtype.provider.MsgtypeItemProviderAdapterFactory());
-	/*	factories
-				.add(new de.uni_paderborn.fujaba.muml.deployment.provider.DeploymentItemProviderAdapterFactory()); */
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.types.provider.TypesItemProviderAdapterFactory());
-		factories
-				.add(new de.uni_paderborn.fujaba.muml.connector.provider.ConnectorItemProviderAdapterFactory());
-		factories.add(new CoreItemProviderAdapterFactory());
-		factories.add(new ExpressionsItemProviderAdapterFactory());
-		factories.add(new EcoreItemProviderAdapterFactory());
-		factories.add(new ModelinstanceItemProviderAdapterFactory());
-		factories.add(new ProtocolItemProviderAdapterFactory());
-
-		// Custom Factories with negative priority
-		factories.addAll(negativePriorityFactories);
-		
-		factories.add(new ResourceItemProviderAdapterFactory());
-		factories.add(new ReflectiveItemProviderAdapterFactory());
-	}
-
-	public AdapterFactory getItemProvidersAdapterFactory() {
-		return adapterFactory;
 	}
 
 }
