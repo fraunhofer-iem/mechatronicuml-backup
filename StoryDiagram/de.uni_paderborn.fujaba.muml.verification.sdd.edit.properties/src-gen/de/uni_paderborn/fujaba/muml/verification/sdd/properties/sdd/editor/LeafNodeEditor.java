323bca1965471612f0e265d8c0556c927858895d
Davic Schubert <tox>
2014-10-19 19:23:15 +0000

    ape properties for sdd

---
+++ StoryDiagram/de.uni_paderborn.fujaba.muml.verification.sdd.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/verification/sdd/properties/sdd/editor/LeafNodeEditor.java
@@ -0,0 +1,94 @@
+package de.uni_paderborn.fujaba.muml.verification.sdd.properties.sdd.editor;
+
+/**
+ * @generated
+ */
+public class LeafNodeEditor
+		extends
+			de.uni_paderborn.fujaba.muml.verification.sdd.properties.sdd.editor.NodeEditor {
+
+	/**
+	 * @generated
+	 */
+	public LeafNodeEditor(String tab,
+			org.eclipse.emf.common.notify.AdapterFactory adapterFactory,
+			org.eclipse.emf.ecore.EClass eClass) {
+		super(tab, adapterFactory, eClass);
+	}
+
+	/**
+	 * @generated
+	 */
+	@Override
+	protected void createProperties() {
+		super.createProperties();
+
+		addSubCategory("de.uni_paderborn.fujaba.properties.category.Booleans",
+				"Booleans", org.eclipse.swt.SWT.HORIZONTAL, true);
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addValue_GeneralTab_Editor(
+					"de.uni_paderborn.fujaba.properties.category.Booleans",
+					true);
+		}
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addValue_GeneralTab_Editor(String category, boolean front) {
+		addEditorToCategory(category, createValue_GeneralTab_Editor(), front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createValue_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = de.uni_paderborn.fujaba.muml.verification.sdd.SDDPackage.eINSTANCE
+				.getLeafNode_Value();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.CheckboxPropertyEditor(
+				adapterFactory, feature);
+
+		return editor;
+
+	}
+
+	//
+	// instantiation
+	//
+
+	/**
+	 * @generated
+	 */
+	public LeafNodeEditor(String tab) {
+		this(
+				tab,
+				de.uni_paderborn.fujaba.muml.verification.sdd.properties.util.PropertiesUtil.INSTANCE
+						.getAdapterFactory(),
+				de.uni_paderborn.fujaba.muml.verification.sdd.SDDPackage.eINSTANCE
+						.getLeafNode());
+	}
+
+	/**
+	 * @generated
+	 */
+	public static class Factory
+			implements
+				de.uni_paderborn.fujaba.properties.runtime.factory.IPropertyEditorFactory {
+		@Override
+		public de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createPropertyEditor(
+				String tab) {
+			return new LeafNodeEditor(tab);
+		}
+
+		@Override
+		public boolean hasTab(java.lang.String tab) {
+			return java.util.Arrays.asList(
+					new java.lang.String[]{"property.tab.general",
+							"property.tab.general", "property.tab.general"})
+					.contains(tab);
+		}
+	}
+
+}
---
+++ StoryDiagram/de.uni_paderborn.fujaba.muml.verification.sdd.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/verification/sdd/properties/sdd/editor/PatternNodeEditor.java
@@ -0,0 +1,106 @@
+package de.uni_paderborn.fujaba.muml.verification.sdd.properties.sdd.editor;
+
+/**
+ * @generated
+ */
+public abstract class PatternNodeEditor
+		extends
+			org.storydriven.core.properties.core.editor.NamedElementEditor {
+
+	/**
+	 * @generated
+	 */
+	public PatternNodeEditor(String tab,
+			org.eclipse.emf.common.notify.AdapterFactory adapterFactory,
+			org.eclipse.emf.ecore.EClass eClass) {
+		super(tab, adapterFactory, eClass);
+	}
+
+	/**
+	 * @generated
+	 */
+	@Override
+	protected void createProperties() {
+		super.createProperties();
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addType_GeneralTab_Editor(null, true);
+		}
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addOutgoingEdges_GeneralTab_Editor(
+					"de.uni_paderborn.fujaba.properties.category.Lists", true);
+		}
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addIncomingEdges_GeneralTab_Editor(
+					"de.uni_paderborn.fujaba.properties.category.Lists", true);
+		}
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addType_GeneralTab_Editor(String category, boolean front) {
+		addEditorToCategory(category, createType_GeneralTab_Editor(), front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createType_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = de.uni_paderborn.fujaba.muml.verification.sdd.SDDPackage.eINSTANCE
+				.getPatternNode_Type();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.OptionPropertyEditor(
+				adapterFactory, feature);
+
+		return editor;
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addOutgoingEdges_GeneralTab_Editor(String category,
+			boolean front) {
+		addEditorToCategory(category, createOutgoingEdges_GeneralTab_Editor(),
+				front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createOutgoingEdges_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = de.uni_paderborn.fujaba.muml.verification.sdd.SDDPackage.eINSTANCE
+				.getNode_OutgoingEdges();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.ListPropertyEditor(
+				adapterFactory, feature);
+
+		return editor;
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addIncomingEdges_GeneralTab_Editor(String category,
+			boolean front) {
+		addEditorToCategory(category, createIncomingEdges_GeneralTab_Editor(),
+				front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createIncomingEdges_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = de.uni_paderborn.fujaba.muml.verification.sdd.SDDPackage.eINSTANCE
+				.getNode_IncomingEdges();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.ListPropertyEditor(
+				adapterFactory, feature);
+
+		return editor;
+
+	}
+
+}
