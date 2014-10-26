74e91fda79eca110cea9a06857c8ccd8890a14f9
Davic Schubert <tox>
2014-10-26 13:41:10 +0000

    added ape properties for deadline of ControllerExchangeNode

--- ReconfigurationModeling/de.uni_paderborn.fujaba.muml.componentstorydiagram.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/componentstorydiagram/properties/componentstorydiagram/editor/ControllerExchangeNodeEditor.java
+++ ReconfigurationModeling/de.uni_paderborn.fujaba.muml.componentstorydiagram.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/componentstorydiagram/properties/componentstorydiagram/editor/ControllerExchangeNodeEditor.java
@@ -23,6 +23,32 @@ public class ControllerExchangeNodeEditor
 	protected void createProperties() {
 		super.createProperties();
 
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addDeadline_GeneralTab_Editor(null, false);
+		}
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addDeadline_GeneralTab_Editor(String category, boolean front) {
+		addEditorToCategory(category, createDeadline_GeneralTab_Editor(), front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createDeadline_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = de.uni_paderborn.fujaba.muml.componentstorydiagram.ComponentstorydiagramPackage.eINSTANCE
+				.getControllerExchangeNode_Deadline();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.NavigationFeaturePropertyEditor(
+				adapterFactory, feature);
+
+		editor.setTooltipMessage("The relative deadline denotes the minimum and maximum duration of the controller exchange. In case of a fading function, the deadline denotes the time interval in which both controllers are active and during which their out-ports are faded. In case of a flat switching, the deadline denotes the time interval in which the spline computing by the flat switching is used as a value of the out-port.");
+
+		return editor;
+
 	}
 
 	//
@@ -55,7 +81,8 @@ public class ControllerExchangeNodeEditor
 
 		@Override
 		public boolean hasTab(java.lang.String tab) {
-			return java.util.Arrays.asList(new java.lang.String[]{}).contains(
+			return java.util.Arrays.asList(
+					new java.lang.String[]{"property.tab.general"}).contains(
 					tab);
 		}
 	}
