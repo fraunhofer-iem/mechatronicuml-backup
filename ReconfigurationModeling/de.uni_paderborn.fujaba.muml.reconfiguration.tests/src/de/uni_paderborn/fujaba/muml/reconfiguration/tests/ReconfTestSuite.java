391c9804de6b0668c6e594efc95e7be762b4fbe2
Andreas Dann <adann@mail.upb.de>
2014-10-28 14:08:06 +0000

    launch configuration for JUnit Plugins on Buckminster (anyway: we need a documentation of buckminster/jenkins in the wiki/trac)

---
+++ ReconfigurationModeling/de.uni_paderborn.fujaba.muml.reconfiguration.tests/src/de/uni_paderborn/fujaba/muml/reconfiguration/tests/ReconfTestSuite.java
@@ -0,0 +1,14 @@
+package de.uni_paderborn.fujaba.muml.reconfiguration.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import de.uni_paderborn.fujaba.muml.tests.gmf.PimGMFMapTest;
+import de.uni_paderborn.fujaba.muml.tests.packages.PimPluginXmlPackageTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ReconfDiagramPluginXmlTest.class })
+public class ReconfTestSuite {
+	// empty
+}
