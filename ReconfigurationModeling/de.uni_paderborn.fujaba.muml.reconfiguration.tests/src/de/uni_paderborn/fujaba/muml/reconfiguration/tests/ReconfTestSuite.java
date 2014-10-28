985bddfaa00a04ba8f67301242bc8a20a557c85c
Andreas Dann <adann@mail.upb.de>
2014-10-28 14:22:08 +0000

    deleted invalid imports

--- ReconfigurationModeling/de.uni_paderborn.fujaba.muml.reconfiguration.tests/src/de/uni_paderborn/fujaba/muml/reconfiguration/tests/ReconfTestSuite.java
+++ ReconfigurationModeling/de.uni_paderborn.fujaba.muml.reconfiguration.tests/src/de/uni_paderborn/fujaba/muml/reconfiguration/tests/ReconfTestSuite.java
@@ -4,8 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import de.uni_paderborn.fujaba.muml.tests.gmf.PimGMFMapTest;
-import de.uni_paderborn.fujaba.muml.tests.packages.PimPluginXmlPackageTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ ReconfDiagramPluginXmlTest.class })
