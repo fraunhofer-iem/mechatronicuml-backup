9b3b1d0d0718fa3d11a33f2bc6c90a8e55f1dc11
Marcus Hüwe <mhuewe@mail.upb.de>
2014-11-07 00:04:03 +0000

    - migrated ilp tests to the new test classes
    Removed generated (EMF) metamodel tests.

---
+++ de.uni_paderborn.fujaba.muml.allocation.ilp.tests/src/de/uni_paderborn/fujaba/muml/allocation/ilp/tests/ILPTestSuite.java
@@ -0,0 +1,13 @@
+package de.uni_paderborn.fujaba.muml.allocation.ilp.tests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	ILPMetamodelTest.class,
+	ILPGenmodelTest.class,
+	ILPValidateMetamodel.class
+})
+public class ILPTestSuite {
+}
