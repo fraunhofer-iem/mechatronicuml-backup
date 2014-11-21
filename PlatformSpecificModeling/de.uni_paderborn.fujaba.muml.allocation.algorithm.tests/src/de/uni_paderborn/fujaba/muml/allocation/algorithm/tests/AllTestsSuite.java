7ed981711d0e35097699fc6e9559a3e3be123dc3
Andreas Dann <adann@mail.upb.de>
2014-11-21 17:59:48 +0000

    this shall fix the execution of junit xtext tests

--- PlatformSpecificModeling/de.uni_paderborn.fujaba.muml.allocation.algorithm.tests/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/tests/AllTestsSuite.java
+++ PlatformSpecificModeling/de.uni_paderborn.fujaba.muml.allocation.algorithm.tests/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/tests/AllTestsSuite.java
@@ -1,14 +1,27 @@
 package de.uni_paderborn.fujaba.muml.allocation.algorithm.tests;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.google.common.annotations.Beta;
+
+import de.uni_paderborn.fujaba.muml.allocation.ilp.lpsolve.xtext.LPSolveStandaloneSetup;
+import de.uni_paderborn.fujaba.muml.allocation.language.AllocationSpecificationLanguageStandaloneSetup;
+
 @RunWith(Suite.class)
+
 @SuiteClasses({
 	Specification2ILPTestSuite.class,
 	LPSolveBasedAlgorithmTest.class
 })
 public class AllTestsSuite {
+	 @BeforeClass
+	 public static void setUpClass() {      
+		 AllocationSpecificationLanguageStandaloneSetup.doSetup();
+		 LPSolveStandaloneSetup.doSetup();
+
+	    }
 
 }
