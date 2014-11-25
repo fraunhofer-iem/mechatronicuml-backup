80e5958e6014191e7fa97a2cd384a246f1500f02
Davic Schubert <tox>
2014-11-25 15:53:11 +0000

    Added APE editor for parameterBindings reference of ReconfigurationRuleCallExpression

--- ReconfigurationModeling/de.uni_paderborn.fujaba.muml.reconfiguration.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/reconfiguration/properties/expression/editor/ReconfigurationRuleCallExpressionEditor.java
+++ ReconfigurationModeling/de.uni_paderborn.fujaba.muml.reconfiguration.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/reconfiguration/properties/expression/editor/ReconfigurationRuleCallExpressionEditor.java
@@ -27,6 +27,10 @@ public class ReconfigurationRuleCallExpressionEditor
 			addReconfigurationRule_GeneralTab_Editor(null, true);
 		}
 
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addParameterBindings_GeneralTab_Editor(null, true);
+		}
+
 	}
 
 	/**
@@ -48,6 +52,30 @@ public class ReconfigurationRuleCallExpressionEditor
 				adapterFactory, feature);
 
 		editor.setTooltipMessage("The reconfiguration rule that is called by this expression.");
+
+		return editor;
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addParameterBindings_GeneralTab_Editor(String category,
+			boolean front) {
+		addEditorToCategory(category,
+				createParameterBindings_GeneralTab_Editor(), front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createParameterBindings_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = de.uni_paderborn.fujaba.muml.reconfiguration.expression.ExpressionPackage.eINSTANCE
+				.getReconfigurationRuleCallExpression_ParameterBindings();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.ListPropertyEditor(
+				adapterFactory, feature);
+
+		editor.setTooltipMessage("The parameter bindings assign a value to each of the parameters of the \nreconfiguration rule that is called by this expression.");
 
 		return editor;
 
@@ -85,6 +113,7 @@ public class ReconfigurationRuleCallExpressionEditor
 		public boolean hasTab(java.lang.String tab) {
 			return java.util.Arrays.asList(
 					new java.lang.String[]{"property.tab.general",
+							"property.tab.general",
 							"property.tab.documentation",
 							"property.tab.extensions"}).contains(tab);
 		}
