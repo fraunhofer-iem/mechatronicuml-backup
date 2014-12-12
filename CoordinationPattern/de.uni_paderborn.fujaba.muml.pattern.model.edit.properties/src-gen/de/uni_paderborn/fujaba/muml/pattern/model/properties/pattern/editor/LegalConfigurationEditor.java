66d9e9746a9d060bd1e825545bfe133082d82734
Sebastian Thiele <sthiele2>
2014-12-12 16:59:57 +0000

    added model.edit.properties for the pattern model

---
+++ CoordinationPattern/de.uni_paderborn.fujaba.muml.pattern.model.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/pattern/model/properties/pattern/editor/LegalConfigurationEditor.java
@@ -0,0 +1,90 @@
+package de.uni_paderborn.fujaba.muml.pattern.model.properties.pattern.editor;
+
+/**
+ * @generated
+ */
+public class LegalConfigurationEditor
+		extends
+			org.storydriven.core.properties.core.editor.CommentableElementEditor {
+
+	/**
+	 * @generated
+	 */
+	public LegalConfigurationEditor(String tab,
+			org.eclipse.emf.common.notify.AdapterFactory adapterFactory,
+			org.eclipse.emf.ecore.EClass eClass) {
+		super(tab, adapterFactory, eClass);
+	}
+
+	/**
+	 * @generated
+	 */
+	@Override
+	protected void createProperties() {
+		super.createProperties();
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addName_GeneralTab_Editor(null, true);
+		}
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addName_GeneralTab_Editor(String category, boolean front) {
+		addEditorToCategory(category, createName_GeneralTab_Editor(), front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createName_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = org.storydriven.core.CorePackage.eINSTANCE
+				.getNamedElement_Name();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.TextPropertyEditor(
+				adapterFactory, feature, false);
+
+		editor.setTooltipMessage("The name attribute of a meta-model element.");
+
+		return editor;
+
+	}
+
+	//
+	// instantiation
+	//
+
+	/**
+	 * @generated
+	 */
+	public LegalConfigurationEditor(String tab) {
+		this(
+				tab,
+				de.uni_paderborn.fujaba.muml.pattern.model.properties.util.PropertiesUtil.INSTANCE
+						.getAdapterFactory(), pattern.PatternPackage.eINSTANCE
+						.getLegalConfiguration());
+	}
+
+	/**
+	 * @generated
+	 */
+	public static class Factory
+			implements
+				de.uni_paderborn.fujaba.properties.runtime.factory.IPropertyEditorFactory {
+		@Override
+		public de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createPropertyEditor(
+				String tab) {
+			return new LegalConfigurationEditor(tab);
+		}
+
+		@Override
+		public boolean hasTab(java.lang.String tab) {
+			return java.util.Arrays.asList(
+					new java.lang.String[]{"property.tab.documentation",
+							"property.tab.extensions", "property.tab.general"})
+					.contains(tab);
+		}
+	}
+
+}
