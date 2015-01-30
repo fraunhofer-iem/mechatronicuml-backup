ce13c8bc06899fc26b2d21a7fa849b8af72457ab
Sebastian Thiele <sthiele2>
2015-01-30 14:41:21 +0000

    [Coordination Pattern diagram] Deleted generated code

--- CoordinationPattern/de.uni_paderborn.fujaba.muml.pattern.diagram/src/de/uni_paderborn/fujaba/muml/pattern/diagram/edit/helpers/MessageTypeEditHelper.java
+++
@@ -1,9 +0,0 @@
-package de.uni_paderborn.fujaba.muml.pattern.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class MessageTypeEditHelper
-		extends
-		de.uni_paderborn.fujaba.muml.pattern.diagram.edit.helpers.MumlBaseEditHelper {
-}
