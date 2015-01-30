db01fec0ad5b6d81700daf4cf7a7f423b51ba4bd
Sebastian Thiele <sthiele2>
2015-01-30 14:44:50 +0000

    [Coordination Pattern diagram editor] Regenerated code.

---
+++ CoordinationPattern/de.uni_paderborn.fujaba.muml.pattern.diagram/src/de/uni_paderborn/fujaba/muml/pattern/diagram/edit/helpers/RoleEditHelper.java
@@ -0,0 +1,9 @@
+package de.uni_paderborn.fujaba.muml.pattern.diagram.edit.helpers;
+
+/**
+ * @generated
+ */
+public class RoleEditHelper
+		extends
+		de.uni_paderborn.fujaba.muml.pattern.diagram.edit.helpers.MumlBaseEditHelper {
+}
