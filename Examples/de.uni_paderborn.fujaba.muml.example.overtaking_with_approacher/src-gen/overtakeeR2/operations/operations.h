0a46cac9ac5de455047cc47494746a2e298e08bb
Goran Piskatchev <goranp@mail.upb.de>
2015-02-03 16:29:14 +0000

    generated C code for the model

---
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen/overtakeeR2/operations/operations.h
@@ -0,0 +1,6 @@
+#ifndef OPERATIONS_H_
+#define OPERATIONS_H_
+
+#include "../lib/types.h"
+
+#endif /*OPERATIONS_H_ */
---
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen/overtakerR1/apiCallCode/implementations.c
@@ -0,0 +1,45 @@
+#include "implementations.h"
+/** Start of user code user includes **/ 
+
+/**End of user code**/
+
+		void overtakerMotorR_velocity_InitApi(){
+/** Start of user code overtakerMotorR_velocity_InitApi **/ 
+		
+/**End of user code**/
+}	
+		void overtakerMotorR_velocity_ExecApi(int velocity){
+/** Start of user code overtakerMotorR_velocity_ExecApi **/ 
+		
+/**End of user code**/
+}	
+		void overtakerMotorL_velocity_InitApi(){
+/** Start of user code overtakerMotorL_velocity_InitApi **/ 
+		
+/**End of user code**/
+}	
+		void overtakerMotorL_velocity_ExecApi(int velocity){
+/** Start of user code overtakerMotorL_velocity_ExecApi **/ 
+		
+/**End of user code**/
+}	
+		void overtakerDistance_distance_InitApi(){
+/** Start of user code overtakerDistance_distance_InitApi **/ 
+		
+/**End of user code**/
+}	
+		int overtakerDistance_distance_ExecApi(){
+/** Start of user code overtakerDistance_distance_ExecApi **/ 
+		
+/**End of user code**/
+}	
+		void overtakerLine_lineLight_InitApi(){
+/** Start of user code overtakerLine_lineLight_InitApi **/ 
+		
+/**End of user code**/
+}	
+		int overtakerLine_lineLight_ExecApi(){
+/** Start of user code overtakerLine_lineLight_ExecApi **/ 
+		
+/**End of user code**/
+}	
