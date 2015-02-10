eeceec5f870de17a7927da7f600927a5755b23e7
Marcus Hüwe <mhuewe@mail.upb.de>
2015-02-10 13:22:51 +0000

    [Allocation] minor cleanups

--- de.uni_paderborn.fujaba.muml.allocation.language.xtext/xtend-gen/de/uni_paderborn/fujaba/muml/allocation/language/typing/TypesUtil.java
+++ de.uni_paderborn.fujaba.muml.allocation.language.xtext/xtend-gen/de/uni_paderborn/fujaba/muml/allocation/language/typing/TypesUtil.java
@@ -2,6 +2,7 @@ package de.uni_paderborn.fujaba.muml.allocation.language.typing;
 
 import com.google.common.base.Objects;
 import de.uni_paderborn.fujaba.muml.allocation.language.cs.ComponentResourceTupleDescriptorCS;
+import de.uni_paderborn.fujaba.muml.allocation.language.cs.EvaluatableElementCS;
 import de.uni_paderborn.fujaba.muml.allocation.language.cs.LocationConstraintCS;
 import de.uni_paderborn.fujaba.muml.allocation.language.cs.LocationTupleDescriptorCS;
 import de.uni_paderborn.fujaba.muml.allocation.language.cs.QoSDimensionCS;
@@ -11,10 +12,12 @@ import de.uni_paderborn.fujaba.muml.allocation.language.cs.ValueTupleDescriptorC
 import de.uni_paderborn.fujaba.muml.allocation.language.cs.WeightingComponentResourceTupleElementCS;
 import de.uni_paderborn.fujaba.muml.hardware.hwresourceinstance.HwresourceinstancePackage;
 import de.uni_paderborn.fujaba.muml.instance.InstancePackage;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
@@ -30,10 +33,8 @@ import org.eclipse.ocl.examples.pivot.manager.TupleTypeManager;
 import org.eclipse.ocl.examples.pivot.utilities.PivotUtil;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
-import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.MapExtensions;
 import org.eclipse.xtext.xbase.lib.Pair;
-import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
 @SuppressWarnings("all")
 public class TypesUtil {
@@ -159,8 +160,8 @@ public class TypesUtil {
     HashMap<String, EClass> _xblockexpression = null;
     {
       final HashMap<String, EClass> namedParts = CollectionLiterals.<String, EClass>newHashMap();
-      final Procedure1<ComponentResourceTupleDescriptorCS> _function = new Procedure1<ComponentResourceTupleDescriptorCS>() {
-        public void apply(final ComponentResourceTupleDescriptorCS t) {
+      final Consumer<ComponentResourceTupleDescriptorCS> _function = new Consumer<ComponentResourceTupleDescriptorCS>() {
+        public void accept(final ComponentResourceTupleDescriptorCS t) {
           String _instance = t.getInstance();
           Pair<String, EClass> _mappedTo = Pair.<String, EClass>of(_instance, InstancePackage.Literals.COMPONENT_INSTANCE);
           String _hwresinstance = t.getHwresinstance();
@@ -169,7 +170,7 @@ public class TypesUtil {
             Collections.<String, EClass>unmodifiableMap(CollectionLiterals.<String, EClass>newHashMap(_mappedTo, _mappedTo_1)));
         }
       };
-      IterableExtensions.<ComponentResourceTupleDescriptorCS>forEach(tupleDescriptorList, _function);
+      tupleDescriptorList.forEach(_function);
       _xblockexpression = namedParts;
     }
     return _xblockexpression;
@@ -289,5 +290,42 @@ public class TypesUtil {
       _xblockexpression = TypesUtil.createTupleType(metaModelManager, namedParts);
     }
     return _xblockexpression;
+  }
+  
+  protected static Type _createType(final EvaluatableElementCS element) {
+    throw new IllegalArgumentException(("unexpected element: " + element));
+  }
+  
+  protected static Type _createType(final LocationConstraintCS constraintCS) {
+    return TypesUtil.createLocationConstraintType(constraintCS);
+  }
+  
+  protected static Type _createType(final RequiredHardwareResourceInstanceConstraintCS constraintCS) {
+    return TypesUtil.createReqHWResInstanceConstraintType(constraintCS);
+  }
+  
+  protected static Type _createType(final ResourceConstraintCS constraintCS) {
+    return TypesUtil.createResourceConstraintType(constraintCS);
+  }
+  
+  protected static Type _createType(final QoSDimensionCS qosDimensionCS) {
+    return TypesUtil.createQoSDimensionType(qosDimensionCS);
+  }
+  
+  public static Type createType(final EvaluatableElementCS constraintCS) {
+    if (constraintCS instanceof LocationConstraintCS) {
+      return _createType((LocationConstraintCS)constraintCS);
+    } else if (constraintCS instanceof RequiredHardwareResourceInstanceConstraintCS) {
+      return _createType((RequiredHardwareResourceInstanceConstraintCS)constraintCS);
+    } else if (constraintCS instanceof ResourceConstraintCS) {
+      return _createType((ResourceConstraintCS)constraintCS);
+    } else if (constraintCS instanceof QoSDimensionCS) {
+      return _createType((QoSDimensionCS)constraintCS);
+    } else if (constraintCS != null) {
+      return _createType(constraintCS);
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        Arrays.<Object>asList(constraintCS).toString());
+    }
   }
 }
