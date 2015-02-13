d10e9f5d8d6d64f95e1e45c12d0e75c22ddfabb3
Sebastian Thiele <sthiele2>
2015-02-13 11:51:22 +0000

    [CoordinationProtocol Diagram Editor, CoordinationPattern Diagram Editor] Beautification, see ticket #1095 , removed the solid line in the messagebuffercompartment

--- CoordinationPattern/de.uni_paderborn.fujaba.muml.pattern.diagram/src/de/uni_paderborn/fujaba/muml/pattern/diagram/edit/parts/MessageBufferMessageBufferCompartmentEditPart.java
+++ CoordinationPattern/de.uni_paderborn.fujaba.muml.pattern.diagram/src/de/uni_paderborn/fujaba/muml/pattern/diagram/edit/parts/MessageBufferMessageBufferCompartmentEditPart.java
@@ -105,6 +105,9 @@ public class MessageBufferMessageBufferCompartmentEditPart extends
 		installEditPolicy(
 				EditPolicyRoles.CANONICAL_ROLE,
 				new de.uni_paderborn.fujaba.muml.pattern.diagram.edit.policies.MessageBufferMessageBufferCompartmentCanonicalEditPolicy());
+		installEditPolicy(
+				de.uni_paderborn.fujaba.common.edit.policies.EditPolicyRoles.COMPARTMENT_BORDER_ROLE,
+				new de.uni_paderborn.fujaba.common.edit.policies.compartment.BorderlessCompartmentEditPolicy());
 	}
 
 	/**
