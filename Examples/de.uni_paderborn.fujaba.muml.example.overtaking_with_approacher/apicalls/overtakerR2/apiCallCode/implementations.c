c2a2c9b126727325857e7f5ee8ab1b57e547284c
Andreas Dann <adann@mail.upb.de>
2015-02-14 17:54:02 +0000

    added appi-calls for demonstrator

---
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/apicalls/overtakerR2/apiCallCode/implementations.c
@@ -0,0 +1,19 @@
+#include "implementations.h"
+/** Start of user code user includes **/ 
+
+void initAll(){
+	ecrobot_init_nxtcolorsensor(NXT_PORT_S1, NXT_COLORSENSOR);
+}
+/**End of user code**/
+
+		void approacherColor_color_InitApi(){
+/** Start of user code approacherColor_color_InitApi **/ 
+		
+/**End of user code**/
+}	
+		int approacherColor_color_ExecApi(){
+/** Start of user code approacherColor_color_ExecApi **/ 
+					return ecrobot_get_nxtcolorsensor_id(NXT_PORT_S1)
+
+/**End of user code**/
+}	
---
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/apicalls/overtakerR2/apiCallCode/implementations.h
@@ -0,0 +1,35 @@
+#include "kernel.h"
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+/**
+ * @file 
+ * @author generated
+ * @brief Specification of API-Calls for the Continuous Port Instances
+ * @details This files contains the implementation of the API-Bindings for the Continuous Port Instance as it is specified in the API-Port-Mapping.
+ *          The abstract API-Calls which are used in the Realtime-Statecharts are implemented here.
+ */
+
+#ifndef APICALL_H_
+#define APICALL_H_
+
+#include "../lib/types.h"
+
+ /***
+
+ 		method to initialize all
+ 	*/
+
+ 		void initAll();
+
+/**
+ * @brief Implemtation of the Parameterbinding: approacherColor_color_InitApi of the Port-Instance: PortInstance: color of the ComponentInstance: approacherColor 
+ */
+		void approacherColor_color_InitApi();	
+/**
+ * @brief Implemtation of the Parameterbinding: approacherColor_color_ExecApi of the Port-Instance: PortInstance: color of the ComponentInstance: approacherColor 
+ */
+		int approacherColor_color_ExecApi();	
+
+
+
+#endif /*APICALL_H_ */
