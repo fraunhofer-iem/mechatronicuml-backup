951f460294cad08e8093dc45e10e76b8b2aaf753
Andreas Dann <adann@mail.upb.de>
2015-02-17 14:14:07 +0000

    approacherR2 working code

--- Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen-customized/approacherR2/apiCallCode/implementations.c
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen-customized/approacherR2/apiCallCode/implementations.c
@@ -3,6 +3,11 @@
 
 /**End of user code**/
 
+void initAll(){
+	ecrobot_init_nxtcolorsensor(NXT_PORT_S1, NXT_COLORSENSOR);
+}
+
+
 		void approacherColor_color_InitApi(){
 /** Start of user code approacherColor_color_InitApi **/ 
 		
@@ -10,6 +15,7 @@
 }	
 		int approacherColor_color_ExecApi(){
 /** Start of user code approacherColor_color_ExecApi **/ 
-		
+									return ecrobot_get_nxtcolorsensor_id(NXT_PORT_S1);
+
 /**End of user code**/
 }	
