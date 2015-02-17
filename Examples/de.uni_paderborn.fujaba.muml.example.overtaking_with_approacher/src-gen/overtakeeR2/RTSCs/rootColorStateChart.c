b397e63be0c37dca6d3a0eba02469c394ffdc8f5
Goran Piskatchev <goranp@mail.upb.de>
2015-02-17 14:38:41 +0000

    fixed extra wifi ports

---
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen/overtakeeR2/RTSCs/rootColorStateChart.c
@@ -0,0 +1,392 @@
+
+		////////////////////////////////////////////////////////////////////////////////
+		// Includes
+		////////////////////////////////////////////////////////////////////////////////
+		#include <stdio.h>
+		#include <stdlib.h>
+		#include "../GlobalIdentifier.h"
+		#include "rootColorStateChart.h"
+		
+		
+
+
+
+		
+		////////////////////////////////////////////////////////////////////////////////
+		// Initialization
+		////////////////////////////////////////////////////////////////////////////////
+		void initializeROOTCOLOR_COLOR_STATE_STATE_Color_color_behaviorRegion(
+				RootColorStateChart* stateChart) {
+		
+			stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_Color_color_behavior_Color_color_behavior =
+					ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE;
+		
+			// execute entry actions
+			// nothing to do
+		
+			initializeROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmissionRegion(
+					stateChart);
+			initializeROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecutionRegion(
+					stateChart);
+		}
+		void initializeROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmissionRegion(
+				RootColorStateChart* stateChart) {
+			Clock_reset(
+					stateChart->clockSending_ofcurrentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmissionClock);
+		
+			stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmission =
+					ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PERIODICTRANSMISSION_PERIODICTRANSMISSION_PERIODICSENDING_STATE;
+		
+			// execute entry actions
+		
+			Clock_reset(
+					stateChart->clockSending_ofcurrentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmissionClock);
+		
+		}
+		void initializeROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecutionRegion(
+				RootColorStateChart* stateChart) {
+			Clock_reset(
+					stateChart->_apiClock_ofcurrentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecutionClock);
+		
+			stateChart->TimeBetweenApiExececutions = 20;
+		
+			stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecution =
+					ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_APIEXECUTION_APIEXECUTION_EXECINITCOMMAND_STATE;
+		
+			// execute entry actions
+		
+			stateChart->initApi();
+		
+		}
+		
+		void RootColorStateChart_initialize(RootColorStateChart* stateChart) {
+		
+			stateChart->color_apiValue = 0;
+		
+			stateChart->currentStateOfRootColor = ROOTCOLOR_COLOR_STATE_STATE;
+		
+			// execute entry actions
+		
+			// nothing to do
+		
+			initializeROOTCOLOR_COLOR_STATE_STATE_Color_color_behaviorRegion(
+					stateChart);
+		}
+		
+		
+
+		////////////////////////////////////////////////////////////////////////////////
+		// Construction and destruction
+		////////////////////////////////////////////////////////////////////////////////
+		RootColorStateChart* RootColorStateChart_create(
+				ColorComponent* parentComponent) {
+			RootColorStateChart* stateChart = (RootColorStateChart*) malloc(
+					sizeof(RootColorStateChart));
+			if (stateChart != NULL) {
+				stateChart->parentComponent = parentComponent;
+		
+				/*
+				 if (stateChart->parentComponent != NULL
+				 ) {
+				 RootColorStateChart_initialize(stateChart);
+				 } else {
+				 fprintf(stderr, "Initialization of RootColorStateChart failed\n");
+		
+				 RootColorStateChart_destroy(stateChart);
+				 stateChart = NULL;
+				 }
+				 } else {
+				 fprintf(stderr, "Creation of RootColorStateChart failed\n");
+				 */}
+			return stateChart;
+		}
+		
+
+			void RootColorStateChart_destroy(RootColorStateChart* stateChart) {
+				if(stateChart != NULL) {
+		
+					free(stateChart);
+				}
+			}
+	
+		////////////////////////////////////////////////////////////////////////////////
+		// Processing the statechart
+		////////////////////////////////////////////////////////////////////////////////
+	
+		
+
+		
+		void ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmissionStateChart_processStep(
+				RootColorStateChart* stateChart) {
+			switch (stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmission) {
+			case ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PERIODICTRANSMISSION_PERIODICTRANSMISSION_PERIODICSENDING_STATE:
+				if (Clock_getTime(
+						stateChart->clockSending_ofcurrentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmissionClock)
+						>= 50 * 1.0
+		
+						) {
+		
+					// execute exit actions
+		
+					// nothing to do
+		
+					// Transition Effects (incl. clock resets)
+		
+					// nothing to do
+		
+					//create new Parameter struct for Color_color_messageapi_messageType_RepositoryMessage
+		
+					struct Color_color_messageapi_messageType_RepositoryMessage * msg =
+		
+							(Color_color_messageapi_messageType_RepositoryMessage*) malloc(
+		
+									sizeof(struct Color_color_messageapi_messageType_RepositoryMessage));
+		
+					msg->_color_value = stateChart->color_apiValue;
+		
+					//send Message
+		
+					MW_sendMessage(
+							MW_getTargetPortIDforIdentifier(
+									stateChart->parentComponent->colorPort->ID),
+							MESSAGE_COLOR_COLOR_MESSAGEAPI_MESSAGETYPE_REPOSITORYMESSAGE,
+							msg);
+		
+					//printf("sent message of type %d",MESSAGE_COLOR_COLOR_MESSAGEAPI_MESSAGETYPE_REPOSITORYMESSAGE );
+		
+					fflush (stdout);
+		
+					//release all created received events
+		
+					//release all created sent events
+		
+					free(msg);
+		
+					// change the state
+		
+					stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmission =
+							ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PERIODICTRANSMISSION_PERIODICTRANSMISSION_PERIODICSENDING_STATE;
+		
+					// execute entry actions
+		
+					Clock_reset(
+							stateChart->clockSending_ofcurrentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmissionClock);
+		
+				} else {
+					// execute do action
+					// nothing to do
+		
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		
+		void ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecutionStateChart_processStep(
+		
+		RootColorStateChart* stateChart) {
+		
+			switch (stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecution) {
+		
+			case ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_APIEXECUTION_APIEXECUTION_EXECINITCOMMAND_STATE:
+		
+				if (1) {
+		
+					// execute exit actions
+		
+					// nothing to do
+		
+					// Transition Effects (incl. clock resets)
+		
+					// nothing to do
+		
+					// nothing to do			
+		
+					//release all created received events
+		
+					//release all created sent events
+		
+					// change the state
+		
+					stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecution =
+		
+							ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_APIEXECUTION_APIEXECUTION_EXECEXECCOMMAND_STATE;
+		
+					// execute entry actions
+		
+					stateChart->color_apiValue = stateChart->execApi();
+		
+					;
+		
+					Clock_reset(
+		
+							stateChart->_apiClock_ofcurrentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecutionClock);
+		
+				} else {
+		
+					// execute do action
+		
+					// nothing to do
+		
+				}
+		
+				break;
+		
+			case ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_APIEXECUTION_APIEXECUTION_EXECEXECCOMMAND_STATE:
+		
+				if (
+		
+				Clock_getTime(
+		
+						stateChart->_apiClock_ofcurrentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecutionClock)
+		
+				>= stateChart->TimeBetweenApiExececutions * 1.0
+		
+				) {
+		
+					// execute exit actions
+		
+					// nothing to do
+		
+					// Transition Effects (incl. clock resets)
+		
+					// nothing to do
+		
+					// nothing to do			
+		
+					//release all created received events
+		
+					//release all created sent events
+		
+					// change the state
+		
+					stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecution =
+		
+							ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_APIEXECUTION_APIEXECUTION_EXECEXECCOMMAND_STATE;
+		
+					// execute entry actions
+		
+					stateChart->color_apiValue = stateChart->execApi();
+		
+					;
+		
+					Clock_reset(
+		
+							stateChart->_apiClock_ofcurrentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecutionClock);
+		
+				} else {
+		
+					// execute do action
+		
+					// nothing to do
+		
+				}
+		
+				break;
+		
+			default:
+		
+				break;
+		
+			}
+		
+		}
+		
+		void ROOTCOLOR_COLOR_STATE_STATE_Color_color_behavior_Color_color_behaviorStateChart_processStep(
+				RootColorStateChart* stateChart) {
+			switch (stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_Color_color_behavior_Color_color_behavior) {
+			case ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE:
+				// execute do action
+				// nothing to do
+		
+				ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecutionStateChart_processStep(
+						stateChart);
+				ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmissionStateChart_processStep(
+						stateChart);
+		
+				break;
+			default:
+				break;
+			}
+		}
+		
+		void RootColorStateChart_processStep(RootColorStateChart* stateChart) {
+			switch (stateChart->currentStateOfRootColor) {
+			case ROOTCOLOR_COLOR_STATE_STATE:
+				// execute do action
+				// nothing to do
+		
+				ROOTCOLOR_COLOR_STATE_STATE_Color_color_behavior_Color_color_behaviorStateChart_processStep(
+						stateChart);
+		
+				break;
+			default:
+				break;
+			}
+		}
+		
+		
+		void ROOTCOLOR_COLOR_STATE_STATE_Color_color_behavior_Color_color_behaviorStateChart_exit(
+				RootColorStateChart* stateChart) {
+			switch (stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_Color_color_behavior_Color_color_behavior) {
+			case ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE:
+				// nothing to do
+		
+				ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecutionStateChart_exit(
+						stateChart);
+				ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmissionStateChart_exit(
+						stateChart);
+		
+				break;
+			default:
+				break;
+			}
+		}
+		void ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecutionStateChart_exit(
+				RootColorStateChart* stateChart) {
+			switch (stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecution) {
+			case ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_APIEXECUTION_APIEXECUTION_EXECINITCOMMAND_STATE:
+				// nothing to do
+		
+				break;
+			case ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_APIEXECUTION_APIEXECUTION_EXECEXECCOMMAND_STATE:
+				// nothing to do
+		
+				break;
+			default:
+				break;
+			}
+		}
+		void ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmissionStateChart_exit(
+				RootColorStateChart* stateChart) {
+			switch (stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmission) {
+			case ROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PERIODICTRANSMISSION_PERIODICTRANSMISSION_PERIODICSENDING_STATE:
+				// nothing to do
+		
+				break;
+			default:
+				break;
+			}
+		}
+				
+			
+		int_t RootColorStateChart_isInState(RootColorStateChart* stateChart,
+				RootColorState state) {
+			return (stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_Color_color_behavior_Color_color_behavior
+					== state
+					|| stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_ApiExecution_ApiExecution
+							== state
+					|| stateChart->currentStateOfROOTCOLOR_COLOR_STATE_STATE_COLOR_COLOR_BEHAVIOR_COLOR_COLOR_BEHAVIOR_COLOR_INITSTATE_STATE_PeriodicTransmission_PeriodicTransmission
+							== state);
+		
+		}
+		
+		bool_t RootColorStateChart_isTerminated(RootColorStateChart* stateChart) {
+			//TODO
+			return true;
+		}
+		
+
+
+
---
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen/overtakerR1/Configuration.c
@@ -0,0 +1,102 @@
+		#include "Configuration.h"
+	void configureOvertakerR1CIC(void){
+
+MessageID * messageIDsOfbuffer;
+		//create instance overtakerMotorR of type MotorComponent
+		 mw->overtakerMotorRComponent = MotorComponent_create();
+					messageIDsOfbuffer = (MessageID*) calloc(1,sizeof(MessageID));
+						messageIDsOfbuffer[0] = MESSAGE_MOTOR_VELOCITY_MESSAGEAPI_MESSAGETYPE_REPOSITORYMESSAGE;
+					MessageBuffer* buffer1ofvelocityContRvelocity = MessageBuffer_create(1,false,1,messageIDsOfbuffer);
+				MessageBuffer** allBuffersOfvelocityContRvelocity = (MessageBuffer**) malloc(1*sizeof(MessageBuffer*));
+				allBuffersOfvelocityContRvelocity[0] = buffer1ofvelocityContRvelocity;
+	
+				mw->overtakerMotorRComponent->velocityPort = Port_create(PORT_OVERTAKERMOTORRMOTORVELOCITYCONTR, 1, allBuffersOfvelocityContRvelocity);
+			
+		//create behavior for component overtakerMotorRComponent
+		mw->overtakerMotorRComponent->stateChart = RootMotorStateChart_create(mw->overtakerMotorRComponent);
+		mw->overtakerMotorRComponent->stateChart->initApi = overtakerMotorR_velocityContR_InitApi;
+		mw->overtakerMotorRComponent->stateChart->execApi = overtakerMotorR_velocityContR_ExecApi;
+		//create instance overtakerMotorL of type MotorComponent
+		 mw->overtakerMotorLComponent = MotorComponent_create();
+					messageIDsOfbuffer = (MessageID*) calloc(1,sizeof(MessageID));
+						messageIDsOfbuffer[0] = MESSAGE_MOTOR_VELOCITY_MESSAGEAPI_MESSAGETYPE_REPOSITORYMESSAGE;
+					MessageBuffer* buffer1ofvelocityContLvelocity = MessageBuffer_create(1,false,1,messageIDsOfbuffer);
+				MessageBuffer** allBuffersOfvelocityContLvelocity = (MessageBuffer**) malloc(1*sizeof(MessageBuffer*));
+				allBuffersOfvelocityContLvelocity[0] = buffer1ofvelocityContLvelocity;
+	
+				mw->overtakerMotorLComponent->velocityPort = Port_create(PORT_OVERTAKERMOTORLMOTORVELOCITYCONTL, 1, allBuffersOfvelocityContLvelocity);
+			
+		//create behavior for component overtakerMotorLComponent
+		mw->overtakerMotorLComponent->stateChart = RootMotorStateChart_create(mw->overtakerMotorLComponent);
+		mw->overtakerMotorLComponent->stateChart->initApi = overtakerMotorL_velocityContL_InitApi;
+		mw->overtakerMotorLComponent->stateChart->execApi = overtakerMotorL_velocityContL_ExecApi;
+		//create instance overtakerDriver of type OvertakerDriverComponent
+		 mw->overtakerDriverComponent = OvertakerDriverComponent_create();
+					messageIDsOfbuffer = (MessageID*) calloc(1,sizeof(MessageID));
+						messageIDsOfbuffer[0] = MESSAGE_EXECUTEOVERTAKINGMESSAGESMESSAGE;
+					MessageBuffer* buffer1ofinitiatorP1initiatorP = MessageBuffer_create(5,false,1,messageIDsOfbuffer);
+				MessageBuffer** allBuffersOfinitiatorP1initiatorP = (MessageBuffer**) malloc(1*sizeof(MessageBuffer*));
+				allBuffersOfinitiatorP1initiatorP[0] = buffer1ofinitiatorP1initiatorP;
+	
+				mw->overtakerDriverComponent->initiatorPPort = Port_create(PORT_OVERTAKERDRIVEROVERTAKERDRIVERINITIATORP1, 1, allBuffersOfinitiatorP1initiatorP);
+			
+					messageIDsOfbuffer = (MessageID*) calloc(1,sizeof(MessageID));
+						messageIDsOfbuffer[0] = MESSAGE_DISTANCE_DISTANCE_MESSAGEAPI_MESSAGETYPE_REPOSITORYMESSAGE;
+					MessageBuffer* buffer1ofdistancedistance = MessageBuffer_create(1,false,1,messageIDsOfbuffer);
+				MessageBuffer** allBuffersOfdistancedistance = (MessageBuffer**) malloc(1*sizeof(MessageBuffer*));
+				allBuffersOfdistancedistance[0] = buffer1ofdistancedistance;
+	
+				mw->overtakerDriverComponent->distancePort = Port_create(PORT_OVERTAKERDRIVEROVERTAKERDRIVERDISTANCE, 1, allBuffersOfdistancedistance);
+			
+					messageIDsOfbuffer = (MessageID*) calloc(1,sizeof(MessageID));
+						messageIDsOfbuffer[0] = MESSAGE_LINE_LINELIGHT_MESSAGEAPI_MESSAGETYPE_REPOSITORYMESSAGE;
+					MessageBuffer* buffer1oflineline = MessageBuffer_create(1,false,1,messageIDsOfbuffer);
+				MessageBuffer** allBuffersOflineline = (MessageBuffer**) malloc(1*sizeof(MessageBuffer*));
+				allBuffersOflineline[0] = buffer1oflineline;
+	
+				mw->overtakerDriverComponent->linePort = Port_create(PORT_OVERTAKERDRIVEROVERTAKERDRIVERLINE, 1, allBuffersOflineline);
+			
+			mw->overtakerDriverComponent->velocityRPort = Port_create(PORT_OVERTAKERDRIVEROVERTAKERDRIVERVELOCITYR, 0, NULL);
+
+			mw->overtakerDriverComponent->velocityLPort = Port_create(PORT_OVERTAKERDRIVEROVERTAKERDRIVERVELOCITYL, 0, NULL);
+
+		//create behavior for component overtakerDriverComponent
+		mw->overtakerDriverComponent->stateChart = RootovertakerDriverRTSCStateChart_create(mw->overtakerDriverComponent);
+		//create instance overtakerDistance of type DistanceComponent
+		 mw->overtakerDistanceComponent = DistanceComponent_create();
+			mw->overtakerDistanceComponent->distancePort = Port_create(PORT_OVERTAKERDISTANCEDISTANCEDISTANCE, 0, NULL);
+
+		//create behavior for component overtakerDistanceComponent
+		mw->overtakerDistanceComponent->stateChart = RootDistanceStateChart_create(mw->overtakerDistanceComponent);
+		mw->overtakerDistanceComponent->stateChart->initApi = overtakerDistance_distance_InitApi;
+		mw->overtakerDistanceComponent->stateChart->execApi = overtakerDistance_distance_ExecApi;
+		//create instance overtakerLine of type LineComponent
+		 mw->overtakerLineComponent = LineComponent_create();
+			mw->overtakerLineComponent->lineLightPort = Port_create(PORT_OVERTAKERLINELINELINELIGHT, 0, NULL);
+
+		//create behavior for component overtakerLineComponent
+		mw->overtakerLineComponent->stateChart = RootLineStateChart_create(mw->overtakerLineComponent);
+		mw->overtakerLineComponent->stateChart->initApi = overtakerLine_lineLight_InitApi;
+		mw->overtakerLineComponent->stateChart->execApi = overtakerLine_lineLight_ExecApi;
+
+	//call the initialize method after generation
+	initializeOvertakerR1CIC();
+
+}
+
+///////////////////////////////////////
+/// \brief Initialize function for RSRI overtakerR1
+/// This is the initialize function for RSRI overtakerR1. This function calls the initialization function
+/// for the middleware of the ECU and of all AtomicComponentInstances which are allocated on this RSRI.
+//////////////////////////////////////
+void initializeOvertakerR1CIC(void){
+ 
+  //init component instances (inside of this init-functions,
+  //the ports ands and the RTSC of the component instance is initialized
+	  MotorComponent_initialize(mw->overtakerMotorRComponent);
+	  MotorComponent_initialize(mw->overtakerMotorLComponent);
+	  OvertakerDriverComponent_initialize(mw->overtakerDriverComponent);
+	  DistanceComponent_initialize(mw->overtakerDistanceComponent);
+	  LineComponent_initialize(mw->overtakerLineComponent);
+	
+}
