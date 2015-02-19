4731840b82eb04049eb4543e11e2861f16fb0b04
Christian Heinzemann <chris227@upb.de>
2015-02-19 16:38:15 +0000

    branched MUML PIM

---
+++ PlatformIndependentModeling/de.uni_paderborn.fujaba.muml.actionlanguage.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/actionlanguage/properties/actionlanguage/editor/TimeValueExpressionEditor.java
@@ -0,0 +1,91 @@
+package de.uni_paderborn.fujaba.muml.actionlanguage.properties.actionlanguage.editor;
+
+/**
+ * @generated
+ */
+public class TimeValueExpressionEditor
+		extends
+			de.uni_paderborn.fujaba.properties.runtime.editors.ClassPropertyEditor {
+
+	/**
+	 * @generated
+	 */
+	public TimeValueExpressionEditor(String tab,
+			org.eclipse.emf.common.notify.AdapterFactory adapterFactory,
+			org.eclipse.emf.ecore.EClass eClass) {
+		super(tab, adapterFactory, eClass);
+	}
+
+	/**
+	 * @generated
+	 */
+	@Override
+	protected void createProperties() {
+		super.createProperties();
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addTimeValue_GeneralTab_Editor(null, true);
+		}
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addTimeValue_GeneralTab_Editor(String category, boolean front) {
+		addEditorToCategory(category, createTimeValue_GeneralTab_Editor(),
+				front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createTimeValue_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage.eINSTANCE
+				.getTimeValueExpression_TimeValue();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.ComboPropertyEditor(
+				adapterFactory, feature);
+
+		editor.setTooltipMessage("The TimeValue which is referenced by the TimeValueExpression.");
+
+		return editor;
+
+	}
+
+	//
+	// instantiation
+	//
+
+	/**
+	 * @generated
+	 */
+	public TimeValueExpressionEditor(String tab) {
+		this(
+				tab,
+				de.uni_paderborn.fujaba.muml.actionlanguage.properties.util.PropertiesUtil.INSTANCE
+						.getAdapterFactory(),
+				de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage.eINSTANCE
+						.getTimeValueExpression());
+	}
+
+	/**
+	 * @generated
+	 */
+	public static class Factory
+			implements
+				de.uni_paderborn.fujaba.properties.runtime.factory.IPropertyEditorFactory {
+		@Override
+		public de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createPropertyEditor(
+				String tab) {
+			return new TimeValueExpressionEditor(tab);
+		}
+
+		@Override
+		public boolean hasTab(java.lang.String tab) {
+			return java.util.Arrays.asList(
+					new java.lang.String[]{"property.tab.general"}).contains(
+					tab);
+		}
+	}
+
+}
---
+++ PlatformIndependentModeling/de.uni_paderborn.fujaba.muml.actionlanguage.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/actionlanguage/properties/actionlanguage/editor/TypedNamedElementExpressionEditor.java
@@ -0,0 +1,116 @@
+package de.uni_paderborn.fujaba.muml.actionlanguage.properties.actionlanguage.editor;
+
+/**
+ * @generated
+ */
+public class TypedNamedElementExpressionEditor
+		extends
+			de.uni_paderborn.fujaba.properties.runtime.editors.ClassPropertyEditor {
+
+	/**
+	 * @generated
+	 */
+	public TypedNamedElementExpressionEditor(String tab,
+			org.eclipse.emf.common.notify.AdapterFactory adapterFactory,
+			org.eclipse.emf.ecore.EClass eClass) {
+		super(tab, adapterFactory, eClass);
+	}
+
+	/**
+	 * @generated
+	 */
+	@Override
+	protected void createProperties() {
+		super.createProperties();
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addTypedNamedElement_GeneralTab_Editor(null, true);
+		}
+
+		if (getTab() == null || "property.tab.general".equals(getTab())) {
+			addIndices_GeneralTab_Editor(null, true);
+		}
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addTypedNamedElement_GeneralTab_Editor(String category,
+			boolean front) {
+		addEditorToCategory(category,
+				createTypedNamedElement_GeneralTab_Editor(), front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createTypedNamedElement_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage.eINSTANCE
+				.getTypedNamedElementExpression_TypedNamedElement();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.ComboPropertyEditor(
+				adapterFactory, feature);
+
+		return editor;
+
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void addIndices_GeneralTab_Editor(String category, boolean front) {
+		addEditorToCategory(category, createIndices_GeneralTab_Editor(), front);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createIndices_GeneralTab_Editor() {
+		final org.eclipse.emf.ecore.EStructuralFeature feature = de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage.eINSTANCE
+				.getTypedNamedElementExpression_Indices();
+		final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.ListPropertyEditor(
+				adapterFactory, feature);
+
+		editor.setTooltipMessage("Indices which refer to an concrete index of an array.");
+
+		return editor;
+
+	}
+
+	//
+	// instantiation
+	//
+
+	/**
+	 * @generated
+	 */
+	public TypedNamedElementExpressionEditor(String tab) {
+		this(
+				tab,
+				de.uni_paderborn.fujaba.muml.actionlanguage.properties.util.PropertiesUtil.INSTANCE
+						.getAdapterFactory(),
+				de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage.eINSTANCE
+						.getTypedNamedElementExpression());
+	}
+
+	/**
+	 * @generated
+	 */
+	public static class Factory
+			implements
+				de.uni_paderborn.fujaba.properties.runtime.factory.IPropertyEditorFactory {
+		@Override
+		public de.uni_paderborn.fujaba.properties.runtime.editors.IPropertyEditor createPropertyEditor(
+				String tab) {
+			return new TypedNamedElementExpressionEditor(tab);
+		}
+
+		@Override
+		public boolean hasTab(java.lang.String tab) {
+			return java.util.Arrays.asList(
+					new java.lang.String[]{"property.tab.general",
+							"property.tab.general"}).contains(tab);
+		}
+	}
+
+}
