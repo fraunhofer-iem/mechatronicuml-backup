dc2eeef2f5ac670b710f6d2eda22d113702da97c
Christian Heinzemann <chris227@upb.de>
2015-02-19 16:33:57 +0000

    branched MUML Core

---
+++ MUMLCore/de.uni_paderborn.fujaba.common/src/de/uni_paderborn/fujaba/common/edit/policies/EditPolicyRoles.java
@@ -0,0 +1,19 @@
+package de.uni_paderborn.fujaba.common.edit.policies;
+
+/**
+ * Edit policy roles for registering our edit policies in parts.
+ */
+public interface EditPolicyRoles {
+
+	public static final String COMPARTMENT_BORDER_ROLE = "CompartmentBorderRole"; //$NON-NLS-1$
+	
+	public static final String COMPARTMENT_ORIENTATION_ROLE = "CompartmentOrientationRole"; //$NON-NLS-1$
+	
+	public static final String COMPONENTSTORYPATTERNVARIABLE_VISUALIZATION_ROLE = "ComponentStoryPatternVariableVisualizationRole"; //$NON-NLS-1$
+		
+	public static final String ERROR_FEEDBACK_ROLE = "ErrorFeedbackPolicy"; //$NON-NLS-1$
+
+	public static final String ENLARGE_COMPARTMENT_ROLE = "EnlargeCompartmentRole";
+
+	public static final String WRAPPING_LABEL_MULTI_LINE_ROLE = "WrappingLabelMultiLineEditPolicy";
+}
