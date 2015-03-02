763709d37c8734cd8a563a30d162b300a9f530a7
Andreas Dann <adann@mail.upb.de>
2015-03-02 16:05:10 +0000

    added line following in api-call overtakeeR1

--- Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen-new/overtakeeR1/apiCallCode/implementations.c
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen-new/overtakeeR1/apiCallCode/implementations.c
@@ -1,10 +1,14 @@
 #include "implementations.h"
 /** Start of user code user includes **/ 
 
-
 static bool_t initd = false;
 static S8 power1 = 0;
 static S8 power2 = 0;
+
+U8 result[1];
+U8 average[1];
+int self_average[1];
+U8 count = 0;
 
 //0 = straight, 1 = rechts von der linie, 2 = links von der linie
 static U8 last_known_direction = 0; 
@@ -17,6 +21,7 @@ static int difference;
 
 static U8 pk = 7;
 static U8 pkd = 32;
+static U8 cutoff = 80;
 
 void initAll(){
 		ecrobot_init_tetrix(NXT_PORT_S1);
@@ -35,8 +40,77 @@ void initAll(){
 }	
 		void overtakeeMotorL_velocityContR_ExecApi(int velocity){
 /** Start of user code overtakeeMotorL_velocityContR_ExecApi **/ 
-		power2 = velocity;
-				ecrobot_tetrix_motors(NXT_PORT_S1, TETRIX_ADDRESS_1,
+		
+				self_average[0] = 0;
+
+    float p = (float)pk/(float)pkd;
+
+	i2c_start_transaction(NXT_PORT_S2, LL_I2C_ADDR, LL_REG_RESULT, 1, result, 1, 0);
+	i2c_start_transaction(NXT_PORT_S2, LL_I2C_ADDR, 0x43, 1, average, 1, 0);
+
+	int i;
+	for (i = 0; i < 8; i++) {
+		int pos = (1 << (7-i));
+		if ((result[0] & pos) == 0) {
+			self_average[0] += (i+1)*10;
+			count++;
+		}
+	}
+
+	self_average[0] /= count;
+
+	if(count == 0)
+	{
+		//No line detected
+		
+		if(last_known_direction == 0)
+		{
+			self_average[0] = 45;
+		}
+		else if(last_known_direction == 1)
+		{
+			self_average[0] = 90;
+		}
+		else if(last_known_direction == 2)
+		{
+			self_average[0] = 0;
+		}
+
+	} 
+	else if(count == 8)
+	{
+		//robot is orthogonal to the line, drive left
+		self_average[0] = 90;
+	}
+	else if(count > 0 && count < 8) 
+	{
+		if(self_average[0] > target)
+		{
+			last_known_direction = 1;
+		}
+		else if(self_average[0] < target)
+		{
+			last_known_direction = 2;
+		}
+		else
+		{
+			last_known_direction = 0;
+		}
+	}
+
+	difference = target - self_average[0];
+
+
+    power1=velocity-((int)(difference*p));
+    power2=velocity+((int)(difference*p));
+
+    if(power2>cutoff){
+    	power2=cutoff;
+    }
+    if(power1>cutoff){
+    	power1=cutoff;
+    }
+	ecrobot_tetrix_motors(NXT_PORT_S1, TETRIX_ADDRESS_1,
 						TETRIX_MODE_PWM, TETRIX_MODE_PWM_REV, power1, power2);
 /**End of user code**/
 }	
@@ -47,9 +121,7 @@ void initAll(){
 }	
 		void overtakeeMotorR_velocityContL_ExecApi(int velocity){
 /** Start of user code overtakeeMotorR_velocityContL_ExecApi **/ 
-		power1 = velocity;
-				ecrobot_tetrix_motors(NXT_PORT_S1, TETRIX_ADDRESS_1,
-						TETRIX_MODE_PWM, TETRIX_MODE_PWM_REV, power1, power2);
+		overtakeeMotorL_velocityContR_ExecApi(velocity);
 /**End of user code**/
 }	
 		void overtakeeLine_lineLight_InitApi(){
--- Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen-new/overtakeeR2/apiCallCode/implementations.h
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen-new/overtakeeR2/apiCallCode/implementations.h
@@ -1,6 +1,7 @@
 #include "kernel.h"
 #include "kernel_id.h"
 #include "ecrobot_interface.h"
+#include "ecrobot_lineleader.h"
 /**
  * @file 
  * @author generated
