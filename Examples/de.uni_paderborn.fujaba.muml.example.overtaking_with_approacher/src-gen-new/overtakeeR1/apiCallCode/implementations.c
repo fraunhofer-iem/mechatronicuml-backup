9870b8d3413d0363db50fa2a1f28e6da24d021f1
Andreas Dann <adann@mail.upb.de>
2015-03-04 11:22:52 +0000

    line leading for overtakeeR1

--- Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen-new/overtakeeR1/apiCallCode/implementations.c
+++ Examples/de.uni_paderborn.fujaba.muml.example.overtaking_with_approacher/src-gen-new/overtakeeR1/apiCallCode/implementations.c
@@ -5,6 +5,9 @@ static bool_t initd = false;
 static S8 power1 = 0;
 static S8 power2 = 0;
 
+static S8 power1_old = 0;
+static S8 power2_old = 0;
+
 U8 result[1];
 U8 average[1];
 int self_average[1];
@@ -40,84 +43,23 @@ void initAll(){
 }	
 		void overtakeeMotorL_velocityContR_ExecApi(int velocity){
 /** Start of user code overtakeeMotorL_velocityContR_ExecApi **/ 
-		count = 0;
-	self_average[0] = 0;
 
-    float p = (float)pk/(float)pkd;
 
-	i2c_start_transaction(NXT_PORT_S2, LL_I2C_ADDR, LL_REG_RESULT, 1, result, 1, 0);
-	i2c_start_transaction(NXT_PORT_S2, LL_I2C_ADDR, 0x43, 1, average, 1, 0);
+	if(1 == 1){
 
-	int i;
-	for (i = 0; i < 8; i++) {
-		int pos = (1 << (7-i));
-		if ((result[0] & pos) == 0) {
-			self_average[0] += (i+1)*10;
-			count++;
-		}
+		overtakeeMotorR_velocityContL_ExecApi(velocity);
 	}
-
-	self_average[0] /= count;
-
-	if(count == 0)
-	{
-		//No line detected
-		
-		if(last_known_direction == 0)
-		{
-			self_average[0] = 45;
-		}
-		else if(last_known_direction == 1)
-		{
-			self_average[0] = 90;
-		}
-		else if(last_known_direction == 2)
-		{
-			self_average[0] = 0;
-		}
-
-	} 
-	else if(count == 8)
-	{
-		//robot is orthogonal to the line, drive left
-		self_average[0] = 90;
-	}
-	else if(count > 0 && count < 8) 
-	{
-		if(self_average[0] > target)
-		{
-			last_known_direction = 1;
-		}
-		else if(self_average[0] < target)
-		{
-			last_known_direction = 2;
-		}
-		else
-		{
-			last_known_direction = 0;
-		}
-	}
-
-	difference = target - self_average[0];
-
-
-    power1=velocity-((int)(difference*p));
-    power2=velocity+((int)(difference*p));
-    if(velocity == 0){
-    	power2 = 0;
-    	power1 = 0;
-    }
-
-    if(power2>cutoff){
-    	power2=cutoff;
-    }
-    if(power1>cutoff){
-    	power1=cutoff;
-    }
+	 else {
+	 		power2 = velocity;
+			power2_old = power2;
+			power1 = power1_old;
+	 	}
+	
 	ecrobot_tetrix_motors(NXT_PORT_S1, TETRIX_ADDRESS_1,
 						TETRIX_MODE_PWM, TETRIX_MODE_PWM_REV, power1, power2);
-/**End of user code**/
-}	
+			
+}
+	 
 		void overtakeeMotorR_velocityContL_InitApi(){
 /** Start of user code overtakeeMotorR_velocityContL_InitApi **/ 
 		
@@ -125,7 +67,101 @@ void initAll(){
 }	
 		void overtakeeMotorR_velocityContL_ExecApi(int velocity){
 /** Start of user code overtakeeMotorR_velocityContL_ExecApi **/ 
-		overtakeeMotorL_velocityContR_ExecApi(velocity);
+		count = 0;
+
+	if(1 == 1){
+
+			if(velocity == 0){
+				power2=0;
+				power1=0;
+
+			}
+			else{
+				power2= 30;
+				power1 = 30;
+
+
+				
+				self_average[0] = 0;
+
+		    	float p = (float)pk/(float)pkd;
+
+				i2c_start_transaction(NXT_PORT_S2, LL_I2C_ADDR, LL_REG_RESULT, 1, result, 1, 0);
+
+				int i;
+				for (i = 0; i < 8; i++) {
+					int pos = (1 << (7-i));
+					if ((result[0] & pos) != 0) {
+						self_average[0] += (i+1)*10;
+						count++;
+					} 
+				}
+				
+
+				self_average[0] /= count;
+
+				if(count == 0)
+				{
+					//No line detected
+					
+					if(last_known_direction == 0)
+					{
+						self_average[0] = 45;
+					}
+					else if(last_known_direction == 1)
+					{
+						self_average[0] = 90;
+					}
+					else if(last_known_direction == 2)
+					{
+						self_average[0] = 0;
+					}
+
+				} 
+				else if(count == 8)
+				{
+					//robot is orthogonal to the line, drive left
+					self_average[0] = 90;
+				}
+				else if(count > 0 && count < 8) 
+				{
+					if(self_average[0] > target)
+					{
+						last_known_direction = 1;
+					}
+					else if(self_average[0] < target)
+					{
+						last_known_direction = 2;
+					}
+					else
+					{
+						last_known_direction = 0;
+					}
+				}
+
+				difference = target - self_average[0];
+		
+
+			    power1=velocity+((int)(difference*p));
+			    power2=velocity-((int)(difference*p));
+
+			    if(power2>cutoff){
+			    	power2=cutoff;
+			    }
+			    if(power1>cutoff){
+			    	power1=cutoff;
+			    }
+		}
+
+	}
+	 else {
+	 		power1 = velocity;
+			power1_old = power1;
+			power2 = power2_old;
+	 	}
+	
+	ecrobot_tetrix_motors(NXT_PORT_S1, TETRIX_ADDRESS_1,
+						TETRIX_MODE_PWM, TETRIX_MODE_PWM_REV, (S8) power1, (S8) power2);
 /**End of user code**/
 }	
 		void overtakeeLine_lineLight_InitApi(){
