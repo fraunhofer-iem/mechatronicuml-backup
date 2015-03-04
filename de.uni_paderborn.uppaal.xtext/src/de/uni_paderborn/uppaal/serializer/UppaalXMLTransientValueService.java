76bd4a519e789e1a9741e7c9abf9fd9176541346
Jan Ladleif <jladleif@mail.upb.de>
2015-03-04 09:34:47 +0000

    UPPAAL serializer now ignores the comment attribute of all templates.

--- de.uni_paderborn.uppaal.xtext/src/de/uni_paderborn/uppaal/serializer/UppaalXMLTransientValueService.java
+++ de.uni_paderborn.uppaal.xtext/src/de/uni_paderborn/uppaal/serializer/UppaalXMLTransientValueService.java
@@ -2,18 +2,17 @@ package de.uni_paderborn.uppaal.serializer;
 
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.xtext.parsetree.reconstr.impl.DefaultTransientValueService;
 
-import de.uni_paderborn.uppaal.templates.Template;
+import de.uni_paderborn.uppaal.templates.AbstractTemplate;
 
 public class UppaalXMLTransientValueService extends DefaultTransientValueService {
 	
 	@Override
 	public boolean isTransient(EObject owner, EStructuralFeature feature, int index) {
 		
-		if (owner instanceof Template)
+		if (owner instanceof AbstractTemplate)
 		{
 			// Ignore the comment field.
 			if (feature instanceof EAttribute)
