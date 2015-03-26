cd10c24187c6e8305ffc49d5e788e1b41d4cf7f7
Sebastian Thiele <sthiele2>
2015-03-26 09:23:02 +0000

    modified the UpaalVerifyForResultAction.java such that it can verify Coordination Patterns. A CoordinationPattern is transformed to a CoordinationProtocol beforehand.

--- UppaalAdapter/de.uni_paderborn.fujaba.muml.verification.uppaal.ui/src/de/uni_paderborn/fujaba/muml/verification/uppaal/ui/UppaalVerifyForResultAction.java
+++ UppaalAdapter/de.uni_paderborn.fujaba.muml.verification.uppaal.ui/src/de/uni_paderborn/fujaba/muml/verification/uppaal/ui/UppaalVerifyForResultAction.java
@@ -1,5 +1,8 @@
 package de.uni_paderborn.fujaba.muml.verification.uppaal.ui;
 
+import java.io.IOException;
+import java.util.Collections;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -9,9 +12,13 @@ import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import de.uni_paderborn.fujaba.muml.constraint.VerifiableElement;
+import de.uni_paderborn.fujaba.muml.pattern.CoordinationPattern;
+import de.uni_paderborn.fujaba.muml.pattern.diagram.custom.export.wizard.PatternVerificationWizard;
 import de.uni_paderborn.fujaba.muml.verification.uppaal.job.VerifyForResultsJob;
 
 public class UppaalVerifyForResultAction extends AbstractHandler {
@@ -26,6 +33,23 @@ public class UppaalVerifyForResultAction extends AbstractHandler {
 		assert ((View) editPart.getModel()).getElement() instanceof VerifiableElement;
 		verifiableElement = (VerifiableElement) ((View) editPart.getModel()).getElement();
 	
+		// if the verifiableElement is a CoordinationPattern, the pattern has to be transformed to a protocol first. This is done by the patternWizard
+		if(verifiableElement instanceof CoordinationPattern)
+		{
+			IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);			
+			PatternVerificationWizard patternWizard = new PatternVerificationWizard();
+			patternWizard.init(workbenchWindow.getWorkbench(), (IStructuredSelection)selection);
+			WizardDialog wizardDialog = new WizardDialog(HandlerUtil.getActiveShell(event), patternWizard);
+			wizardDialog.open();
+			verifiableElement = patternWizard.getTransformedCoordinationProtocolFromCoordinationPattern();
+//			try {
+			//	verifiableElement.eResource().save(Collections.emptyMap() );
+			//} catch (IOException e) {
+				// TODO Auto-generated catch block
+			//	e.printStackTrace();
+		//	}
+		}
+		
 		//Schedule the verification job
 		UppaalVerifyForResultWizard wizard = new UppaalVerifyForResultWizard();
 		Job job = new VerifyForResultsJob(verifiableElement, wizard, wizard, new PropertiesResultWizard());
