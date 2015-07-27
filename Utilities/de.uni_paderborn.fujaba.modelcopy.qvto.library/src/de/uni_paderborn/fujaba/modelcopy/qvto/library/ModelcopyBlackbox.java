b303b18bc51f41970c9242f028c1c587cd2d9024
Ingo Budde <bingo@mail.upb.de>
2015-07-27 15:39:12 +0000

    [Modelcopy Library] Keep order for bidirectional references; implementation similar to EcoreUtil.copy().

--- Utilities/de.uni_paderborn.fujaba.modelcopy.qvto.library/src/de/uni_paderborn/fujaba/modelcopy/qvto/library/ModelcopyBlackbox.java
+++ Utilities/de.uni_paderborn.fujaba.modelcopy.qvto.library/src/de/uni_paderborn/fujaba/modelcopy/qvto/library/ModelcopyBlackbox.java
@@ -6,7 +6,9 @@ import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.util.InternalEList;
 
 
 public class ModelcopyBlackbox {
@@ -15,9 +17,24 @@ public class ModelcopyBlackbox {
 		super();
 	}
 
+	@SuppressWarnings("unchecked")
 	public static void setValues(EObject element, EStructuralFeature feature, List<Object> values) {
 		if (feature.isMany()) {
-			element.eSet(feature, values);
+			if (isBidirectional(feature)) {
+				InternalEList<Object> target = ((InternalEList<Object>) element.eGet(feature));
+				int index = 0;
+				for (Object value : values) {
+					int position = target.indexOf(value);
+					if (position == -1) {
+						target.addUnique(index, value);
+					} else if (index != position) {
+						target.move(index, value);
+					}
+					index++;
+				}
+			} else {
+				element.eSet(feature, values);
+			}
 		} else if (values.isEmpty()) {
 			element.eSet(feature, null);
 		} else {
@@ -25,6 +42,13 @@ public class ModelcopyBlackbox {
 		}
 	}
 	
+	private static boolean isBidirectional(EStructuralFeature feature) {
+		if (feature instanceof EReference) {
+			return ((EReference) feature).getEOpposite() != null;
+		}
+		return false;
+	}
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static List<Object> getValues(EObject element, EStructuralFeature feature) {
 		Object value = element.eGet(feature);
