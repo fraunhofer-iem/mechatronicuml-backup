a45541ba3d88d308bff4cda70f3b8dce0be7ff4d
Marcus Hüwe <mhuewe@mail.upb.de>
2015-08-03 14:39:16 +0000

    [Allocation] updated test suite classes
    - explicitly register muml and and hardware root packages
      (otherwise the qvto compilation fails (mars) - might be
       related to the qvto commit de499dbfbd960a63f62c4938d9dc7117)
    - guarded standalone setup calls

--- PlatformSpecificModeling/de.uni_paderborn.fujaba.muml.allocation.algorithm.ilp.lpsolve.tests/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/ilp/lpsolve/tests/AllTestsSuite.java
+++ PlatformSpecificModeling/de.uni_paderborn.fujaba.muml.allocation.algorithm.ilp.lpsolve.tests/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/ilp/lpsolve/tests/AllTestsSuite.java
@@ -1,12 +1,15 @@
 package de.uni_paderborn.fujaba.muml.allocation.algorithm.ilp.lpsolve.tests;
 
+import org.eclipse.emf.common.EMFPlugin;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import de.uni_paderborn.fujaba.muml.MumlPackage;
 import de.uni_paderborn.fujaba.muml.allocation.ilp.lpsolve.xtext.LPSolveStandaloneSetup;
 import de.uni_paderborn.fujaba.muml.allocation.language.AllocationSpecificationLanguageStandaloneSetup;
+import de.uni_paderborn.fujaba.muml.hardware.HardwarePackage;
 
 @RunWith(Suite.class)
 @SuiteClasses({
@@ -14,8 +17,15 @@ import de.uni_paderborn.fujaba.muml.allocation.language.AllocationSpecificationL
 })
 public class AllTestsSuite {
 	@BeforeClass
-	public static void setUpClass() {      
-		AllocationSpecificationLanguageStandaloneSetup.doSetup();
-		LPSolveStandaloneSetup.doSetup();
+	public static void setUpClass() {  
+		if (!EMFPlugin.IS_ECLIPSE_RUNNING) {
+			AllocationSpecificationLanguageStandaloneSetup.doSetup();
+			LPSolveStandaloneSetup.doSetup();
+		}
+		// explicitly setup these packages otherwise the qvto compilation
+		// fails (mars) - might be related to the qvto commit
+		// de499dbfbd960a63f62c4938d9dc71172e075120
+		MumlPackage.eINSTANCE.eClass();
+		HardwarePackage.eINSTANCE.eClass();
 	}
 }
