e8ad9301a949b2b2528fdade8172ba648166d35e
Marcus Hüwe <mhuewe@mail.upb.de>
2015-08-03 14:39:16 +0000

    [Allocation] updated test suite classes
    - explicitly register muml and and hardware root packages
      (otherwise the qvto compilation fails (mars) - might be
       related to the qvto commit de499dbfbd960a63f62c4938d9dc7117)
    - guarded standalone setup calls

--- de.uni_paderborn.fujaba.muml.allocation.algorithm.ilp.tests/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/ilp/tests/Specification2ILPTestSuite.java
+++ de.uni_paderborn.fujaba.muml.allocation.algorithm.ilp.tests/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/ilp/tests/Specification2ILPTestSuite.java
@@ -1,12 +1,15 @@
 package de.uni_paderborn.fujaba.muml.allocation.algorithm.ilp.tests;
 
+import org.eclipse.emf.common.EMFPlugin;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import de.uni_paderborn.fujaba.muml.MumlPackage;
 import de.uni_paderborn.fujaba.muml.allocation.ilp.lpsolve.xtext.LPSolveStandaloneSetup;
 import de.uni_paderborn.fujaba.muml.allocation.language.AllocationSpecificationLanguageStandaloneSetup;
+import de.uni_paderborn.fujaba.muml.hardware.HardwarePackage;
 
 @RunWith(Suite.class)
 @SuiteClasses({
@@ -16,14 +19,20 @@ import de.uni_paderborn.fujaba.muml.allocation.language.AllocationSpecificationL
 	RequiredHardwareResourceInstance2ILPTest.class,
 	ResourceConstraint2ILPTest.class,
 	MeasureFunction2ILPTest.class,
-	// XXX: deactivated for now
-	// ClassifierContextDeclCS2ILPTest.class,
+	ClassifierContextDeclCS2ILPTest.class,
 	MetaModelImportTest.class
 })
 public class Specification2ILPTestSuite {
 	@BeforeClass
-	public static void setUpClass() {      
-		AllocationSpecificationLanguageStandaloneSetup.doSetup();
-		LPSolveStandaloneSetup.doSetup();
+	public static void setUpClass() {
+		if (!EMFPlugin.IS_ECLIPSE_RUNNING) {
+			AllocationSpecificationLanguageStandaloneSetup.doSetup();
+			LPSolveStandaloneSetup.doSetup();
+		}
+		// explicitly setup these packages otherwise the qvto compilation
+		// fails (mars) - might be related to the qvto commit
+		// de499dbfbd960a63f62c4938d9dc71172e075120
+		MumlPackage.eINSTANCE.eClass();
+		HardwarePackage.eINSTANCE.eClass();
 	}
 }
