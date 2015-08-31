8ba4bf55db6d2a0be7c8abd2f1e05808bba2f8b3
Ingo Budde <bingo@mail.upb.de>
2015-08-31 09:03:42 +0000

    Setting up branch SharedEditingDomains.

---
+++ PlatformIndependentModeling/de.uni_paderborn.fujaba.muml.component.diagram/src/de/uni_paderborn/fujaba/muml/component/diagram/providers/MumlEditPartProvider.java
@@ -0,0 +1,32 @@
+/*
+ * <copyright>
+ * Copyright (c) 2013 Software Engineering Group, Heinz Nixdorf Institute, University of Paderborn, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Software Engineering Group - initial API and implementation
+ * </copyright>
+ */
+package de.uni_paderborn.fujaba.muml.component.diagram.providers;
+
+import org.eclipse.gmf.tooling.runtime.providers.DefaultEditPartProvider;
+
+/**
+ * @generated
+ */
+public class MumlEditPartProvider extends DefaultEditPartProvider {
+
+	/**
+	 * @generated
+	 */
+	public MumlEditPartProvider() {
+		super(
+				new de.uni_paderborn.fujaba.muml.component.diagram.edit.parts.MumlEditPartFactory(),
+				de.uni_paderborn.fujaba.muml.component.diagram.part.MumlVisualIDRegistry.TYPED_INSTANCE,
+				de.uni_paderborn.fujaba.muml.component.diagram.edit.parts.ModelElementCategoryEditPart.MODEL_ID);
+	}
+
+}
