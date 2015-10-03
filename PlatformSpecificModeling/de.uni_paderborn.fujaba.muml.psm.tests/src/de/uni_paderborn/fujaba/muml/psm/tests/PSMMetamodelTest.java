703ab97f506d126bf724ccae10f1976d6bb8cb6c
Andreas Dann <adann@mail.upb.de>
2015-10-03 15:51:37 +0000

    PSM: added test-suite metamodel, genmodel, QVTO

---
+++ PlatformSpecificModeling/de.uni_paderborn.fujaba.muml.psm.tests/src/de/uni_paderborn/fujaba/muml/psm/tests/PSMMetamodelTest.java
@@ -0,0 +1,51 @@
+package de.uni_paderborn.fujaba.muml.psm.tests;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.storydriven.core.CorePackage;
+
+import de.uni_paderborn.fujaba.muml.MumlPackage;
+import de.uni_paderborn.fujaba.muml.hardware.HardwarePackage;
+import de.uni_paderborn.fujaba.muml.swplatform.SwplatformPackage;
+import de.uni_paderborn.fujaba.tests.MetamodelTest;
+import de.uni_paderborn.fujaba.tests.TestUtilities;
+
+@RunWith(Parameterized.class)
+public class PSMMetamodelTest extends MetamodelTest {
+
+	public PSMMetamodelTest(EPackage ePackage) {
+		super(ePackage);
+	}
+
+	@Parameters
+	public static Collection<Object[]> getData() throws IOException {
+		ResourceSet resourceSet = new ResourceSetImpl();
+
+		CorePackage.eINSTANCE.eClass();
+		MumlPackage.eINSTANCE.eClass();
+		HardwarePackage.eINSTANCE.eClass();
+		SwplatformPackage.eINSTANCE.eClass();
+		// Resource coreModel = TestUtilities.loadResource(resourceSet,
+		// "org.storydriven.core/model/core.ecore");
+
+		Resource mumlModel = TestUtilities.loadResource(resourceSet,
+				"de.uni_paderborn.fujaba.muml.psm/model/psm.ecore");
+
+		EcoreUtil.resolveAll(resourceSet);
+
+		EPackage muml = (EPackage) mumlModel.getContents().get(0);
+
+		return Collections.singleton(new Object[] { muml });
+	}
+
+}
