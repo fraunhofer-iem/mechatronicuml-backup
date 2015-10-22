cecb1fd4efad2ec61c64cc1c257520d1cc17263c
Ingo Budde <bingo@mail.upb.de>
2015-10-22 18:05:13 +0000

    #1008: Improved Blackbox implementation

--- PlatformIndependentModeling/de.uni_paderborn.fujaba.muml.types/src/de/uni_paderborn/fujaba/muml/types/blackbox/TypesBlackbox.java
+++ PlatformIndependentModeling/de.uni_paderborn.fujaba.muml.types/src/de/uni_paderborn/fujaba/muml/types/blackbox/TypesBlackbox.java
@@ -1,55 +1,42 @@
 package de.uni_paderborn.fujaba.muml.types.blackbox;
 
-import java.io.IOException;
-import java.util.Collections;
-
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-
 import de.uni_paderborn.fujaba.muml.types.PredefinedTypesResourceFactory;
 import de.uni_paderborn.fujaba.muml.types.PrimitiveDataType;
 
 public class TypesBlackbox {
 	public static PrimitiveDataType getVoid() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("void");
+		return getTypeByName("void");
 	}
 	public static PrimitiveDataType getInt8() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("int8");
+		return getTypeByName("int8");
 	}
 	public static PrimitiveDataType getInt16() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("int16");
+		return getTypeByName("int16");
 	}
 	public static PrimitiveDataType getInt32() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("int32");
+		return getTypeByName("int32");
 	}
 	public static PrimitiveDataType getInt64() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("int64");
+		return getTypeByName("int64");
 	}
 	public static PrimitiveDataType getDouble() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("double");
+		return getTypeByName("double");
 	}
 	public static PrimitiveDataType getUint8() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("uint8");
+		return getTypeByName("uint8");
 	}
 	public static PrimitiveDataType getUint16() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("uint16");
+		return getTypeByName("uint16");
 	}
 	public static PrimitiveDataType getUint32() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("uint32");
+		return getTypeByName("uint32");
 	}
 	public static PrimitiveDataType getUint64() {
-		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject("uint64");
+		return getTypeByName("uint64");
 	}
 	
 	public static PrimitiveDataType getTypeByName(String name) {
-
-		Resource resource = PredefinedTypesResourceFactory.getResource();		
-		for(EObject obj : resource.getContents()) {
-			PrimitiveDataType type = (PrimitiveDataType)obj;
-			if(type.getName().equals(name)){
-				return type;
-			}
-		}
-		return null;
+		// This relies on the fact that the unique identifier is equal to the name.
+		return (PrimitiveDataType) PredefinedTypesResourceFactory.getResource().getEObject(name);
 	}
 }
