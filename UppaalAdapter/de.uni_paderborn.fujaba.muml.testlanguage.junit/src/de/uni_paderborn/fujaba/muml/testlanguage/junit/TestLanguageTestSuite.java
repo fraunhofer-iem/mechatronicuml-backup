18ea866384ec0538d6ba1c48c3142df96ee2f936
milanp <milanp>
2015-10-28 16:58:39 +0000

    added standalone MTCTL setup

--- UppaalAdapter/de.uni_paderborn.fujaba.muml.testlanguage.junit/src/de/uni_paderborn/fujaba/muml/testlanguage/junit/TestLanguageTestSuite.java
+++ UppaalAdapter/de.uni_paderborn.fujaba.muml.testlanguage.junit/src/de/uni_paderborn/fujaba/muml/testlanguage/junit/TestLanguageTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import de.uni_paderborn.fujaba.muml.verification.preferences.VerificationPreferencesPlugin;
+import de.uni_paderborn.fujaba.muml.verification.uppaal.MtctlStandaloneSetup;
 
 @RunWith(Suite.class)
 @SuiteClasses({ RunTestCases.class })
@@ -15,10 +16,11 @@ public class TestLanguageTestSuite {
 	@BeforeClass
 	public static void init() {
 		// Set path to the UPPAAL verifyta tool.
+		MtctlStandaloneSetup.doSetup();
 		IPreferenceStore preferenceStore = VerificationPreferencesPlugin
 				.getDefault().getPreferenceStore();
 		preferenceStore.setValue(VerificationPreferencesPlugin.VERIFYTA_KEY,
-				"/data/jenkins/uppaal64-4.1.19/bin-Linux/verifyta");
+				"C:\\Users\\Milan\\Desktop\\uppaal-4.1.19\\uppaal-4.1.19\\bin-Win32\\verifyta.exe");
 	}
 
 }
