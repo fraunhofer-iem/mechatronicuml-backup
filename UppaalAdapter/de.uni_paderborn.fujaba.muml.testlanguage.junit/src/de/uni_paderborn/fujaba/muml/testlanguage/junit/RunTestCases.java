0bf4d307320509ea5effa131063c21e024cab1e5
milanp <milanp>
2015-10-28 14:26:04 +0000

    added test case

--- UppaalAdapter/de.uni_paderborn.fujaba.muml.testlanguage.junit/src/de/uni_paderborn/fujaba/muml/testlanguage/junit/RunTestCases.java
+++ UppaalAdapter/de.uni_paderborn.fujaba.muml.testlanguage.junit/src/de/uni_paderborn/fujaba/muml/testlanguage/junit/RunTestCases.java
@@ -35,9 +35,9 @@ public class RunTestCases {
 	public static Collection<Object[]> getTestCases() {
 		cases = new LinkedList<Object[]>();
 
- 		addCase("platform:/plugin/de.uni_paderborn.fujaba.muml.testlanguage.junit/tests/bebots_AllPositionsTransmission/default.testlanguage");
- 		addCase("platform:/plugin/de.uni_paderborn.fujaba.muml.testlanguage.junit/tests/bebots_Delegation/default.testlanguage");
- 		addCase("platform:/plugin/de.uni_paderborn.fujaba.muml.testlanguage.junit/tests/bebots_Navigation/default.testlanguage");
+// 		addCase("platform:/plugin/de.uni_paderborn.fujaba.muml.testlanguage.junit/tests/bebots_AllPositionsTransmission/default.testlanguage");
+// 		addCase("platform:/plugin/de.uni_paderborn.fujaba.muml.testlanguage.junit/tests/bebots_Delegation/default.testlanguage");
+// 		addCase("platform:/plugin/de.uni_paderborn.fujaba.muml.testlanguage.junit/tests/bebots_Navigation/default.testlanguage");
  		addCase("platform:/plugin/de.uni_paderborn.fujaba.muml.testlanguage.junit/tests/overtaking-cars/default.testlanguage");
 // 		addCase("platform:/plugin/muml_testlanguage.junit/tests/overtaking-cars/default.testlanguage");
 //		addCase("platform:/plugin/muml_testlanguage.junit/tests/overtaking-cars-w-contraflow_Allow/default.testlanguage");
