ed9a9c293ff572b6ff0282111c551af125a7b87c
Ingo Budde <bingo@mail.upb.de>
2015-11-25 12:24:52 +0000

    Adding "org.storydriven.core" to "MUML_1.1_ActionLanguage" branch.

---
+++ org.storydriven.core.edit.properties/src-gen/org/storydriven/core/properties/core/editor/CommentableElementEditor.java
@@ -0,0 +1,76 @@
+package org.storydriven.core.properties.core.editor;
+
+/**
+ * @generated
+ */
+public abstract class CommentableElementEditor
+		extends
+			de.uni_paderborn.fujaba.properties.runtime.editors.ClassPropertyEditor {
+
+	/**
+	 * @generated
+	 */
+	public CommentableElementEditor(String tab,
+			org.eclipse.emf.common.notify.AdapterFactory adapterFactory,
+			org.eclipse.emf.ecore.EClass eClass) {
+		super(tab, adapterFactory, eClass);
+	}
+
+	/**
+	 * @generated
+	 */
+	@Override
+	protected void createProperties() {
+		if (tab == null) {
+
+			addPropertyEditor(createEditorExtension_ExtensionsTab_Editor(),
+					false);
+
+			addPropertyEditor(createEditorComment_DocumentationTab_Editor(),
+					false);
+
+		} else if ("property.tab.general".equals(tab)) { // Tab General
+
+		} else if ("property.tab.documentation".equals(tab)) { // Tab Documentation
+
+			addPropertyEditor(createEditorComment_DocumentationTab_Editor(),
+					false);
+
+		} else if ("property.tab.extensions".equals(tab)) { // Tab Extensions
+
+			addPropertyEditor(createEditorExtension_ExtensionsTab_Editor(),
+					false);
+
+		} else {
+		}
+	}
+
+	private de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editorComment_DocumentationTab;
+	private de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor createEditorComment_DocumentationTab_Editor() {
+		if (this.editorComment_DocumentationTab == null) {
+			final org.eclipse.emf.ecore.EStructuralFeature feature = org.storydriven.core.CorePackage.eINSTANCE
+					.getCommentableElement_Comment();
+			final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.TextPropertyEditor(
+					adapterFactory, feature, true);
+
+			editor.setTooltipMessage("The comment string that can be used to attach arbitrary information to CommentableElements.");
+
+			this.editorComment_DocumentationTab = editor;
+		}
+		return this.editorComment_DocumentationTab;
+	}
+
+	private de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editorExtension_ExtensionsTab;
+	private de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor createEditorExtension_ExtensionsTab_Editor() {
+		if (this.editorExtension_ExtensionsTab == null) {
+			final org.eclipse.emf.ecore.EStructuralFeature feature = org.storydriven.core.CorePackage.eINSTANCE
+					.getExtendableElement_Extension();
+			final de.uni_paderborn.fujaba.properties.runtime.editors.AbstractStructuralFeaturePropertyEditor editor = new de.uni_paderborn.fujaba.properties.runtime.editors.FlattenedListPropertyEditor(
+					adapterFactory, feature);
+
+			this.editorExtension_ExtensionsTab = editor;
+		}
+		return this.editorExtension_ExtensionsTab;
+	}
+
+}
---
+++ org.storydriven.core/src-gen/org/storydriven/core/Repository.java
@@ -0,0 +1,20 @@
+/**
+ */
+package org.storydriven.core;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Repository</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <!-- begin-model-doc -->
+ * Repositories are used for grouping model elements that may be reused in different contexts.
+ * <!-- end-model-doc -->
+ *
+ *
+ * @see org.storydriven.core.CorePackage#getRepository()
+ * @model abstract="true"
+ * @generated
+ */
+public interface Repository extends NamedElement, CommentableElement {
+} // Repository
