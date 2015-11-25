6f9eb5e5c602c1d3792b70cbd28b4454be4f16bb
Ingo Budde <bingo@mail.upb.de>
2015-11-25 12:26:15 +0000

    Adding actionlanguage metamodel to "MUML_1.1_ActionLanguage" branch.

---
+++ de.uni_paderborn.fujaba.muml.actionlanguage/src/de/uni_paderborn/fujaba/muml/actionlanguage/IfStatement.java
@@ -0,0 +1,141 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uni_paderborn.fujaba.muml.actionlanguage;
+
+import org.eclipse.emf.common.util.EList;
+import org.storydriven.core.expressions.Expression;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>If Statement</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <!-- begin-model-doc -->
+ * An if statement is used if the referenced block should be executed only when the condition expression evaluates to true.
+ * An if statement always have one if-condition and one corresponding if-block, any number of else-if-conditions and corresponding else-if-blocks and at most one else-block. 
+ *  
+ * <!-- end-model-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * </p>
+ * <ul>
+ *   <li>{@link de.uni_paderborn.fujaba.muml.actionlanguage.IfStatement#getIfCondition <em>If Condition</em>}</li>
+ *   <li>{@link de.uni_paderborn.fujaba.muml.actionlanguage.IfStatement#getIfBlock <em>If Block</em>}</li>
+ *   <li>{@link de.uni_paderborn.fujaba.muml.actionlanguage.IfStatement#getElseBlock <em>Else Block</em>}</li>
+ *   <li>{@link de.uni_paderborn.fujaba.muml.actionlanguage.IfStatement#getElseIfStatements <em>Else If Statements</em>}</li>
+ * </ul>
+ *
+ * @see de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage#getIfStatement()
+ * @model
+ * @generated
+ */
+public interface IfStatement extends Expression {
+	/**
+	 * Returns the value of the '<em><b>If Condition</b></em>' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>If Condition</em>' containment reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * If condition of the if statement.
+	 * <!-- end-model-doc -->
+	 * @return the value of the '<em>If Condition</em>' containment reference.
+	 * @see #setIfCondition(Expression)
+	 * @see de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage#getIfStatement_IfCondition()
+	 * @model containment="true"
+	 * @generated
+	 */
+	Expression getIfCondition();
+
+	/**
+	 * Sets the value of the '{@link de.uni_paderborn.fujaba.muml.actionlanguage.IfStatement#getIfCondition <em>If Condition</em>}' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>If Condition</em>' containment reference.
+	 * @see #getIfCondition()
+	 * @generated
+	 */
+	void setIfCondition(Expression value);
+
+	/**
+	 * Returns the value of the '<em><b>If Block</b></em>' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>If Block</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * Block which is executed if the if condition evaluates to true.
+	 * <!-- end-model-doc -->
+	 * @return the value of the '<em>If Block</em>' containment reference.
+	 * @see #setIfBlock(Block)
+	 * @see de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage#getIfStatement_IfBlock()
+	 * @model containment="true"
+	 * @generated
+	 */
+	Block getIfBlock();
+
+	/**
+	 * Sets the value of the '{@link de.uni_paderborn.fujaba.muml.actionlanguage.IfStatement#getIfBlock <em>If Block</em>}' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>If Block</em>' containment reference.
+	 * @see #getIfBlock()
+	 * @generated
+	 */
+	void setIfBlock(Block value);
+
+	/**
+	 * Returns the value of the '<em><b>Else Block</b></em>' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Else Block</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * Block which is executed if no if or elseif condition evaluates to true.
+	 * <!-- end-model-doc -->
+	 * @return the value of the '<em>Else Block</em>' containment reference.
+	 * @see #setElseBlock(Block)
+	 * @see de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage#getIfStatement_ElseBlock()
+	 * @model containment="true"
+	 * @generated
+	 */
+	Block getElseBlock();
+
+	/**
+	 * Sets the value of the '{@link de.uni_paderborn.fujaba.muml.actionlanguage.IfStatement#getElseBlock <em>Else Block</em>}' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Else Block</em>' containment reference.
+	 * @see #getElseBlock()
+	 * @generated
+	 */
+	void setElseBlock(Block value);
+
+	/**
+	 * Returns the value of the '<em><b>Else If Statements</b></em>' containment reference list.
+	 * The list contents are of type {@link de.uni_paderborn.fujaba.muml.actionlanguage.ElseIfStatement}.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Else If Statements</em>' reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Else If Statements</em>' containment reference list.
+	 * @see de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage#getIfStatement_ElseIfStatements()
+	 * @model containment="true"
+	 * @generated
+	 */
+	EList<ElseIfStatement> getElseIfStatements();
+
+} // IfStatement
---
+++ de.uni_paderborn.fujaba.muml.actionlanguage/src/de/uni_paderborn/fujaba/muml/actionlanguage/impl/ElementAccessorExpressionImpl.java
@@ -0,0 +1,48 @@
+/**
+ * <copyright>
+ * Copyright (c) 2013 Software Engineering Group, Heinz Nixdorf Institute, University of Paderborn, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Software Engineering Group - initial API and implementation
+ * </copyright>
+ */
+package de.uni_paderborn.fujaba.muml.actionlanguage.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.storydriven.core.expressions.impl.ExpressionImpl;
+
+import de.uni_paderborn.fujaba.muml.actionlanguage.ActionlanguagePackage;
+import de.uni_paderborn.fujaba.muml.actionlanguage.ElementAccessorExpression;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Element Accessor Expression</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public abstract class ElementAccessorExpressionImpl extends ExpressionImpl implements ElementAccessorExpression {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ElementAccessorExpressionImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ActionlanguagePackage.Literals.ELEMENT_ACCESSOR_EXPRESSION;
+	}
+
+} //ElementAccessorExpressionImpl
