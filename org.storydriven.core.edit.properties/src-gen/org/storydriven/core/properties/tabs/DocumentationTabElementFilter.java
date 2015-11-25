66d8c557437e5178118913c017ae9a2f064ce907
Ingo Budde <bingo@mail.upb.de>
2015-11-25 12:23:49 +0000

    Moving plugins back

--- org.storydriven.core.edit.properties/src-gen/org/storydriven/core/properties/tabs/DocumentationTabElementFilter.java
+++
@@ -1,9 +0,0 @@
-package org.storydriven.core.properties.tabs;
-
-public class DocumentationTabElementFilter
-		extends
-			de.uni_paderborn.fujaba.properties.runtime.filter.ElementFilter {
-	public DocumentationTabElementFilter() {
-		super("property.tab.documentation");
-	}
-}
--- org.storydriven.core.edit.properties/src-gen/org/storydriven/core/properties/tabs/GeneralTabElementFilter.java
+++
@@ -1,9 +0,0 @@
-package org.storydriven.core.properties.tabs;
-
-public class GeneralTabElementFilter
-		extends
-			de.uni_paderborn.fujaba.properties.runtime.filter.ElementFilter {
-	public GeneralTabElementFilter() {
-		super("property.tab.general");
-	}
-}
