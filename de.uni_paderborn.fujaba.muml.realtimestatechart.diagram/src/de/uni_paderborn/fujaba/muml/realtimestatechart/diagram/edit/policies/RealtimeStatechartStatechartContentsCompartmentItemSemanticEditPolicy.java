24cdaf7e7dd6ba904a048bfe96cf33d5d7c41143
Ingo Budde <bingo@mail.upb.de>
2015-11-25 18:27:57 +0000

    Branching plugins for #661 (Make Action Language extensible) and #1333 (Store Expression as String).

---
+++ de.uni_paderborn.fujaba.muml.realtimestatechart.diagram/src/de/uni_paderborn/fujaba/muml/realtimestatechart/diagram/edit/policies/RealtimeStatechartStatechartContentsCompartmentItemSemanticEditPolicy.java
@@ -0,0 +1,43 @@
+/*
+ * <copyright>
+ * Copyright (c) 2013 Software Engineering Group, Heinz Nixdorf Institute, University of Paderborn, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Software Engineering Group - initial API and implementation
+ * </copyright>
+ */
+package de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.edit.policies;
+
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
+
+/**
+ * @generated
+ */
+public class RealtimeStatechartStatechartContentsCompartmentItemSemanticEditPolicy
+		extends de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.edit.policies.MumlBaseItemSemanticEditPolicy {
+
+	/**
+	 * @generated
+	 */
+	public RealtimeStatechartStatechartContentsCompartmentItemSemanticEditPolicy() {
+		super(de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.providers.MumlElementTypes.RealtimeStatechart_2007);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Command getCreateCommand(CreateElementRequest req) {
+		if (de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.providers.MumlElementTypes.State_3032 == req
+				.getElementType()) {
+			return getGEFWrapper(
+					new de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.edit.commands.StateCreateCommand(req));
+		}
+		return super.getCreateCommand(req);
+	}
+
+}
