c5b56a5100cfcdf5bae9f710268ac85464689b94
Ingo Budde <bingo@mail.upb.de>
2015-11-25 12:22:06 +0000

    Adding "org.storydriven.core" to "MUML_1.1_ActionLanguage" branch.

--- MUMLCore/org.storydriven.core.edit.properties/src-gen/org/storydriven/core/properties/tabs/ExtensionsTabElementFilter.java
+++
@@ -1,9 +0,0 @@
-package org.storydriven.core.properties.tabs;
-
-public class ExtensionsTabElementFilter
-		extends
-			de.uni_paderborn.fujaba.properties.runtime.filter.ElementFilter {
-	public ExtensionsTabElementFilter() {
-		super("property.tab.extensions");
-	}
-}
