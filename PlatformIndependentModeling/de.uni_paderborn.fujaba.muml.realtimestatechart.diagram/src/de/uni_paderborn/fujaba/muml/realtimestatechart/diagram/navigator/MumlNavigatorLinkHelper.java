07e8b443aff0459c970a239806380272671e4c76
Ingo Budde <bingo@mail.upb.de>
2016-01-04 13:51:41 +0000

    [RTSC] Readded lost functionality to show raiseMessageEvent parameterBindings at Transition when no schemas are used.

--- PlatformIndependentModeling/de.uni_paderborn.fujaba.muml.realtimestatechart.diagram/src/de/uni_paderborn/fujaba/muml/realtimestatechart/diagram/navigator/MumlNavigatorLinkHelper.java
+++ PlatformIndependentModeling/de.uni_paderborn.fujaba.muml.realtimestatechart.diagram/src/de/uni_paderborn/fujaba/muml/realtimestatechart/diagram/navigator/MumlNavigatorLinkHelper.java
@@ -98,7 +98,8 @@ public class MumlNavigatorLinkHelper implements ILinkHelper {
 		if (abstractNavigatorItem instanceof de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.navigator.MumlNavigatorItem) {
 			navigatorView = ((de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.navigator.MumlNavigatorItem) abstractNavigatorItem)
 					.getView();
-		} else if (abstractNavigatorItem instanceof de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.navigator.MumlNavigatorGroup) {
+		} else
+			if (abstractNavigatorItem instanceof de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.navigator.MumlNavigatorGroup) {
 			de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.navigator.MumlNavigatorGroup navigatorGroup = (de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.navigator.MumlNavigatorGroup) abstractNavigatorItem;
 			if (navigatorGroup
 					.getParent() instanceof de.uni_paderborn.fujaba.muml.realtimestatechart.diagram.navigator.MumlNavigatorItem) {
