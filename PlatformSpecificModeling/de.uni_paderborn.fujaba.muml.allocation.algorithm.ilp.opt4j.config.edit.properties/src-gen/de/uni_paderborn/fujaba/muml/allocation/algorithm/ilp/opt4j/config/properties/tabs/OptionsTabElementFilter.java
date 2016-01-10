792e45500a23ae7009d2c6cb85e5456d485439cc
Marcus Hüwe <mhuewe@mail.upb.de>
2016-01-10 16:36:31 +0000

    [Allocation] sigh... forgot to commit the opt4j.config.edit* plugins

---
+++ PlatformSpecificModeling/de.uni_paderborn.fujaba.muml.allocation.algorithm.ilp.opt4j.config.edit.properties/src-gen/de/uni_paderborn/fujaba/muml/allocation/algorithm/ilp/opt4j/config/properties/tabs/OptionsTabElementFilter.java
@@ -0,0 +1,8 @@
+
+package de.uni_paderborn.fujaba.muml.allocation.algorithm.ilp.opt4j.config.properties.tabs;
+
+public class OptionsTabElementFilter extends de.uni_paderborn.fujaba.properties.runtime.filter.ElementFilter {
+	public OptionsTabElementFilter() {
+		super("options");
+	}
+}
