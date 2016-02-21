190f2c205d00c7dff8479e6b8dd61357f4b59fa8
Uwe Pohlmann <upohl>
2016-02-21 16:18:22 +0000

    added name to state chart and added generated modelica and c-code

---
+++ Examples/de.uni_paderborn.fujaba.muml.example.mini.simpleRTSC/c-src/root-cic/components/aComponentComponent.c
@@ -0,0 +1,53 @@
+		#include "../components/aComponentComponent_Interface.h"
+		
+
+		
+	
+		void AComponentComponent_initialize(AComponentComponent* component) {
+		
+			AComponentSimpleStatechartStateChart_initialize(component->stateChart);
+		
+		}
+		
+
+		AComponentComponent* AComponentComponent_create(void) {
+		
+			AComponentComponent* component = (AComponentComponent*) malloc(
+					sizeof(AComponentComponent));
+		
+			if (component != NULL) {
+		
+				component->stateChart = NULL;
+		
+			} else {
+		
+			}
+		
+			return component;
+		}
+		
+
+		void AComponentComponent_destroy(AComponentComponent* component) {
+			if (component != NULL) {
+		
+				//suicide
+				free(component);
+			}
+		}
+		
+		
+		void AComponentComponent_processStep(AComponentComponent* component) {
+				
+		
+		
+					component->stateChart->AComponentSimpleStatechart_isExecutable = true;
+					component->stateChart->AComponentInit_isExecutable = true;
+				AComponentSimpleStatechartStateChart_processStep(component->stateChart);
+		}
+	
+		AComponentSimpleStatechartStateChart* AComponentComponent_getStateMachine(AComponentComponent* component) {
+					return component->stateChart;
+				}
+
+		
+
