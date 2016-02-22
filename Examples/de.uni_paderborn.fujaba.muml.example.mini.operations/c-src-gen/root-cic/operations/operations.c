d57d897080ba9e910c991ed9d4ed12375b3a734f
Uwe Pohlmann <upohl>
2016-02-22 12:09:26 +0000

    added reference implementation

--- Examples/de.uni_paderborn.fujaba.muml.example.mini.operations/c-src-gen/root-cic/operations/operations.c
+++ Examples/de.uni_paderborn.fujaba.muml.example.mini.operations/c-src-gen/root-cic/operations/operations.c
@@ -1,5 +1,7 @@
 
 #include "operations.h"
+	#include "../components/aComponentComponent_Interface.h"
+
 /** Start of user code user includes **/ 
 
 /**End of user code**/
--- Examples/de.uni_paderborn.fujaba.muml.example.mini.operations/c-src-gen/root-cic/operations/operations.h
+++ Examples/de.uni_paderborn.fujaba.muml.example.mini.operations/c-src-gen/root-cic/operations/operations.h
@@ -6,25 +6,23 @@
 
 	#include "../types/standardTypes.h"
 	#include "../types/customTypes.h"
-	#include "components/aComponentComponent_Interface.h"
 
 
-
-
-// Operations of RTSC of component aComponent
+  	  	  typedef struct AComponentRootStatechartStateChart AComponentRootStatechartStateChart;
+  	  	// Operations of RTSC of component aComponent
 			
 		/**
 		 * @brief Implementation stub of operation decreaseInt
  		 * @details This function shall contain the implementation of the operation decreaseInt.
 		 * @param myInt 
 		 */
-		int32_T aComponentDecreaseInt(AComponentRootStatechartStateChart* stateChart, int32_T myInt);
+		int32_T aComponentDecreaseInt( AComponentRootStatechartStateChart* stateChart, int32_T myInt);
 		/**
 		 * @brief Implementation stub of operation increaseInt
  		 * @details This function shall contain the implementation of the operation increaseInt.
 		 * @param myInt 
 		 */
-		int32_T aComponentIncreaseInt(AComponentRootStatechartStateChart* stateChart, int32_T myInt);
+		int32_T aComponentIncreaseInt( AComponentRootStatechartStateChart* stateChart, int32_T myInt);
 
 #ifdef __cplusplus
   }
