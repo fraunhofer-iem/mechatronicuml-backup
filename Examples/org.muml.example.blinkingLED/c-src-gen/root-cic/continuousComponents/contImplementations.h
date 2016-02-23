50346ebe7d6fcb5fdfdbba852fcf33eeef9ee675
Uwe Pohlmann <upohl>
2016-02-23 10:18:56 +0000

    added src code

---
+++ Examples/org.muml.example.blinkingLED/c-src-gen/root-cic/continuousComponents/contImplementations.h
@@ -0,0 +1,17 @@
+#ifndef CONTIMPLEMENTATION_H
+#define CONTIMPLEMENTATION_H
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+	#include "../types/standardTypes.h"
+	#include "../types/customTypes.h"
+
+void initializeContinuousImplementations();
+
+	void write_voltage_oflEDComponent_C2(int8_T value);
+
+#ifdef __cplusplus
+  }
+#endif
+#endif /* CONTIMPLEMENTATION_H */
