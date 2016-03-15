2f3779c64251f4df4d093f880fa469cbeeb32d86
Sebastian Thiele <sthiele2>
2016-03-15 16:22:02 +0000

    #1492

--- MUMLCore/de.uni_paderborn.fujaba.export/src/de/uni_paderborn/fujaba/export/pages/AbstractFujabaExportSourcePage.java
+++ MUMLCore/de.uni_paderborn.fujaba.export/src/de/uni_paderborn/fujaba/export/pages/AbstractFujabaExportSourcePage.java
@@ -40,8 +40,7 @@ import de.uni_paderborn.fujaba.export.listeners.ResourceChangeListener;
 import de.uni_paderborn.fujaba.export.providers.GreyedAdapterFactoryLabelProvider;
 import de.uni_paderborn.fujaba.export.providers.NullContentProvider;
 
-public abstract class AbstractFujabaExportSourcePage extends
-		ExtensibleModelSelectionPage implements IWizardPage {
+public abstract class AbstractFujabaExportSourcePage extends ExtensibleModelSelectionPage implements IWizardPage {
 
 	/**
 	 * Page Extension that allows the user to select the domain element
@@ -55,7 +54,7 @@ public abstract class AbstractFujabaExportSourcePage extends
 	protected ArrayList<ResourceChangeListener> resourceChangedListeners = new ArrayList<ResourceChangeListener>();
 
 	protected ArrayList<ICheckStateListener> checkStateListeners = new ArrayList<ICheckStateListener>();
-	
+
 	/**
 	 * Creates a fujaba-export source page.
 	 * 
@@ -66,8 +65,8 @@ public abstract class AbstractFujabaExportSourcePage extends
 	 *            The default selection. If null it indicates that nothing is
 	 *            selected.
 	 */
-	public AbstractFujabaExportSourcePage(String pageId, FormToolkit toolkit,
-			ResourceSet resourceSet, ISelection selection) {
+	public AbstractFujabaExportSourcePage(String pageId, FormToolkit toolkit, ResourceSet resourceSet,
+			ISelection selection) {
 		super(pageId, new ResourceLocationProvider(selection), resourceSet);
 		setUnloadAllowed(false);
 		this.toolkit = toolkit;
@@ -77,23 +76,21 @@ public abstract class AbstractFujabaExportSourcePage extends
 		setDescription("Select the MechatronicUML domain element that should be transformed.");
 
 		String fileExtension = wizardPageGetSourceFileExtension();
-		Assert.isNotNull(
-				fileExtension,
+		Assert.isNotNull(fileExtension,
 				"Please implement 'wizardPageGetSourceFileExtension()' to provide a non-null file extension that the MechatronicUML-export-wizard supports.");
 		setModelFileExtension(fileExtension);
 	}
 
-	public void addSelectionListenerOfTreeViewer(ICheckStateListener listener) {		
-		if(domainElementExtension != null && domainElementExtension.getTreeViewer()!= null)
+	public void addSelectionListenerOfTreeViewer(ICheckStateListener listener) {
+		if (domainElementExtension != null && domainElementExtension.getTreeViewer() != null)
 			domainElementExtension.getTreeViewer().addCheckStateListener(listener);
 		else
 			checkStateListeners.add(listener);
 	}
-	
+
 	@Override
 	public void createControl(Composite parent) {
-		int sectionStyle = Section.TITLE_BAR | Section.CLIENT_INDENT
-				| Section.EXPANDED;
+		int sectionStyle = Section.TITLE_BAR | Section.CLIENT_INDENT | Section.EXPANDED;
 		Section section = toolkit.createSection(parent, sectionStyle);
 		section.setText("Source Properties");
 
@@ -102,12 +99,11 @@ public abstract class AbstractFujabaExportSourcePage extends
 		composite.setLayout(new GridLayout());
 		super.createControl(composite);
 		setControl(section);
-		if(domainElementExtension.getTreeViewer()!= null)
-			{
-				for(ICheckStateListener listener : checkStateListeners){
-					domainElementExtension.getTreeViewer().addCheckStateListener(listener);
-				}
+		if (domainElementExtension.getTreeViewer() != null) {
+			for (ICheckStateListener listener : checkStateListeners) {
+				domainElementExtension.getTreeViewer().addCheckStateListener(listener);
 			}
+		}
 		validatePage();
 	}
 
@@ -137,14 +133,12 @@ public abstract class AbstractFujabaExportSourcePage extends
 			for (Object element : ((IStructuredSelection) selection).toArray()) {
 				EObject eObject = null;
 				if (element instanceof IAdaptable) {
-					eObject = (EObject) ((IAdaptable) element)
-							.getAdapter(EObject.class);
+					eObject = (EObject) ((IAdaptable) element).getAdapter(EObject.class);
 				}
 				if (element instanceof EObject) {
 					eObject = (EObject) element;
 				}
-				if (eObject != null && eObject.eResource() != null
-						&& eObject.eResource() == this.getResource()) {
+				if (eObject != null && eObject.eResource() != null && eObject.eResource() == this.getResource()) {
 					validElements.add(eObject);
 				}
 			}
@@ -181,8 +175,7 @@ public abstract class AbstractFujabaExportSourcePage extends
 		return toolkit.createText(parent, "", style);
 	}
 
-	public class DomainElementPageExtension implements
-			ModelSelectionPageExtension {
+	public class DomainElementPageExtension implements ModelSelectionPageExtension {
 
 		protected CheckboxTreeViewer treeViewer;
 		protected Resource resource;
@@ -197,8 +190,7 @@ public abstract class AbstractFujabaExportSourcePage extends
 
 		@Override
 		public void createControl(Composite parent) {
-			int sectionStyle = Section.TITLE_BAR | Section.CLIENT_INDENT
-					| Section.EXPANDED;
+			int sectionStyle = Section.TITLE_BAR | Section.CLIENT_INDENT | Section.EXPANDED;
 			Section section = toolkit.createSection(parent, sectionStyle);
 			section.setText("Select source elements");
 
@@ -228,13 +220,10 @@ public abstract class AbstractFujabaExportSourcePage extends
 				@Override
 				public void checkStateChanged(CheckStateChangedEvent event) {
 					if (event.getChecked()) {
-						if (!wizardPageSupportsSourceModelElement((EObject) event
-								.getElement())) {
-							event.getCheckable().setChecked(event.getElement(),
-									false);
+						if (!wizardPageSupportsSourceModelElement((EObject) event.getElement())) {
+							event.getCheckable().setChecked(event.getElement(), false);
 						} else if (wizardPageGetSupportedSelectionMode() != ElementSelectionMode.ELEMENT_SELECTION_MODE_MULTI) {
-							treeViewer.setCheckedElements(new Object[] { event
-									.getElement() });
+							treeViewer.setCheckedElements(new Object[] { event.getElement() });
 						}
 					}
 
@@ -253,23 +242,17 @@ public abstract class AbstractFujabaExportSourcePage extends
 				EditingDomain editingDomain = AdapterFactoryEditingDomain
 						.getEditingDomainFor(resource.getContents().get(0));
 				if (editingDomain instanceof AdapterFactoryEditingDomain) {
-					adapterFactory = ((AdapterFactoryEditingDomain) editingDomain)
-							.getAdapterFactory();
+					adapterFactory = ((AdapterFactoryEditingDomain) editingDomain).getAdapterFactory();
 				}
 			}
 			if (adapterFactory != null) {
-				treeViewer
-						.setContentProvider(new AdapterFactoryContentProvider(
-								adapterFactory));
-				treeViewer
-						.setLabelProvider(new GreyedAdapterFactoryLabelProvider(
-								adapterFactory) {
-							@Override
-							public boolean isEnabled(Object object) {
-								return object instanceof EObject
-										&& wizardPageSupportsSourceModelElement((EObject) object);
-							}
-						});
+				treeViewer.setContentProvider(new AdapterFactoryContentProvider(adapterFactory));
+				treeViewer.setLabelProvider(new GreyedAdapterFactoryLabelProvider(adapterFactory) {
+					@Override
+					public boolean isEnabled(Object object) {
+						return object instanceof EObject && wizardPageSupportsSourceModelElement((EObject) object);
+					}
+				});
 				treeViewer.setInput(resource);
 				selection = wizardPageGetDefaultSelection(resource);
 			} else {
@@ -282,11 +265,8 @@ public abstract class AbstractFujabaExportSourcePage extends
 		public void setCheckedElements(ISelection selection) {
 			treeViewer.setSelection(selection);
 			if (selection instanceof IStructuredSelection) {
-				treeViewer
-						.setCheckedElements(((IStructuredSelection) selection)
-								.toArray());
-				for (Object element : ((IStructuredSelection) selection)
-						.toArray()) {
+				treeViewer.setCheckedElements(((IStructuredSelection) selection).toArray());
+				for (Object element : ((IStructuredSelection) selection).toArray()) {
 					treeViewer.reveal(element);
 				}
 			} else {
@@ -304,8 +284,7 @@ public abstract class AbstractFujabaExportSourcePage extends
 	@Override
 	public void validatePage() {
 		ElementSelectionMode elementSelectionMode = wizardPageGetSupportedSelectionMode();
-		Assert.isNotNull(
-				elementSelectionMode,
+		Assert.isNotNull(elementSelectionMode,
 				"Please implement 'wizardGetSupportedSelectionMode()' to provide a non-null selection mode that your MechatronicUML export wizard supports.");
 
 		String errorMessage = null;
@@ -319,8 +298,7 @@ public abstract class AbstractFujabaExportSourcePage extends
 					break;
 				}
 			}
-			if (errorMessage == null
-					&& elementSelectionMode != ElementSelectionMode.ELEMENT_SELECTION_MODE_MULTI
+			if (errorMessage == null && elementSelectionMode != ElementSelectionMode.ELEMENT_SELECTION_MODE_MULTI
 					&& getSourceElements().length > 1) {
 				errorMessage = "Multiple source elements are not supported.";
 			}
@@ -345,14 +323,17 @@ public abstract class AbstractFujabaExportSourcePage extends
 
 	// default implementation
 	public ISelection wizardPageGetDefaultSelection(Resource resource) {
-		TreeIterator<EObject> it = resource.getAllContents();
-		while (it.hasNext()) {
-			EObject element = it.next();
-			if (wizardPageSupportsSourceModelElement(element)) {
-				return new StructuredSelection(element);
+		if (this.selection == null) {
+			TreeIterator<EObject> it = resource.getAllContents();
+			while (it.hasNext()) {
+				EObject element = it.next();
+				if (wizardPageSupportsSourceModelElement(element)) {
+					this.selection = new StructuredSelection(element);
+				}
 			}
 		}
-		return null;
+
+		return this.selection;
 	}
 
 	public void addResourceChangeListener(ResourceChangeListener listener) {
