6e19893f3376d055a2e027764bf6b4f8a04e7e46
Ingo Budde <bingo@mail.upb.de>
2016-03-21 10:37:50 +0000

    [PSM] Check in generated Xtend Java files.

---
+++ PlatformSpecificModeling/org.muml.psm.allocation.ilp.lpsolve.xtext/xtend-gen/de/uni_paderborn/fujaba/muml/allocation/ilp/lpsolve/xtext/resource/LPSolveResource.java
@@ -0,0 +1,21 @@
+package de.uni_paderborn.fujaba.muml.allocation.ilp.lpsolve.xtext.resource;
+
+import com.google.inject.Injector;
+import de.uni_paderborn.fujaba.common.xtext.AbstractLanguageResource;
+import de.uni_paderborn.fujaba.muml.allocation.ilp.IntegerLinearProgram;
+import de.uni_paderborn.fujaba.muml.allocation.ilp.lpsolve.xtext.LPSolveStandaloneSetup;
+import org.eclipse.emf.ecore.EObject;
+
+@SuppressWarnings("all")
+public class LPSolveResource extends AbstractLanguageResource {
+  @Override
+  protected Injector getInjector() {
+    LPSolveStandaloneSetup _lPSolveStandaloneSetup = new LPSolveStandaloneSetup();
+    return _lPSolveStandaloneSetup.createInjectorAndDoEMFRegistration();
+  }
+  
+  @Override
+  protected boolean isSerializable(final EObject object) {
+    return (object instanceof IntegerLinearProgram);
+  }
+}
