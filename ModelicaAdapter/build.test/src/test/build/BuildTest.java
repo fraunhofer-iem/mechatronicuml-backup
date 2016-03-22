a33e90152b094e2ef91eccf3bbc2a240cac3fea5
Andreas Dann <adann@mail.upb.de>
2016-03-22 12:40:06 +0000

    adapted build.test for modelica

--- ModelicaAdapter/build.test/src/test/build/BuildTest.java
+++ ModelicaAdapter/build.test/src/test/build/BuildTest.java
@@ -23,7 +23,7 @@ public class BuildTest {
 
 		// close the metamodel for building acceleo project
 		IProject project = root.getProject("org.muml.modelica.adapter.m2t.transform");
-		project.close(progressMonitor);
+	//	project.close(progressMonitor);
 		project = root.getProject("org.muml.modelica.adapter.m2t.transform.edit");
 	//	project.close(progressMonitor);
 		root.accept(new IResourceVisitor() {
