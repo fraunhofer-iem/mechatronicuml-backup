4dab094b88517a4b61416cda811dde419d537bdd
Andreas Dann <adann@mail.upb.de>
2016-03-30 17:46:52 +0000

    adapted code for acceleo build plugin

--- Graphviz/m2t/build.execution/src/build/execution/Startup.java
+++ Graphviz/m2t/build.execution/src/build/execution/Startup.java
@@ -1,16 +1,23 @@
 package build.execution;
 
 import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 
 public class Startup implements IStartup {
 
 	@Override
 	public void earlyStartup() {
-		// TODO Auto-generated method stub
-		BuildExecution build = new BuildExecution();
-		build.test();
-		PlatformUI.getWorkbench().close();
+		final IWorkbench workbench = PlatformUI.getWorkbench();
+		workbench.getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				
+				BuildExecution build = new BuildExecution();
+				build.test();
+				workbench.close();
+				
+			}
+		});
 	}
 
 }
