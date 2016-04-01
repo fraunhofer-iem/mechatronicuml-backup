90a945a7b5406828722dac9349e0c4657bbc51bd
Andreas Dann <adann@mail.upb.de>
2016-04-01 16:15:23 +0000

    adapted build config

--- DevelopmentTools/build.test/src/test/build/BuildTest.java
+++ DevelopmentTools/build.test/src/test/build/BuildTest.java
@@ -1,5 +1,7 @@
 package test.build;
 
+import static org.junit.Assert.*;
+
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.IWorkspace;
@@ -9,28 +11,46 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
 
 public class BuildTest {
 
 	@Test
-	public void test() throws CoreException {
+	public void test()  {
 		IProgressMonitor progressMonitor = new NullProgressMonitor();
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 
-		root.accept(new IResourceVisitor() {
+		try {
+			root.accept(new IResourceVisitor() {
 
-			@Override
-			public boolean visit(IResource resource) throws CoreException {
-				System.out.println(resource.getName());
-				if (resource instanceof IWorkspaceRoot)
-					return true;
-				return false;
-			}
-		});
-		workspace.build(IncrementalProjectBuilder.FULL_BUILD, progressMonitor);
-		workspace.save(true, progressMonitor);
+				@Override
+				public boolean visit(IResource resource) throws CoreException {
+					System.out.println(resource.getName());
+					if (resource instanceof IWorkspaceRoot)
+						return true;
+					return false;
+				}
+			});
+		} catch (CoreException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			workspace.build(IncrementalProjectBuilder.FULL_BUILD, progressMonitor);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			fail("build failed!!!!!!!!");
+			e.printStackTrace();
+		}
+		try {
+			workspace.save(true, progressMonitor);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
