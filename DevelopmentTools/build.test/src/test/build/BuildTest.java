013682b40ac5587b2c38a0623259484c6cf19eff
Andreas Dann <adann@mail.upb.de>
2016-04-01 16:52:42 +0000

    added test to find compilation problems

--- DevelopmentTools/build.test/src/test/build/BuildTest.java
+++ DevelopmentTools/build.test/src/test/build/BuildTest.java
@@ -1,7 +1,9 @@
 package test.build;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import org.eclipse.acceleo.internal.ide.ui.builders.AcceleoMarkerUtils;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.IWorkspace;
@@ -11,14 +13,14 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.junit.Assert;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaModelMarker;
 import org.junit.Test;
-import org.junit.internal.runners.statements.Fail;
 
 public class BuildTest {
 
 	@Test
-	public void test()  {
+	public void test() {
 		IProgressMonitor progressMonitor = new NullProgressMonitor();
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
@@ -45,6 +47,29 @@ public class BuildTest {
 			fail("build failed!!!!!!!!");
 			e.printStackTrace();
 		}
+		
+		try {
+			root.accept(new IResourceVisitor() {
+
+				@Override
+				public boolean visit(IResource resource) throws CoreException {
+					System.out.println(resource.getName());
+					if (resource instanceof ICompilationUnit)
+						if(findJavaProblemMarkers((ICompilationUnit) resource).length>0){
+							fail("compilation problem");
+						};
+					return false;
+				}
+			});
+		} catch (CoreException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		
+		
+		
 		try {
 			workspace.save(true, progressMonitor);
 		} catch (CoreException e) {
@@ -53,4 +78,17 @@ public class BuildTest {
 		}
 	}
 
+	
+	  public IMarker[] findJavaProblemMarkers(ICompilationUnit cu) 
+		      throws CoreException {
+		      IResource javaSourceFile = cu.getUnderlyingResource();
+		      IMarker[] markers = 
+		         javaSourceFile.findMarkers(IJavaModelMarker.JAVA_MODEL_PROBLEM_MARKER,
+		            true, IResource.DEPTH_INFINITE);
+		      javaSourceFile.findMarkers(AcceleoMarkerUtils.PROBLEM_MARKER_ID,
+			            true, IResource.DEPTH_INFINITE);
+			return markers;
+		   }
+
+	
 }
