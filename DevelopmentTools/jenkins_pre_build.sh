56a5714eaf73f1bcca1ed16ee8ac8cb4c7689df7
Andreas Dann <adann@mail.upb.de>
2016-04-01 16:12:00 +0000

    adapted pre config

--- DevelopmentTools/jenkins_pre_build.sh
+++ DevelopmentTools/jenkins_pre_build.sh
@@ -35,13 +35,13 @@ function build_command(){
 		then
 			sed -i 's#</feature>#<plugin\nid="build.test"\ndownload-size="0"\ninstall-size="0"\nversion="0.0.0"\n unpack="false"/>\n</feature>#' "$BUCKMINSTER_FEATURE_DIR/feature.xml"
 		fi
-		BUILDCOMMAND=$(echo -e "build -c --continueonerror \nlaunch -t 240 -l build.test/BuildTest.launch --stdout --stderr")
+		BUILDCOMMAND=$(echo -e "build -c --continueonerror \n junit -t 240 -l build.test/BuildTest.launch --stdout --stderr")
 		if [ -n "$XTEND" ]
 		then
 			BUILDCOMMAND=$(echo -e "$BUILDCOMMAND \n build")
 			if [ -n "$ACCELEO" ] 
 			then
-				BUILDCOMMAND=$(echo -e "$BUILDCOMMAND \n launch -t 240 -l build.test/BuildTest.launch --stdout --stderr")
+				BUILDCOMMAND=$(echo -e "$BUILDCOMMAND \n junit -t 240 -l build.test/BuildTest.launch --stdout --stderr")
 			fi
 		fi
 	fi	
