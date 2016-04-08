44be82425330e0f703cd26d227266648ab3f004e
Ingo Budde <bingo@mail.upb.de>
2016-04-08 20:23:15 +0000

    [Renaming] [Migration] [SimulinkAdapter] Textual replacement of nsURIs from fujaba2simulink.ecore.

--- SimulinkAdapter/org.muml.simulink.adapter.corrmodel/src/org/muml/simulink/adapter/corrmodel/behavior/Behavior_modelPackage.java
+++ SimulinkAdapter/org.muml.simulink.adapter.corrmodel/src/org/muml/simulink/adapter/corrmodel/behavior/Behavior_modelPackage.java
@@ -42,7 +42,7 @@ public interface Behavior_modelPackage extends EPackage {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	String eNS_URI = "http://de.uni_paderborn.fujaba2simulink.behavior";
+	String eNS_URI = "http://www.muml.org/simulink/adapter/corrmodel/behavior/1.0.0";
 
 	/**
 	 * The package namespace name.
