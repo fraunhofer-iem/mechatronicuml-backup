622b3b0d02518d7cffd66452a6cf6c9cd6dc3b6c
Ingo Budde <bingo@mail.upb.de>
2016-04-08 19:35:36 +0000

    [Renaming] [Migration] [ReconfigurationVerification] Textual replacement of nsURIs from TCTL.ecore.

--- ReconfigurationVerification/org.muml.reconfigurationverification.fotctl.xtext/src-gen/org/muml/reconfigurationverification/fotctl/xtext/validation/AbstractFOTCTLJavaValidator.java
+++ ReconfigurationVerification/org.muml.reconfigurationverification.fotctl.xtext/src-gen/org/muml/reconfigurationverification/fotctl/xtext/validation/AbstractFOTCTLJavaValidator.java
@@ -14,7 +14,7 @@ public class AbstractFOTCTLJavaValidator extends org.eclipse.xtext.validation.Ab
 	protected List<EPackage> getEPackages() {
 	    List<EPackage> result = new ArrayList<EPackage>();
 	    result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.muml.org/reconfigurationverification/fotctl/1.0.0"));
-	    result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.fujaba.de/muml/verification/tctl/0.1.0"));
+	    result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.muml.org/reconfigurationverification/tctl/1.0.0"));
 		return result;
 	}
 
