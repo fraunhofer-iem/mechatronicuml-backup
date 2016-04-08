6385d5740eaaa44797b5b1590c9b6380f905bb9d
Ingo Budde <bingo@mail.upb.de>
2016-04-08 23:05:34 +0000

    [Renaming] [Migration] Textual replacement of references to org.muml.pim.component.diagram/model

--- PlatformIndependentModeling/org.muml.pim.tests/src/org/muml/pim/tests/gmf/PimGMFMapTest.java
+++ PlatformIndependentModeling/org.muml.pim.tests/src/org/muml/pim/tests/gmf/PimGMFMapTest.java
@@ -22,7 +22,7 @@ public class PimGMFMapTest extends GMFMapTest {
 	@Parameters
 	public static Collection<Object[]> getMappingModels() {
 		List<Object[]> mappingModels = new ArrayList<Object[]>();
-		mappingModels.add(new Object[] { "de.uni_paderborn.fujaba.muml.component.diagram/model/component.gmfmap" });
+		mappingModels.add(new Object[] { "org.muml.pim.component.diagram/model/component.gmfmap" });
 		mappingModels.add(new Object[] { "de.uni_paderborn.fujaba.muml.componentinstanceconfiguration.diagram/model/componentinstanceconfiguration.gmfmap" });
 		mappingModels.add(new Object[] { "de.uni_paderborn.fujaba.muml.realtimestatechart.diagram/model/realtimestatechart.gmfmap" });
 		return mappingModels;
