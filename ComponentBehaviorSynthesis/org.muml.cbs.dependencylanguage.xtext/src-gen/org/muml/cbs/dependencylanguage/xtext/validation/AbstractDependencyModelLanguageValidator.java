1b27ada68083dd0fdfadace043a0238bad45d072
Ingo Budde <bingo@mail.upb.de>
2016-04-08 19:26:14 +0000

    [Renaming] [Migration] [PIM] Textual replacement of nsURIs (actionlanguage.ecore).

--- ComponentBehaviorSynthesis/org.muml.cbs.dependencylanguage.xtext/src-gen/org/muml/cbs/dependencylanguage/xtext/validation/AbstractDependencyModelLanguageValidator.java
+++ ComponentBehaviorSynthesis/org.muml.cbs.dependencylanguage.xtext/src-gen/org/muml/cbs/dependencylanguage/xtext/validation/AbstractDependencyModelLanguageValidator.java
@@ -13,7 +13,7 @@ public class AbstractDependencyModelLanguageValidator extends org.eclipse.xtext.
 	protected List<EPackage> getEPackages() {
 	    List<EPackage> result = new ArrayList<EPackage>();
 	    result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.fujaba.de/muml/dependencylanguage/0.4.0"));
-	    result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.fujaba.de/muml/actionlanguage/0.4.0"));
+	    result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.muml.org/pim/actionlanguage/1.0.0"));
 	    result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.muml.org/pim/valuetype/1.0.0"));
 	    result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.storydriven.org/core/expressions/0.3.1"));
 	    result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.muml.org/pim/behavior/1.0.0"));
