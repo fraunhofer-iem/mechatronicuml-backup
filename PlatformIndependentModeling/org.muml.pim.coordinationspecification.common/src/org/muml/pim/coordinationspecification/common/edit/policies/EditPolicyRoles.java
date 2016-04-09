12247848619ed2422e74e2d1b906f64810feebfa
Ingo Budde <bingo@mail.upb.de>
2016-04-09 01:01:23 +0000

    [Renaming] [Migration] Textual replacement of references to org.muml.pim.coordinationspecification.common

--- PlatformIndependentModeling/org.muml.pim.coordinationspecification.common/src/org/muml/pim/coordinationspecification/common/edit/policies/EditPolicyRoles.java
+++ PlatformIndependentModeling/org.muml.pim.coordinationspecification.common/src/org/muml/pim/coordinationspecification/common/edit/policies/EditPolicyRoles.java
@@ -2,9 +2,9 @@ package org.muml.pim.coordinationspecification.common.edit.policies;
 
 public interface EditPolicyRoles extends org.muml.core.common.edit.policies.EditPolicyRoles {
 	
-	// name to EditPolicy de.uni_paderborn.fujaba.muml.coordinationspecification.common.edit.policies.CustomConnectorOfServiceAssumptionsWrappedLabelUpdateEditPolicy
+	// name to EditPolicy org.muml.pim.coordinationspecification.common.edit.policies.CustomConnectorOfServiceAssumptionsWrappedLabelUpdateEditPolicy
 	public static final String QoSA_LABEL_UpdateEditPolicy = "CustomQualityOfServiceAssumptionsLabelUpdateEditPolicy"; //$NON-NLS-1$
 	
-	// name of EditPolicy de.uni_paderborn.fujaba.muml.coordinationspecification.common.edit.policies.CustomAbstractCoordinationSpecificationUpdateEditPolicy
+	// name of EditPolicy org.muml.pim.coordinationspecification.common.edit.policies.CustomAbstractCoordinationSpecificationUpdateEditPolicy
 	public static final String ABSTRACT_COORDINATION_SPECIFICATION_EDITPOLICY = "CustomQualityOfServiceAssumptionsLabelUpdateEditPolicy";
 }
