0b94645c914301eb754a9961d8b98ac7735eeacb
Ingo Budde <bingo@mail.upb.de>
2016-04-18 14:29:57 +0000

    [PM] Removed imported package declaration

--- PlatformModeling/org.muml.pm.software.oslanguage.xtext.ui/src-gen/org/muml/pm/software/oslanguage/xtext/ui/OperatingSystemLanguageExecutableExtensionFactory.java
+++ PlatformModeling/org.muml.pm.software.oslanguage.xtext.ui/src-gen/org/muml/pm/software/oslanguage/xtext/ui/OperatingSystemLanguageExecutableExtensionFactory.java
@@ -4,11 +4,10 @@
 package org.muml.pm.software.oslanguage.xtext.ui;
 
 import org.eclipse.xtext.ui.guice.AbstractGuiceAwareExecutableExtensionFactory;
+import org.muml.pm.software.oslanguage.xtext.ui.internal.OperatingSystemLanguageActivator;
 import org.osgi.framework.Bundle;
 
 import com.google.inject.Injector;
-
-import org.muml.pm.software.oslanguage.xtext.ui.internal.OperatingSystemLanguageActivator;
 
 /**
  * This class was generated. Customizations should only happen in a newly
