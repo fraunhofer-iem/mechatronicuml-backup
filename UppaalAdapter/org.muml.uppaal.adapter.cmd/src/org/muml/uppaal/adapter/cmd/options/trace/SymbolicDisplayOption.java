703b42358d0a996e9fcd218f9501327eb3005531
Christopher Gerking <cgerking@campus.upb.de>
2016-04-27 12:56:16 +0000

    Remove unused org.muml.uppaal.adapter.cmd plugin

--- UppaalAdapter/org.muml.uppaal.adapter.cmd/src/org/muml/uppaal/adapter/cmd/options/trace/SymbolicDisplayOption.java
+++
@@ -1,8 +0,0 @@
-package org.muml.uppaal.adapter.cmd.options.trace;
-
-import org.muml.core.common.cmd.Option;
-import org.muml.uppaal.adapter.cmd.VerifyTACommand;
-
-public abstract class SymbolicDisplayOption extends Option<VerifyTACommand> {
-
-}
