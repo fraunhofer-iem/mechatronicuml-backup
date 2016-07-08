803e57d3ee082eb5bb4d00f766284714edc789ef
Marcus Hüwe <mhuewe@mail.upb.de>
2016-07-08 11:44:27 +0000

    [PSM] removed @NonNull annotation (just for testing...)

--- PlatformSpecificModeling/bar/src/bar/BarCSPostOrderVisitor.java
+++ PlatformSpecificModeling/bar/src/bar/BarCSPostOrderVisitor.java
@@ -1,12 +1,11 @@
 package bar;
 
-import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.ocl.xtext.base.cs2as.CS2ASConversion;
 import org.eclipse.ocl.xtext.completeocl.cs2as.CompleteOCLCSPostOrderVisitor;
 
 public class BarCSPostOrderVisitor extends CompleteOCLCSPostOrderVisitor {
 
-	public BarCSPostOrderVisitor(@NonNull CS2ASConversion context) {
+	public BarCSPostOrderVisitor(CS2ASConversion context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 	}	
