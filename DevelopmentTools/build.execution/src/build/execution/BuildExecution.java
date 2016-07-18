ba1c6ae673f48530579b9990be3564af4cf2c08f
Marcus Hüwe <mhuewe@mail.upb.de>
2016-07-18 16:18:59 +0000

    [DevelopmentTools] MyCompiler

--- DevelopmentTools/build.execution/src/build/execution/BuildExecution.java
+++ DevelopmentTools/build.execution/src/build/execution/BuildExecution.java
@@ -1,7 +1,14 @@
 package build.execution;
 
+import java.io.File;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.log4j.BasicConfigurator;
 import org.eclipse.acceleo.internal.ide.ui.builders.AcceleoMarkerUtils;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
@@ -18,9 +25,80 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.xtend.core.XtendInjectorSingleton;
 import org.eclipse.xtend.core.compiler.batch.Main;
+import org.eclipse.xtend.core.compiler.batch.XtendBatchCompiler;
+
+import com.google.common.base.Joiner;
+import com.google.inject.Injector;
 
 public class BuildExecution {
+	
+	private static class MyCompiler {
+		public static void main(String[] args) {
+			List<String> existingDirs = new ArrayList<String>();
+			BasicConfigurator.configure();
+			Injector injector = XtendInjectorSingleton.INJECTOR;
+			XtendBatchCompiler compiler = injector.getInstance(XtendBatchCompiler.class);
+			if ((args == null) || (args.length == 0)) {
+				printUsage();
+				return;
+			}
+			Iterator<String> arguments = Arrays.asList(args).iterator();
+			while (arguments.hasNext()) {
+				String argument = arguments.next().trim();
+				if ("-d".equals(argument)) {
+					compiler.setOutputPath(arguments.next().trim());
+				} else if ("-classpath".equals(argument) || "-cp".equals(argument)) {
+					compiler.setClassPath(arguments.next().trim());
+				} else if ("-tempdir".equals(argument) || "-td".equals(argument)) {
+					compiler.setTempDirectory(arguments.next().trim());
+				} else if ("-encoding".equals(argument)) {
+					compiler.setFileEncoding(arguments.next().trim());
+				} else if ("-javaSourceVersion".equals(argument)) {
+					compiler.setJavaSourceVersion(arguments.next().trim());
+				} else if ("-noSuppressWarningsAnnotation".equals(argument)) {
+					compiler.setGenerateSyntheticSuppressWarnings(false);
+				} else if ("-generateGeneratedAnnotation".equals(argument)) {
+					compiler.setGenerateGeneratedAnnotation(true);
+				} else if ("-includeDateInGeneratedAnnnotation".equals(argument)) {
+					compiler.setIncludeDateInGeneratedAnnotation(true);
+				} else if ("-generateAnnotationComment".equals(argument)) {
+					compiler.setGeneratedAnnotationComment(arguments.next().trim());
+				} else if ("-useCurrentClassLoader".equals(argument)) {
+					compiler.setUseCurrentClassLoaderAsParent(true);
+				} else if ("-writeTraceFiles".equals(argument)) {
+					compiler.setWriteTraceFiles(true);
+				} else {
+					existingDirs.add(argument);
+					String pathes = Joiner.on(File.pathSeparator).join(existingDirs);
+					compiler.setSourcePath(pathes);
+				}
+			}
+			if (!compiler.compile()) {
+				System.out.println("XXX: failed");
+			} else {
+				System.out.println("XXX: no failure");
+			}
+		}
+
+		private static void printUsage() {
+			PrintStream out = System.out;
+			out.println("Usage: Main <options> <source directories>");
+			out.println("where possible options include:");
+			out.println("-d <directory>                      Specify where to place generated xtend files");
+			out.println("-tp <path>                          Temp directory to hold generated stubs and classes");
+			out.println("-cp <path>                          Specify where to find user class files");
+			out.println("-encoding <encoding>                Specify character encoding used by source files");
+			out.println("-javaSourceVersion <version>        Create Java Source compatible to this version. Can be: 1.5, 1.6, 1.7 or 1.8");
+			out.println("-noSuppressWarningsAnnotation       Don't put @SuppressWarnings() into generated Java Code");
+			out.println("-generateGeneratedAnnotation        Put @Generated into generated Java Code");
+			out.println("-includeDateInGeneratedAnnnotation  If -generateGeneratedAnnotation is used, add the current date/time.");
+			out.println("-generateAnnotationComment <string> If -generateGeneratedAnnotation is used, add a comment.");
+			out.println("-useCurrentClassLoader              Use current classloader as parent classloader");
+			out.println("-writeTraceFiles                    Write Trace-Files");
+		}
+	}
 
 	public static IMarker[] build() throws CoreException, InvocationTargetException, InterruptedException {
 		IProgressMonitor progressMonitor = new NullProgressMonitor();
@@ -36,7 +114,7 @@ public class BuildExecution {
 					return true;
 				if (resource.getName().endsWith("xtext") || resource.getName().endsWith("xtext.ui")) {
 					System.out.println("before start: " + resource.getName());
-					Main.main(new String[] {
+					MyCompiler.main(new String[] {
 						"-classpath", "${project_classpath:" + resource.getName() + "}",
 						"-d", "xtend-gen",
 						"src", "src-gen"
