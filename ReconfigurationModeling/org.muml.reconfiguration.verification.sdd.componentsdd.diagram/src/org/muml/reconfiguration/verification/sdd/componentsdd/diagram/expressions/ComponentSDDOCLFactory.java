1583b8467bbdcccd2c248c626b4c2545ab09ac16
Uwe Pohlmann <upohl>
2016-09-30 10:49:23 +0000

    do or do not.... there is no try... fixed parserException

--- ReconfigurationModeling/org.muml.reconfiguration.verification.sdd.componentsdd.diagram/src/org/muml/reconfiguration/verification/sdd/componentsdd/diagram/expressions/ComponentSDDOCLFactory.java
+++ ReconfigurationModeling/org.muml.reconfiguration.verification.sdd.componentsdd.diagram/src/org/muml/reconfiguration/verification/sdd/componentsdd/diagram/expressions/ComponentSDDOCLFactory.java
@@ -151,8 +151,9 @@ public class ComponentSDDOCLFactory {
 		public Expression(String body, EClassifier context, Map<String, EClassifier> environment) {
 			super(body, context);
 			pivotOclInstance = org.eclipse.ocl.pivot.utilities.OCL.newInstance();
-			OCLHelper pivotHelper = pivotOclInstance.createOCLHelper(context);
+			
 			try {
+				OCLHelper pivotHelper = pivotOclInstance.createOCLHelper(context);
 				pivotOclExpression = pivotHelper.createQuery(body());
 				setStatus(IStatus.OK, null, null);
 			} catch (org.eclipse.ocl.pivot.utilities.ParserException e1) {
