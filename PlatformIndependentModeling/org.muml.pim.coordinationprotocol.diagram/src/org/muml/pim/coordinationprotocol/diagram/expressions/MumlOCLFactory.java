7f55b3d8f810a7a28ed8863cd5855c006387b746
Uwe Pohlmann <upohl>
2016-09-30 09:27:29 +0000

    fixed parserException

--- PlatformIndependentModeling/org.muml.pim.coordinationprotocol.diagram/src/org/muml/pim/coordinationprotocol/diagram/expressions/MumlOCLFactory.java
+++ PlatformIndependentModeling/org.muml.pim.coordinationprotocol.diagram/src/org/muml/pim/coordinationprotocol/diagram/expressions/MumlOCLFactory.java
@@ -150,8 +150,9 @@ public class MumlOCLFactory {
 		public Expression(String body, EClassifier context, Map<String, EClassifier> environment) {
 			super(body, context);
 			pivotOclInstance = org.eclipse.ocl.pivot.utilities.OCL.newInstance();
-			OCLHelper pivotHelper = pivotOclInstance.createOCLHelper(context);
+			
 			try {
+				OCLHelper pivotHelper = pivotOclInstance.createOCLHelper(context);
 				pivotOclExpression = pivotHelper.createQuery(body());
 				setStatus(IStatus.OK, null, null);
 			} catch (org.eclipse.ocl.pivot.utilities.ParserException e1) {
