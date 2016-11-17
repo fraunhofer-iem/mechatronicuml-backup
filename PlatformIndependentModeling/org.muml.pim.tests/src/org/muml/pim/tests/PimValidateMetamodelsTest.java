c468862cccbed695b2c4deee3f53f1f4f1245f30
Ingo Budde <bingo@mail.upb.de>
2016-11-17 13:53:52 +0000

    [Tests] Added dependency to actionlanguage.

--- PlatformIndependentModeling/org.muml.pim.tests/src/org/muml/pim/tests/PimValidateMetamodelsTest.java
+++ PlatformIndependentModeling/org.muml.pim.tests/src/org/muml/pim/tests/PimValidateMetamodelsTest.java
@@ -7,13 +7,19 @@ import java.util.Collection;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.muml.core.CorePackage;
 import org.muml.core.tests.ValidateMetamodelsTest;
+import org.muml.pim.PimPackage;
+import org.muml.pim.actionlanguage.ActionlanguagePackage;
 
 @RunWith(Parameterized.class)
 public class PimValidateMetamodelsTest extends ValidateMetamodelsTest {
 
 	public PimValidateMetamodelsTest(String resourcePath) {
 		super(resourcePath);
+		CorePackage.eINSTANCE.eClass();
+		PimPackage.eINSTANCE.eClass();
+		ActionlanguagePackage.eINSTANCE.eClass();
 	}
 
 	@Parameters
