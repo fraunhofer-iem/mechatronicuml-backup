af31d8574130b9be3922ff59ca74bdef0d6a878f
Uwe Pohlmann <upohl>
2017-01-25 14:48:29 +0000

    test

---
+++ Example/inProgress/org.muml.example.productionBroker/src-gen-container/mx.0.1/operations/iDManagementOpRep.h
@@ -0,0 +1,23 @@
+#ifndef IDMANAGEMENT_H_
+#define IDMANAGEMENT_H_
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+	#include "../types/standardTypes.h"
+	#include "../types/customTypes.h"
+
+
+			
+		/**
+		 * @brief Implementation stub of operation getID
+ 		 * @details This function shall contain the implementation of the operation getID.
+		 * 
+		 */
+		int32_T IDManagement_iDManagementGetID();
+
+
+#ifdef __cplusplus
+  }
+#endif
+#endif /*IDMANAGEMENT_H_ */
