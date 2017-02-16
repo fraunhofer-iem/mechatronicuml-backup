0986adceba2f46a3742fc0c9928f86870d77c797
Christopher Gerking <cgerking@campus.upb.de>
2017-02-16 13:37:23 +0000

    Implement getValues(...) in QVTo instead of Java Enable mapping inheritance for transform(...) Avoid code duplication for create(...)

--- org.muml.modelcopy.qvto.library/src/org/muml/modelcopy/qvto/library/ModelcopyBlackbox.java
+++ org.muml.modelcopy.qvto.library/src/org/muml/modelcopy/qvto/library/ModelcopyBlackbox.java
@@ -1,8 +1,5 @@
 package org.muml.modelcopy.qvto.library;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
@@ -49,23 +46,6 @@ public class ModelcopyBlackbox {
 		return false;
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static List<Object> getValues(EObject element, EStructuralFeature feature) {
-		Object value = element.eGet(feature);
-		if (feature.isMany()) {
-			return new ArrayList<Object>((Collection) value);
-		} else if (value != null) {
-			return Collections.singletonList(value);
-		}
-		return Collections.emptyList();
-	}
-	
-	
-//	@Operation(contextual=true)
-//	public static boolean before(Date self, Date when) {
-//		return self.before(when);
-//	}
-//	
 }
 
 
