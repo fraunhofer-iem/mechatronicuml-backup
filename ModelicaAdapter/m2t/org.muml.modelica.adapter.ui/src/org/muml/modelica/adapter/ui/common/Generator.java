19accd1b353f4227c1732fba0ecfaa8ff8717ec1
Uwe Pohlmann <upohl>
2017-03-01 11:52:09 +0000

    added logging

--- ModelicaAdapter/m2t/org.muml.modelica.adapter.ui/src/org/muml/modelica/adapter/ui/common/Generator.java
+++ ModelicaAdapter/m2t/org.muml.modelica.adapter.ui/src/org/muml/modelica/adapter/ui/common/Generator.java
@@ -7,8 +7,10 @@ import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.BasicMonitor;
 import org.muml.modelica.adapter.main.Generate;
+import org.muml.modelica.adapter.ui.Activator;
 import org.muml.pim.instance.ComponentInstanceConfiguration;
 
 public class Generator {
@@ -24,7 +26,14 @@ public class Generator {
 			//GenerateAll generator = new GenerateAll(modelURI, target, Collections.<Object>emptyList());
 			//generator.doGenerate(monitor);
 			Generate generator = new Generate(cic, directory.getLocation().toFile(), Collections.<Object>emptyList());
+			long start;
+			start = System.currentTimeMillis();
 			generator.generate(BasicMonitor.toMonitor(monitor), false);
+			Double finalTime = Double.valueOf(Double.valueOf(System.currentTimeMillis() - start)
+					.doubleValue() / 1000d);
+			Status logTransformationTime = new Status(Status.INFO,Activator.PLUGIN_ID,"Time to generate Modelica Code: "+finalTime);
+			// writes log into the .log file within the .metadata folder of the workspace
+			Activator.getDefault().getLog().log(logTransformationTime);
 		} finally {
 			directory.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
 		}
