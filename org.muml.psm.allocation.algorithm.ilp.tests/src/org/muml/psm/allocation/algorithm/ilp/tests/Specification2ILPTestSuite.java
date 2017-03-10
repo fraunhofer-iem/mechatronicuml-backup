72a138cb76c27f9d1a7f4dd98fd2d72390a61bc5
Marcus Hüwe <mhuewe@mail.upb.de>
2017-03-10 16:44:52 +0000

    [ASL] Do not explicitly initialize pim and hardware package
    See r20132 for the details.

--- org.muml.psm.allocation.algorithm.ilp.tests/src/org/muml/psm/allocation/algorithm/ilp/tests/Specification2ILPTestSuite.java
+++ org.muml.psm.allocation.algorithm.ilp.tests/src/org/muml/psm/allocation/algorithm/ilp/tests/Specification2ILPTestSuite.java
@@ -4,8 +4,6 @@ import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import org.muml.pim.PimPackage;
-import org.muml.pm.hardware.HardwarePackage;
 import org.muml.psm.allocation.ilp.lpsolve.xtext.LPSolveStandaloneSetup;
 import org.muml.psm.allocation.language.xtext.AllocationSpecificationLanguageStandaloneSetup;
 
@@ -26,10 +24,5 @@ public class Specification2ILPTestSuite {
 	public static void setUpClass() {
 		AllocationSpecificationLanguageStandaloneSetup.doSetup();
 		LPSolveStandaloneSetup.doSetup();
-		// explicitly setup these packages otherwise the qvto compilation
-		// fails (mars) - might be related to the qvto commit
-		// de499dbfbd960a63f62c4938d9dc71172e075120
-		PimPackage.eINSTANCE.eClass();
-		HardwarePackage.eINSTANCE.eClass();
 	}
 }
