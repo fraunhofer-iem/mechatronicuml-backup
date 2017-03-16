60048b5ee6a21093c54be4932c23351cebd58178
Ingo Budde <bingo@mail.upb.de>
2017-03-16 16:14:05 +0000

    [ImplicitCopy] Implement #1552: Use ECollections.setEList() which is cleaner.

--- ImplicitCopy/org.muml.implicitcopy.qvto.library/src/org/muml/ImplicitCopy/qvto/library/ImplicitCopyBlackbox.java
+++ ImplicitCopy/org.muml.implicitcopy.qvto.library/src/org/muml/ImplicitCopy/qvto/library/ImplicitCopyBlackbox.java
@@ -2,14 +2,13 @@ package org.muml.ImplicitCopy.qvto.library;
 
 import java.util.List;
 
+import org.eclipse.emf.common.util.ECollections;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.util.InternalEList;
-
 
 public class ImplicitCopyBlackbox {
-	
+
 	public ImplicitCopyBlackbox() {
 		super();
 	}
@@ -17,36 +16,12 @@ public class ImplicitCopyBlackbox {
 	@SuppressWarnings("unchecked")
 	public static void setValues(EObject element, EStructuralFeature feature, List<Object> values) {
 		if (feature.isMany()) {
-			if (isBidirectional(feature)) {
-				InternalEList<Object> target = ((InternalEList<Object>) element.eGet(feature));
-				int index = 0;
-				for (Object value : values) {
-					int position = target.indexOf(value);
-					if (position == -1) {
-						target.addUnique(index, value);
-					} else if (index != position) {
-						target.move(index, value);
-					}
-					index++;
-				}
-			} else {
-				element.eSet(feature, values);
-			}
+			EList<Object> listToSet = (EList<Object>) element.eGet(feature);
+			ECollections.setEList(listToSet, values);
 		} else if (values.isEmpty()) {
 			element.eSet(feature, null);
 		} else {
 			element.eSet(feature, values.get(0));
 		}
 	}
-	
-	private static boolean isBidirectional(EStructuralFeature feature) {
-		if (feature instanceof EReference) {
-			return ((EReference) feature).getEOpposite() != null;
-		}
-		return false;
-	}
-
 }
-
-
-
