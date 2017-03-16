17d73d78e719b8cd8b72bdc3271c83a0be7e8ecd
Marcus Hüwe <mhuewe@mail.upb.de>
2017-03-16 12:12:10 +0000

    [ASL] Remove generated xtend-gen code... finally

--- org.muml.psm.allocation.language.xtext.ui/xtend-gen/org/muml/psm/allocation/language/xtext/ui/view/BaseHandler.java
+++
@@ -1,8 +0,0 @@
-package org.muml.psm.allocation.language.xtext.ui.view;
-
-import org.eclipse.core.commands.AbstractHandler;
-
-@SuppressWarnings("all")
-public abstract class BaseHandler extends AbstractHandler {
-  protected final static String viewID = "org.muml.psm.allocation.language.xtext.ui.view.evaluation";
-}
