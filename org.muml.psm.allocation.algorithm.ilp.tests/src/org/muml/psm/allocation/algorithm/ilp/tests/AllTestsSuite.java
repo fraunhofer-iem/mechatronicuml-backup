5dcd0abdc0cf26f6ff0984938e09d911d8031c98
Marcus Hüwe <mhuewe@mail.upb.de>
2017-04-06 14:38:49 +0000

    [ASL] Add testsuite for the *algorithms.ilp.tests plugin

---
+++ org.muml.psm.allocation.algorithm.ilp.tests/src/org/muml/psm/allocation/algorithm/ilp/tests/AllTestsSuite.java
@@ -0,0 +1,13 @@
+package org.muml.psm.allocation.algorithm.ilp.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	OclContext2ILPTest.class
+})
+public class AllTestsSuite {
+
+}
