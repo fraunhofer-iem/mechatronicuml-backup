ea8f6b2c28fbdd0a50bd3a7ef01213974d26d977
Marcus Hüwe <mhuewe@mail.upb.de>
2017-04-07 09:40:39 +0000

    [ASL] Setup LPSolve (xtext) before running the opt4j.tests

--- org.muml.psm.allocation.algorithm.ilp.opt4j.tests/src/org/muml/psm/allocation/algorithm/ilp/opt4j/tests/AllTestsSuite.java
+++ org.muml.psm.allocation.algorithm.ilp.opt4j.tests/src/org/muml/psm/allocation/algorithm/ilp/opt4j/tests/AllTestsSuite.java
@@ -1,8 +1,10 @@
 package org.muml.psm.allocation.algorithm.ilp.opt4j.tests;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.muml.psm.allocation.ilp.lpsolve.xtext.LPSolveStandaloneSetup;
 
 @RunWith(Suite.class)
 @SuiteClasses({
@@ -10,5 +12,8 @@ import org.junit.runners.Suite.SuiteClasses;
 	ILP2SATTest.class
 })
 public class AllTestsSuite {
-
+	@BeforeClass
+	public static void setup() {
+		LPSolveStandaloneSetup.doSetup();
+	}
 }
