6cdafb80fa7dd53e7603197136ace98dae50299f
Marcus Hüwe <mhuewe@mail.upb.de>
2017-04-07 07:48:03 +0000

    [ASL] Register lpsolve parser before executing the algorithm.ilp.tests tests
    (Trying to get the testsuite pass via maven)

--- org.muml.psm.allocation.algorithm.ilp.tests/src/org/muml/psm/allocation/algorithm/ilp/tests/AllTestsSuite.java
+++ org.muml.psm.allocation.algorithm.ilp.tests/src/org/muml/psm/allocation/algorithm/ilp/tests/AllTestsSuite.java
@@ -1,13 +1,18 @@
 package org.muml.psm.allocation.algorithm.ilp.tests;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.muml.psm.allocation.ilp.lpsolve.xtext.LPSolveStandaloneSetup;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	OclContext2ILPTest.class
 })
 public class AllTestsSuite {
-
+	@BeforeClass
+	public static void setup() {
+		LPSolveStandaloneSetup.doSetup();
+	}
 }
