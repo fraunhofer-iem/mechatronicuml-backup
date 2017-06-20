6aef9d2c7bd026076520dd71a79264d37b21c7f7
Uwe Pohlmann <upohl>
2017-06-20 19:43:05 +0000

    added a sleep of 100 ms to the broker, productionstation, and generator to reduce the processor useage

--- Example/inProgress/org.muml.example.productionBroker/src-gen-container/broker/main.c
+++ Example/inProgress/org.muml.example.productionBroker/src-gen-container/broker/main.c
@@ -1,6 +1,6 @@
 	#include "lib/Debug.h"
 		#include "component_container/MCC_brokerComponent.h"
-
+		#include <time.h>
 
 //variable for component Instances
 			BrokerComponent* atomic_c1;
@@ -10,9 +10,14 @@ int main(){
 	#ifdef DEBUG
 	printDebugInformation("Initialization done...start execution.\n");
 	#endif
+	int milisec = 100; // length of time to sleep, in miliseconds
+	struct timespec req = {0};
+	req.tv_sec = 0;
+	req.tv_nsec = milisec * 1000000L;
 	while (1) {
 
 			BrokerComponent_processStep(atomic_c1);
+			nanosleep(&req, (struct timespec *)NULL);
 	}
 }	
 
--- Example/inProgress/org.muml.example.productionBroker/src-gen-container/generator/main.c
+++ Example/inProgress/org.muml.example.productionBroker/src-gen-container/generator/main.c
@@ -1,6 +1,6 @@
 	#include "lib/Debug.h"
 		#include "component_container/MCC_recipeGeneratorComponent.h"
-
+		#include <time.h>
 
 //variable for component Instances
 			RecipeGeneratorComponent* atomic_c1;
@@ -10,9 +10,14 @@ int main(){
 	#ifdef DEBUG
 	printDebugInformation("Initialization done...start execution.\n");
 	#endif
+	int milisec = 100; // length of time to sleep, in miliseconds
+	struct timespec req = {0};
+	req.tv_sec = 0;
+	req.tv_nsec = milisec * 1000000L;
 	while (1) {
 
 			RecipeGeneratorComponent_processStep(atomic_c1);
+			nanosleep(&req, (struct timespec *)NULL);
 	}
 }	
 
