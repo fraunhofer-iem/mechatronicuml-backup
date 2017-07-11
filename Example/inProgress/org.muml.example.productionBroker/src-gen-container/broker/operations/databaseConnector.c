a5c0f9c2743b7c54865b901ac880bc4d09e89b79
Melanie Bruns <mbruns42>
2017-07-11 12:06:55 +0000

    [ProductionBroker] Slimmed sqliteDatabase.c into databaseConnector.c that just does curl rrequests
    Not done yet, just correct functionality

---
+++ Example/inProgress/org.muml.example.productionBroker/src-gen-container/broker/operations/databaseConnector.c
@@ -0,0 +1,217 @@
+/*
+ * Compile this file together with the sqlite database engine source code
+ * to generate the executable. 
+ *
+ */
+
+#include <stdio.h>  /* puts() */
+#include <stdlib.h> /* exit() */
+#include <string.h> /* strchr(), strlen(), .. */
+#include <curl/curl.h>
+#include "cJSON.h"
+
+char *readConfigFile();
+void sendToDatabaseServer(char *jsonString);
+int insertOrder(int orderId, int ingredientID, int amount);
+int defineProductionStationForOrder(int orderID, int productionStationID);
+int getOrderIngredientID(int orderID);
+int getOrderAmount(int orderID);
+int searchOrder(int searchingPS, int latestOrderID, int producibleIngredients);
+int deleteOrder(int orderID);
+int heartBeatProductionStation(int productionStationID);
+void extractLogsAndExit()
+
+CURL *curl;
+char *url = readConfigFile();
+
+/**
+ * Takes a json string and sends it to the server via post request
+ *
+ */
+void sendToDatabaseServer(char *jsonString)
+{
+	curl = curl_easy_init();
+	if (curl)
+	{
+		printf("Curl init successful. \n");
+		/* First set the URL that is about to receive our POST. This URL can
+		   just as well be a https:// URL if that is what should receive the data. */
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		/* Only allow HTTP traffic */
+		curl_easy_setopt(curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTP);
+		printf("Json String: %s \n", jsonString);
+		//setting correct headers so that the server will interpret
+		//the post body as json
+		struct curl_slist *headers = NULL;
+		headers = curl_slist_append(headers, "Accept: application/json");
+		headers = curl_slist_append(headers, "Content-Type: application/json");
+		headers = curl_slist_append(headers, "charsets: utf-8");
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		/* pass in a pointer to the data - libcurl will not copy */
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonString);
+		/* Perform the request, res will get the return code */
+		CURLcode res = curl_easy_perform(curl);
+		/* Check for errors */
+		if (res != CURLE_OK)
+		{
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+					curl_easy_strerror(res));
+		}
+		curl_easy_cleanup(curl);
+	}
+}
+
+char *readConfigFile()
+{
+	char *buffer = 0;
+	long length;
+	char *url = "";
+
+	FILE *fp = fopen("config.json", "r");
+	if (fp)
+	{
+		fseek(fp, 0, SEEK_END);
+		length = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		buffer = malloc(length);
+		if (buffer)
+		{
+			fread(buffer, 1, length, fp);
+		}
+		fclose(fp);
+	}
+	if (buffer)
+	{
+		cJSON *root = cJSON_Parse(buffer);
+		if (root)
+		{
+			url = malloc(strlen(cJSON_GetObjectItem(root, "url")->valuestring));
+			sprintf(url, "%s", cJSON_GetObjectItem(root, "url")->valuestring);
+			cJSON_Delete(root);
+		}
+	}
+
+	return url;
+}
+
+/**
+ * Inserts and order with its ID, ingredient and amount and initial status "IDLE"
+ * and the current time as orderTime
+ */
+int insertOrder(int orderID, int ingredientID, int amount)
+{
+	cJSON *update;
+	update = cJSON_CreateObject();
+	cJSON_AddItemToObject(update, "update", cJSON_CreateString("newOrder"));
+	cJSON *changedTables;
+	cJSON_AddItemToObject(update, "changedTables", changedTables = cJSON_CreateObject());
+	cJSON *orders;
+	cJSON_AddItemToObject(changedTables, "Orders", orders = cJSON_CreateObject());
+	cJSON_AddNumberToObject(orders, "OrderID", orderID);
+	cJSON_AddStringToObject(orders, "OrderStatus", "IDLE");
+	sendToDatabaseServer(cJSON_Print(update));
+
+	return 0;
+}
+
+/**
+ * Inserts pair orderId and productionStation into the table OrderAllocation
+ * Sets status of the order in table Orders to 'IN_PRODUCTION'
+ */
+int defineProductionStationForOrder(int orderID, int productionStationID)
+{
+	cJSON *update = cJSON_CreateObject();
+	cJSON_AddItemToObject(update, "update", cJSON_CreateString("orderAssigned"));
+	cJSON *changedTables;
+	cJSON_AddItemToObject(update, "changedTables", changedTables = cJSON_CreateObject());
+	cJSON *orders;
+	cJSON_AddItemToObject(changedTables, "Orders", orders = cJSON_CreateObject());
+	cJSON_AddNumberToObject(orders, "OrderID", orderID);
+	cJSON_AddStringToObject(orders, "OrderStatus", "IN_PRODUCTION");
+	cJSON *orderAllocationJSON;
+	cJSON_AddItemToObject(changedTables, "OrderAllocation", orderAllocationJSON = cJSON_CreateObject());
+	cJSON_AddNumberToObject(orderAllocationJSON, "OrderID", orderID);
+	cJSON_AddNumberToObject(orderAllocationJSON, "ProductionStationID", productionStationID);
+	sendToDatabaseServer(cJSON_Print(update));
+
+	return 0;
+}
+
+/**
+ * Sets orderStatus to DONE
+ */
+int deleteOrder(int orderID)
+{
+	cJSON *update;
+	update = cJSON_CreateObject();
+	cJSON_AddItemToObject(update, "update", cJSON_CreateString("orderDone"));
+	cJSON *changedTables;
+	cJSON_AddItemToObject(update, "changedTables", changedTables = cJSON_CreateObject());
+	cJSON *orders;
+	cJSON_AddItemToObject(changedTables, "Orders", orders = cJSON_CreateObject());
+	cJSON_AddNumberToObject(orders, "OrderID", orderID);
+	cJSON_AddStringToObject(orders, "OrderStatus", "DONE");
+	sendToDatabaseServer(cJSON_Print(update));
+
+	return 0;
+}
+
+
+/**
+ * Retrieve the ingredientID for the order with the given id.
+ */
+int getOrderIngredientID(int orderID)
+{
+	printf("Successfully retrieved ingredientID %d for order %d.\n", ingredientID, orderID);
+	return ingredientID;
+}
+
+int getOrderAmount(int orderID)
+{
+	printf("Successfully retrieved amount %d for order %d.\n", amount, orderID);
+	return amount;
+}
+
+/**
+ * Searches an order without a production station assigned
+ */
+int searchOrder(int searchingPS, int latestOrderID, int producibleIngredients)
+{
+	//json format: {update: {searchOrder, changedTables: {ProductionStations: {ProductionStationID: id}}}}
+	cJSON *update;
+	update = cJSON_CreateObject();
+	cJSON_AddItemToObject(update, "update", cJSON_CreateString("searchOrder"));
+	cJSON *changedTables;
+	cJSON_AddItemToObject(update, "changedTables", changedTables = cJSON_CreateObject());
+	cJSON *productionStations;
+	cJSON_AddItemToObject(changedTables, "ProductionStations", productionStations = cJSON_CreateObject());
+	cJSON_AddNumberToObject(productionStations, "ProductionStationID", searchingPS);
+
+	sendToDatabaseServer(cJSON_Print(update));
+
+	return orderID;
+}
+
+
+/**
+ * Inserts and order with its ID, ingredient and amount and initial status "IDLE"
+ * and the current time as orderTime
+ */
+int heartBeatProductionStation(int productionStationID)
+{
+	//Send notification about seen production station to the visualization server
+	//json format: {update: {searchOrder, changedTables: {ProductionStations: {ProductionStationID: id}}}}
+	cJSON *update;
+	update = cJSON_CreateObject();
+	cJSON_AddItemToObject(update, "update", cJSON_CreateString("searchOrder"));
+	cJSON *changedTables;
+	cJSON_AddItemToObject(update, "changedTables", changedTables = cJSON_CreateObject());
+	cJSON *productionStations;
+	cJSON_AddItemToObject(changedTables, "ProductionStations", productionStations = cJSON_CreateObject());
+	cJSON_AddNumberToObject(productionStations, "ProductionStationID", productionStationID);
+
+	sendToDatabaseServer(cJSON_Print(update));
+
+	return orderID;
+}
+
