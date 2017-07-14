76929c2782328ea7d00d0b429a7be43bfb5ec2de
Uwe Pohlmann <upohl>
2017-07-14 12:21:13 +0000

    deleted deprecated eloquent / ASL plugins

--- PlatformSpecificModeling/deprecated/org.muml.psm.allocation.algorithm.ilp.opt4j/src/org/muml/psm/allocation/algorithm/ilp/opt4j/AllocationProblem.java
+++
@@ -1,7 +0,0 @@
-package org.muml.psm.allocation.algorithm.ilp.opt4j;
-
-import org.muml.psm.allocation.ilp.IntegerLinearProgram;
-
-public interface AllocationProblem {
-	IntegerLinearProgram getILP();
-}
--- PlatformSpecificModeling/deprecated/org.muml.psm.allocation.algorithm.ilp.opt4j/src/org/muml/psm/allocation/algorithm/ilp/opt4j/ilp2sat/IVisitor.java
+++
@@ -1,7 +0,0 @@
-package org.muml.psm.allocation.algorithm.ilp.opt4j.ilp2sat;
-
-import org.eclipse.emf.ecore.EObject;
-
-public interface IVisitor {
-	public void visit(EObject object);
-}
