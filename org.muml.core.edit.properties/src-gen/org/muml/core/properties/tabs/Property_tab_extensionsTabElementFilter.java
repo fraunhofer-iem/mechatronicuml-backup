a85e798a2d58265a5a8857fa67d890fade0cce7b
Ingo Budde <bingo@mail.upb.de>
2017-08-14 23:44:21 +0000

    [SiriusEvaluation] Branch core.edit.properties

---
+++ org.muml.core.edit.properties/src-gen/org/muml/core/properties/tabs/Property_tab_extensionsTabElementFilter.java
@@ -0,0 +1,8 @@
+
+package org.muml.core.properties.tabs;
+
+public class Property_tab_extensionsTabElementFilter extends org.muml.ape.runtime.filter.ElementFilter {
+	public Property_tab_extensionsTabElementFilter() {
+		super("property.tab.extensions");
+	}
+}
