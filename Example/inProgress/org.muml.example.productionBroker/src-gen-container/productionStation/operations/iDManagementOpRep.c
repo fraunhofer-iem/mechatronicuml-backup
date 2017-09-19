4b00e61a195425d5c5a76402e2932db7fdef3aa0
Melanie Bruns <mbruns42>
2017-09-19 10:46:47 +0000

    [ProductionBroker] Read ID for station from config.json

--- Example/inProgress/org.muml.example.productionBroker/src-gen-container/productionStation/operations/iDManagementOpRep.c
+++ Example/inProgress/org.muml.example.productionBroker/src-gen-container/productionStation/operations/iDManagementOpRep.c
@@ -1,18 +1,53 @@
 
 #include "iDManagementOpRep.h"
 /** Start of user code User includes **/ 
-
-
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "cJSON.h"
 /**End of user code**/
 
 int32_T IDManagement_iDManagementGetID(){
 
 /** Start of user code IDManagement_iDManagementGetID **/ 
-//@TODO: add your implementation here
-#warning Missing implemenation of repository operation 'IDManagement_iDManagementGetID'
-	//default return value
-	int32_T ret = 0;
-	return ret;
+	int32_T ID;
+	char *buffer = 0;
+	long length;
+
+	FILE *fp = fopen("config.json", "r");
+	if (fp)
+	{
+		fseek(fp, 0, SEEK_END);
+		length = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		buffer = malloc(length);
+		if (buffer)
+		{
+			fread(buffer, 1, length, fp);
+		}
+		fclose(fp);
+	}
+	if (buffer)
+	{
+		cJSON *root = cJSON_Parse(buffer);
+		if (root)
+		{
+			cJSON *ID_item = cJSON_GetObjectItem(root, "ID");
+			printf("IDisNumber=%d\n", cJSON_GetObjectItem(root, "ID")->valueint);
+			if (cJSON_IsNumber(ID_item))
+			{
+			  ID = ID_item->valueint;
+			}
+			printf("Found the following ID: %ld\n", ID);
+			return ID;
+		}
+	}
+	//If we did not read an ID from a config.json, generate a random one
+	srand(clock());
+	ID = rand();
+	printf("RandNr:%ld",ID);
+	//TODO store as config.json
+	return ID;
 /**End of user code**/
 
 }
