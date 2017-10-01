2bb2b7b8a4d087c29c13ab98395b2967341831f4
Ingo Budde <bingo@mail.upb.de>
2017-10-01 09:38:19 +0000

    [SiriusBranch] Remove view change hack that resulted in exceptions

---
+++ realtimestatechart.design/src/realtimestatechart/design/providers/MyDecoratorProvider.java
@@ -0,0 +1,48 @@
+package realtimestatechart.design.providers;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.common.core.service.AbstractProvider;
+import org.eclipse.gmf.runtime.common.core.service.IOperation;
+import org.eclipse.gmf.runtime.diagram.ui.services.decorator.CreateDecoratorsOperation;
+import org.eclipse.gmf.runtime.diagram.ui.services.decorator.IDecoratorProvider;
+import org.eclipse.gmf.runtime.diagram.ui.services.decorator.IDecoratorTarget;
+import org.eclipse.gmf.runtime.notation.Bounds;
+import org.eclipse.gmf.runtime.notation.Node;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.sirius.viewpoint.DRepresentationElement;
+import org.muml.pim.realtimestatechart.RealtimestatechartPackage;
+
+public class MyDecoratorProvider extends AbstractProvider implements IDecoratorProvider {
+
+	public MyDecoratorProvider() {
+		
+	}
+	
+	public void createDecorators(IDecoratorTarget decoratorTarget) {
+//		View view = decoratorTarget.getAdapter(View.class);
+//		if (view instanceof Node) {
+//			Node node = (Node) view;
+//			if (node.getLayoutConstraint() instanceof Bounds) {
+//				Bounds bounds = (Bounds) node.getLayoutConstraint();
+//				bounds.setHeight(20);
+//			}
+//		}
+	}
+
+	public boolean provides(IOperation operation) {
+		if (operation instanceof CreateDecoratorsOperation) {
+			IDecoratorTarget decorationTarget = ((CreateDecoratorsOperation) operation).getDecoratorTarget();
+			View view = decorationTarget.getAdapter(View.class);
+			if ("3009".equals(view.getType())) {
+				if (view.getElement() instanceof DRepresentationElement) {
+					EObject target = ((DRepresentationElement)view.getElement()).getTarget();
+					if (target.eClass().getEPackage() == RealtimestatechartPackage.eINSTANCE) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+}
---
+++ realtimestatechart.design/src/realtimestatechart/design/providers/MyViewProvider.java
@@ -0,0 +1,98 @@
+/*
+ * <copyright>
+ * Copyright (c) 2013 Software Engineering Group, Heinz Nixdorf Institute, University of Paderborn, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Software Engineering Group - initial API and implementation
+ * </copyright>
+ */
+package realtimestatechart.design.providers;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.gmf.runtime.common.core.service.AbstractProvider;
+import org.eclipse.gmf.runtime.common.core.service.IOperation;
+import org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint;
+import org.eclipse.gmf.runtime.diagram.core.providers.IViewProvider;
+import org.eclipse.gmf.runtime.diagram.core.services.view.CreateDiagramViewOperation;
+import org.eclipse.gmf.runtime.diagram.core.services.view.CreateEdgeViewOperation;
+import org.eclipse.gmf.runtime.diagram.core.services.view.CreateNodeViewOperation;
+import org.eclipse.gmf.runtime.diagram.core.services.view.CreateViewForKindOperation;
+import org.eclipse.gmf.runtime.diagram.core.services.view.CreateViewOperation;
+import org.eclipse.gmf.runtime.notation.Bounds;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.Edge;
+import org.eclipse.gmf.runtime.notation.LayoutConstraint;
+import org.eclipse.gmf.runtime.notation.Node;
+import org.eclipse.gmf.runtime.notation.View;
+
+public class MyViewProvider extends AbstractProvider implements IViewProvider {
+
+	public MyViewProvider() {
+		
+	}
+	public final boolean provides(IOperation operation) {
+		if (operation instanceof CreateViewForKindOperation) {
+			return provides((CreateViewForKindOperation) operation);
+		}
+		assert operation instanceof CreateViewOperation;
+		if (operation instanceof CreateDiagramViewOperation) {
+			return provides((CreateDiagramViewOperation) operation);
+		} else if (operation instanceof CreateEdgeViewOperation) {
+			return provides((CreateEdgeViewOperation) operation);
+		} else if (operation instanceof CreateNodeViewOperation) {
+			return provides((CreateNodeViewOperation) operation);
+		}
+		return false;
+	}
+
+	
+	protected boolean provides(CreateViewForKindOperation op) {
+		/*
+		    if (op.getViewKind() == Node.class)
+		      return getNodeViewClass(op.getSemanticAdapter(), op.getContainerView(), op.getSemanticHint()) != null;
+		    if (op.getViewKind() == Edge.class)
+		      return getEdgeViewClass(op.getSemanticAdapter(), op.getContainerView(), op.getSemanticHint()) != null;
+		*/
+		return true;
+	}
+
+
+	protected boolean provides(CreateDiagramViewOperation op) {
+		return false;
+	}
+
+	protected boolean provides(CreateNodeViewOperation op) {
+//		View view = op.getContainerView();
+//		if ("3009".equals(view.getType()) && view instanceof Node) {
+//			LayoutConstraint layoutConstraint = ((Node) view).getLayoutConstraint();
+//			if (layoutConstraint instanceof Bounds) {
+//				((Bounds) layoutConstraint).setHeight(20);
+//			}
+//		}
+		return false;
+	}
+
+	protected boolean provides(CreateEdgeViewOperation op) {
+		return false;
+
+	}
+	public Diagram createDiagram(IAdaptable semanticAdapter, String diagramKind, PreferencesHint preferencesHint) {
+		return null;
+	}
+
+	public Node createNode(IAdaptable semanticAdapter, View containerView, String semanticHint, int index,
+			boolean persisted, PreferencesHint preferencesHint) {
+		
+		return null;
+	}
+
+	public Edge createEdge(IAdaptable semanticAdapter, View containerView, String semanticHint, int index,
+			boolean persisted, PreferencesHint preferencesHint) {
+		return null;
+	}
+
+}
