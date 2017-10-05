2056d3e5cc5faeec989e0bec9eeea7c9ed96b4b7
Uwe Pohlmann <upohl>
2017-10-05 15:26:19 +0000

    edited testcase

--- Example/inProgress/org.muml.example.productionBroker/src-gen-container/broker/operations/databaseConnector_test.c
+++ Example/inProgress/org.muml.example.productionBroker/src-gen-container/broker/operations/databaseConnector_test.c
@@ -9,9 +9,14 @@ static void null_test_success(void **state) {
 	(void) state; /*unused*/
 	defineProductionStationForOrder(1,1);
 	defineProductionStationForOrder(2,2);
+	defineProductionStationForOrder(3,3);
 	node* first = getFirst();
-	printf("TestString:%d\n",first->stationID);
-	printf("TestNext:%d\n",first->next->stationID);
+	assert_true(first->stationID==1);
+	assert_true(first->next->stationID==2);
+	assert_true(first->next->next->stationID==3);
+	printf("TestFirst=1:%d\n",first->stationID);
+	printf("TestSecond=2:%d\n",first->next->stationID);
+	printf("TestThird=3:%d\n",first->next->next->stationID);
 }
 
 int main(void) {
