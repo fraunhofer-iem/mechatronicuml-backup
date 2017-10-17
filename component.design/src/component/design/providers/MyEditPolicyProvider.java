40e91ec8b863ac9c2e9e64c01c7e870421799fdb
Ingo Budde <bingo@mail.upb.de>
2017-10-17 14:30:27 +0000

    [SiriusBranch] Integrated embed as parts QVTO Transformation for ComponentParts.

--- component.design/src/component/design/providers/MyEditPolicyProvider.java
+++ component.design/src/component/design/providers/MyEditPolicyProvider.java
@@ -5,12 +5,21 @@ import org.eclipse.gef.EditPart;
 import org.eclipse.gmf.runtime.common.core.service.IOperation;
 import org.eclipse.gmf.runtime.diagram.ui.services.editpolicy.CreateEditPoliciesOperation;
 import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.sirius.diagram.DNode;
+import org.eclipse.sirius.diagram.description.DiagramDescription;
+import org.eclipse.sirius.diagram.description.NodeMapping;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
+import org.muml.pim.component.ComponentPart;
+
+import component.design.edit.policies.UpdateComponentPartEditPolicy;
 
 public class MyEditPolicyProvider extends AbstractEditPolicyProvider {
 
 	public void createEditPolicies(EditPart editPart) {
-		
+		EObject element = getSemanticElement(editPart);
+		if (element instanceof ComponentPart) {
+			editPart.installEditPolicy("ComponentPartUpdateRole", new UpdateComponentPartEditPolicy());
+		}
 	}
 
 	private EObject getSemanticElement(EditPart editPart) {
@@ -25,13 +34,37 @@ public class MyEditPolicyProvider extends AbstractEditPolicyProvider {
 		}
 		return null;
 	}
+	
+	DiagramDescription findDiagramDescription(EditPart editPart) {
+		if (editPart.getModel() instanceof View) {
+			View view = (View) editPart.getModel();
+			if (view.getElement() instanceof DNode) {
+				DNode node = (DNode) view.getElement();
+				EObject mappingElement = node.getActualMapping();
+				while (mappingElement != null) {
+					if (mappingElement instanceof DiagramDescription) {
+						return (DiagramDescription) mappingElement;
+					}
+					mappingElement = mappingElement.eContainer();
+				}
+			}
+		}
+		return null;
+	}
 
 	public boolean provides(IOperation operation) {
 		if (operation instanceof CreateEditPoliciesOperation) {
 			CreateEditPoliciesOperation castedOperation = (CreateEditPoliciesOperation) operation;
 			EditPart editPart = castedOperation.getEditPart();
-			EObject element = getSemanticElement(editPart);
 			
+			// check if I am responsible for this edit part
+			DiagramDescription diagramDescription = findDiagramDescription(editPart);
+			if (diagramDescription != null && "org.muml.pim.component.diagram".equals(diagramDescription.getName())) {
+				EObject element = getSemanticElement(editPart);
+				if (element instanceof ComponentPart) {
+					return true;
+				}
+			}
 		}
 		return false;
 	}
