a64b0ebca63bc40fa81236432e646d820b98ea6c
Ingo Budde <bingo@mail.upb.de>
2018-03-04 13:56:24 +0000

    [SiriusBranch] Working on Reconfiguration Editors

---
+++ org.muml.reconfiguration.design/src/org/muml/reconfiguration/design/edit/policies/MyEditPolicyProvider.java
@@ -0,0 +1,89 @@
+package org.muml.reconfiguration.design.edit.policies;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.common.core.service.IOperation;
+import org.eclipse.gmf.runtime.diagram.ui.services.editpolicy.CreateEditPoliciesOperation;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.sirius.diagram.AbstractDNode;
+import org.eclipse.sirius.diagram.DDiagram;
+import org.eclipse.sirius.diagram.description.DiagramDescription;
+import org.eclipse.sirius.viewpoint.DSemanticDecorator;
+import org.muml.reconfiguration.componentstorypattern.PortVariable;
+
+public class MyEditPolicyProvider extends AbstractEditPolicyProvider {
+
+	public void createEditPolicies(EditPart editPart) {
+		EObject element = getSemanticElement(editPart);
+//		if (element instanceof ComponentPart) {
+//			editPart.installEditPolicy("ComponentPartUpdate", new UpdateComponentPartEditPolicy());
+//		}
+//		if (element instanceof ComponentInstance) {
+//			editPart.installEditPolicy("ComponentInstanceUpdate", new UpdateComponentInstanceEditPolicy());
+//		}
+		if (element instanceof PortVariable) {
+			editPart.installEditPolicy(org.muml.pim.common.edit.policies.EditPolicyRoles.PORT_VISUALIZATION_ROLE,
+					new PortVariableEditPolicy());
+		}
+//		if (element instanceof PortPart) {
+//			editPart.installEditPolicy(org.muml.pim.common.edit.policies.EditPolicyRoles.PORT_VISUALIZATION_ROLE,
+//					new PortPartEditPolicy());
+//		}
+//		if (element instanceof PortInstance) {
+//			editPart.installEditPolicy(org.muml.pim.common.edit.policies.EditPolicyRoles.PORT_VISUALIZATION_ROLE,
+//					new PortInstanceEditPolicy());
+//		}
+	}
+
+	private EObject getSemanticElement(EditPart editPart) {
+		Object model = editPart.getModel();
+		if (model instanceof View) {
+			View view = (View) model;
+			EObject element = view.getElement();
+			if (element instanceof DSemanticDecorator) {
+				element = ((DSemanticDecorator) element).getTarget();
+			}
+			return element;
+		}
+		return null;
+	}
+
+	DiagramDescription findDiagramDescription(EditPart editPart) {
+		if (editPart.getModel() instanceof View) {
+			View view = (View) editPart.getModel();
+			if (view.getElement() instanceof AbstractDNode) {
+				AbstractDNode node = (AbstractDNode) view.getElement();
+				if (node != null) {
+					DDiagram diagram = node.getParentDiagram();
+					if (diagram != null) {
+						return diagram.getDescription();
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public boolean provides(IOperation operation) {
+		if (operation instanceof CreateEditPoliciesOperation) {
+			CreateEditPoliciesOperation castedOperation = (CreateEditPoliciesOperation) operation;
+			EditPart editPart = castedOperation.getEditPart();
+
+			// check if I am responsible for this edit part
+			DiagramDescription diagramDescription = findDiagramDescription(editPart);
+			if (diagramDescription != null) {
+				if ("org.muml.reconfiguration.componentstoryrule.diagram".equals(diagramDescription.getName())) {
+					EObject element = getSemanticElement(editPart);
+					if (element instanceof PortVariable) {
+						return true;
+					}
+//					if (element instanceof ComponentVariable) {
+//						return true;
+//					}
+				}
+			}
+		}
+		return false;
+	}
+
+}
