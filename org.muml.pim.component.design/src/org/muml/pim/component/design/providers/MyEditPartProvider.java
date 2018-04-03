5ea736ac305eecd1352ac3338c5d6bd01aef4b5d
Ingo Budde <bingo@mail.upb.de>
2018-04-03 08:49:21 +0000

    [SiriusBranch] Working on Component Editor

--- org.muml.pim.component.design/src/org/muml/pim/component/design/providers/MyEditPartProvider.java
+++ org.muml.pim.component.design/src/org/muml/pim/component/design/providers/MyEditPartProvider.java
@@ -2,18 +2,16 @@ package org.muml.pim.component.design.providers;
 
 import org.eclipse.gmf.runtime.diagram.ui.services.editpart.AbstractEditPartProvider;
 import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.sirius.diagram.CustomStyle;
-import org.muml.pim.component.diagram.edit.parts.HybridPortEditPart;
 
 public class MyEditPartProvider extends AbstractEditPartProvider {
     @Override
-    protected Class getNodeEditPartClass(View view) {
-        if (view.getElement() instanceof CustomStyle) {
-            CustomStyle customStyle = (CustomStyle) view.getElement();
-            if (customStyle.getId().equals("org.muml.pim.port.style")) {
-                return HybridPortEditPart.class;
-            }
-        }
+    protected Class<?> getNodeEditPartClass(View view) {
+//        if (view.getElement() instanceof CustomStyle) {
+//            CustomStyle customStyle = (CustomStyle) view.getElement();
+//            if (customStyle.getId().equals("org.muml.pim.port.style")) {
+//                return HybridPortEditPart.class;
+//            }
+//        }
         return super.getNodeEditPartClass(view);
     }
 }
